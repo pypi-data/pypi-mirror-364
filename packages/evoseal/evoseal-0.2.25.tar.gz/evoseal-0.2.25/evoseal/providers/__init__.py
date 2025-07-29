4058b8a61a510bb8ceb1260ab879d66ba91fbe40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:57 +0000

    Add package evoseal/0.2.25/evoseal-0.2.25.tar.gz

---
+++ packages/evoseal/evoseal-0.2.25.tar.gz/evoseal-0.2.25/evoseal/providers/__init__.py
@@ -0,0 +1,9 @@
+"""AI/ML model providers for EVOSEAL.
+
+This module contains implementations of various AI/ML model providers that can be used
+with EVOSEAL for code generation and analysis.
+"""
+
+from evoseal.providers.seal_providers import SEALProvider
+
+__all__ = ["SEALProvider"]
