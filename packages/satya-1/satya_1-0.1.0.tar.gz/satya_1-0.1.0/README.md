f2000bb895546089125fcd029ffcf3ee1ff22f46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:51 +0000

    Add package satya-1/0.1.0/satya_1-0.1.0.tar.gz

---
+++ packages/satya-1/satya_1-0.1.0.tar.gz/satya_1-0.1.0/README.md
@@ -0,0 +1,26 @@
+# satya
+
+A simple interpreter for data structures and algorithms in natural language.
+
+## Installation
+
+```bash
+pip install satya
+```
+
+## Usage
+
+```python
+from satya import SatyaInterpreter
+
+interpreter = SatyaInterpreter()
+print(interpreter.run("Create linked list with 3 nodes"))
+```
+
+## Features
+- Linked lists, stacks, queues, graphs
+- Natural language commands (e.g., "Add edge A B", "Push X", "Is 121 palindrome?")
+- Math and logic (factorial, fibonacci, prime, armstrong, sort, search)
+
+## License
+MIT
---
+++ packages/satya-1/satya_1-0.1.0.tar.gz/satya_1-0.1.0/setup.py
@@ -0,0 +1,19 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='satya-1',
+    version='0.1.0',
+    packages=find_packages(),
+    description='Satya - Educational Python interpreter for data structures and algorithms in natural language.',
+    long_description=open('README.md').read() if __import__('os').path.exists('README.md') else '',
+    long_description_content_type='text/markdown',
+    author='Satya Bharadwaj Vemparala',
+    author_email='vvempara@gitam.in.com',
+    
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.6',
+)
