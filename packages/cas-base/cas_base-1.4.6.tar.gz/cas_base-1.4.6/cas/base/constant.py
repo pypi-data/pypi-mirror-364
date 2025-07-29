132a05054888cfa806ea90542415cf7a39ae8af7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:40 +0000

    Add package cas-base/1.4.6/cas_base-1.4.6.tar.gz

---
+++ packages/cas-base/cas_base-1.4.6.tar.gz/cas_base-1.4.6/cas/base/constant.py
@@ -0,0 +1,15 @@
+import os
+
+base_dir = "./"
+config_dir = os.path.join(base_dir, "config")
+log_dir = os.path.join(config_dir, "logs")
+log_level = os.getenv("LOG_LEVEL", "INFO")
+current_env = os.getenv("CURRENT_ENV", "pro")
+
+is_dev = current_env == "dev"
+
+if not os.path.exists(config_dir):
+    os.makedirs(config_dir)
+
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
