4721b515e25588c08398ace8e41527476b626fe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:58 +0000

    Add package django-pass-gen-safe/0.1.1/django_pass_gen_safe-0.1.1.tar.gz

---
+++ packages/django-pass-gen-safe/django_pass_gen_safe-0.1.1.tar.gz/django_pass_gen_safe-0.1.1/management/generate_password.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+import random
+import string
+
+class Command(BaseCommand):
+    help = 'Génère un mot de passe sécurisé'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--length', type=int, default=12, help='Longueur du mot de passe')
+        parser.add_argument('--no-special', action='store_true', help='Exclure les caractères spéciaux')
+
+    def handle(self, *args, **options):
+        length = options['length']
+        use_special = not options['no-special']
+
+        letters = string.ascii_letters
+        digits = string.digits
+        special = string.punctuation
+        all_chars = letters + digits + (special if use_special else '')
+
+        password = ''.join(random.choice(all_chars) for _ in range(length))
+        self.stdout.write(f"Mot de passe généré : {password}")
---
+++ packages/django-pass-gen-safe/django_pass_gen_safe-0.1.1.tar.gz/django_pass_gen_safe-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-pass-gen-safe',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=['django>=4.0', 'cryptography>=41.0.0'],
+    description='Générateur de mots de passe et trousseau sécurisé pour Django',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Momo',
+    author_email='omomo9414@gmail.com',
+    classifiers=[
+        'Framework :: Django',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
