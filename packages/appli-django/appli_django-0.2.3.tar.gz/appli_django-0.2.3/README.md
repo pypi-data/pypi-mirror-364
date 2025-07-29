2a2b91389209b5f2906d0f10153b0c4c922f2be6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:01 +0000

    Add package appli-django/0.2.3/appli_django-0.2.3.tar.gz

---
+++ packages/appli-django/appli_django-0.2.3.tar.gz/appli_django-0.2.3/README.md
@@ -0,0 +1,20 @@
+# appli-django
+
+📦 **appli-django** est un outil en ligne de commande (CLI) qui permet de créer rapidement une structure d'application Django enrichie avec les fichiers utiles, y compris :
+
+- `views.py`
+- `urls.py`
+- `forms.py`
+- `serializers.py`
+- Et le dossier `migrations/` avec `__init__.py`
+
+---
+
+## Installation
+
+### Depuis les sources (localement) :
+
+
+```bash
+pip install appli-django
+appli-django <nom_de_ton_appli>
---
+++ packages/appli-django/appli_django-0.2.3.tar.gz/appli_django-0.2.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='appli-django',
+    version='0.2.3',
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'appli-django=appli_django.cli:main',
+        ],
+    },
+    author='Nicosidick',
+    description='Un outil CLI pour créer des apps Django automatiquement.',
+
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type='text/markdown',  # ⬅️ TRÈS IMPORTANT
+
+    license='MIT',
+    python_requires='>=3.6',
+)
