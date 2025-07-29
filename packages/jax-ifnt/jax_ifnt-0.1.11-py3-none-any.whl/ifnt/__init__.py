4f9e435952a00cd91da5603693f5c164c48f2fdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:20 +0000

    Add package jax-ifnt/0.1.11/jax_ifnt-0.1.11-py3-none-any.whl

---
+++ packages/jax-ifnt/jax_ifnt-0.1.11-py3-none-any.whl/ifnt/__init__.py
@@ -0,0 +1,21 @@
+from .util import (
+    disable,
+    index_guard,
+    is_traced,
+    print,
+    raise_if_traced,
+    skip_if_traced,
+)
+from . import random, testing
+
+
+__all__ = [
+    "disable",
+    "index_guard",
+    "is_traced",
+    "print",
+    "raise_if_traced",
+    "random",
+    "skip_if_traced",
+    "testing",
+]
