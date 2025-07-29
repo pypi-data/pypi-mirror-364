d3e70e63c8f2951d7c42057d2de65eb004672253
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:50 +0000

    Add package lang111/0.1.0/lang111-0.1.0-py3-none-any.whl

---
+++ packages/lang111/lang111-0.1.0-py3-none-any.whl/satya/__init__.py
@@ -0,0 +1,7 @@
+# Satya Language Library
+# A Python library for the Satya language interpreter
+
+from .core import SatyaLinkedList, SatyaStack, SatyaQueue, SatyaGraph, SatyaInterpreter
+
+__version__ = '0.1.0'
+__all__ = ['SatyaLinkedList', 'SatyaStack', 'SatyaQueue', 'SatyaGraph', 'SatyaInterpreter']
