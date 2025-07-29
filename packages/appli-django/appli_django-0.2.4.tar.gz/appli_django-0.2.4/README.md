dbef244c134c5470d405fc5e34e8e488a9043799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:06:42 +0000

    Add package appli-django/0.2.4/appli_django-0.2.4.tar.gz

---
+++ packages/appli-django/appli_django-0.2.4.tar.gz/appli_django-0.2.4/README.md
@@ -0,0 +1,12 @@
+# appli-django
+
+🚀 Génère rapidement une structure complète d'application Django avec :
+
+- `models/`, `views/`, `migrations/`
+- `urls.py`, `admin.py`, `apps.py`, `tests.py`
+
+## ✅ Installation
+
+```bash
+pip install appli-django
+appli-django <nom du app>
---
+++ packages/appli-django/appli_django-0.2.4.tar.gz/appli_django-0.2.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='appli-django',
+    version='0.2.4',
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'appli-django=appli_django.cli:main',
+        ],
+    },
+    author='Nicosidick',
+    description='Créer rapidement une structure complète d\'application Django.',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+    python_requires='>=3.6',
+)
