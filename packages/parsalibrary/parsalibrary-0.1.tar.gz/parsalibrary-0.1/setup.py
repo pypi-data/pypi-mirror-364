91b8907174a0a8b12fa41103cb90fb14e2535d8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:47 +0000

    Add package parsalibrary/0.1/parsalibrary-0.1.tar.gz

---
+++ packages/parsalibrary/parsalibrary-0.1.tar.gz/parsalibrary-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='parsalibrary',
+    version='0.1',
+    packages=find_packages(),
+    description='A simple library management system',
+    author='Parsa Roohafzaei',
+    author_email='parsaroohafzaei@gmail.com',
+    keywords=['library', 'books', 'management'],
+)
