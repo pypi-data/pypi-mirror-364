64a0dfbd96be8b2349f4670fef746423acb23dca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:12 +0000

    Add package pyalamake/1.1.2/pyalamake-1.1.2.tar.gz

---
+++ packages/pyalamake/pyalamake-1.1.2.tar.gz/pyalamake-1.1.2/src/pyalamake/build_info.py
@@ -0,0 +1,12 @@
+class BuildInfo:  # pylint: disable=too-few-public-methods
+    python_version = '3.12.3'
+    os_name = 'posix:ubuntu'
+    version = '1.1.2'
+    git_sha = 'c7d85f4478cb7d4c9712ee66092b06f5f777fb78'
+    git_branch = 'master'
+    git_uncommitted = [
+        ' M do_publish',
+        ' m tools/xplat_utils',
+    ]
+    git_unpushed = [
+    ]
