761667002fb7cc5e368f708372dcc45da169514a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:48 +0000

    Add package ledlang/0.2.8.dev20250722213428/ledlang-0.2.8.dev20250722213428-py3-none-any.whl

---
+++ packages/ledlang/ledlang-0.2.8.dev20250722213428-py3-none-any.whl/ledlang/pytests/test_multi.py
@@ -0,0 +1,43 @@
+import os
+import pty
+import serial
+import threading
+from ledlang import MultiLEDLang, PytestLEDDeviceSimulator
+
+def test_multi():
+    simulator1 = PytestLEDDeviceSimulator("3x3")
+    simulator2 = PytestLEDDeviceSimulator("3x3")
+    threading.Thread(target=simulator1.run, daemon=True).start()
+    threading.Thread(target=simulator2.run, daemon=True).start()
+    displays = {
+        'size': '3x3',
+        'displays': [
+            {
+                'serial': simulator1.serial,
+                'rotation': 0
+            },
+            {
+                'serial': simulator2.serial,
+                'rotation': 0
+            },
+        ]
+    }
+    multi = MultiLEDLang(displays)
+    multi.play(multi.compile("""
+    INIT 6x3
+    DISPLAY [
+    1 1 0 0 1 1
+    1 1 0 0 1 1
+    1 1 0 0 1 1
+    ]
+    """))
+    assert simulator1.kill() == [
+        [1, 1, 0],
+        [1, 1, 0],
+        [1, 1, 0],
+    ]
+    assert simulator2.kill() == [
+        [0, 1, 1],
+        [0, 1, 1],
+        [0, 1, 1],
+    ]
