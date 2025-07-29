c3a6ac0aaf5ca195eee8f239abdeb6c59b35a48d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:30 +0000

    Add package fink-anomaly-detection-model/0.4.81/fink_anomaly_detection_model-0.4.81-py3-none-any.whl

---
+++ packages/fink-anomaly-detection-model/fink_anomaly_detection_model-0.4.81-py3-none-any.whl/fink_anomaly_detection_model/__init__.py
@@ -0,0 +1,4 @@
+from .main_script_ import fink_ad_model_train
+from .reactions_reader import get_reactions
+from .reactions_reader import load_reactions
+from .data_transformer import data_transform
