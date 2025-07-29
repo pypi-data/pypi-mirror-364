6e72568af45fb34fc91ccef4d25644b687553b01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:56 +0000

    Add package async-obj/1.0.1/async_obj-1.0.1-py3-none-any.whl

---
+++ packages/async-obj/async_obj-1.0.1-py3-none-any.whl/src/__init__.py
@@ -0,0 +1,21 @@
+# Copyright 2025 Gun Deniz Akkoc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# https://github.com/gunakkoc/async_obj
+
+from .async_obj import async_obj
+__all__ = ['async_obj']
+
+__version__ = "1.0.0"
+__author__ = 'Gun Deniz Akkoc'
