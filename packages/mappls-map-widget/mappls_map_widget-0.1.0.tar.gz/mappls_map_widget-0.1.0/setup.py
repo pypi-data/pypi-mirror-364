c99ca5289cf71b1662cc547c30df5437990a5d0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:56 +0000

    Add package mappls-map-widget/0.1.0/mappls_map_widget-0.1.0.tar.gz

---
+++ packages/mappls-map-widget/mappls_map_widget-0.1.0.tar.gz/mappls_map_widget-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mappls-map-widget",
+    version="0.1.0",
+    description="Mappls Map Widget for Django",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Soumay Gupta",
+    author_email="pmrs9673@gmail.com",
+    url="https://github.com/soumayg9673/mappls-django",
+    license="MIT",
+    packages=find_packages(include=["mappls_map_widget", "mappls_map_widget.*"]),
+    include_package_data=True,
+    install_requires=[
+        "Django>=3.2",
+    ],
+    python_requires=">=3.7",
+)
