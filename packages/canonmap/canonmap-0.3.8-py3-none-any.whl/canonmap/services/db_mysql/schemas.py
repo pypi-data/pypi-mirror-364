6d1b9cf3d096535167dae839b97f3e7aad72fb02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:03 +0000

    Add package canonmap/0.3.8/canonmap-0.3.8-py3-none-any.whl

---
+++ packages/canonmap/canonmap-0.3.8-py3-none-any.whl/canonmap/services/db_mysql/schemas.py
@@ -0,0 +1,23 @@
+from typing import List, Optional
+from enum import Enum
+from pydantic import BaseModel
+
+class CreateDDLResponse(BaseModel):
+    ddl: str
+    ddl_path: Optional[str] = None
+
+class FieldTransform(str, Enum):
+    INITIALISM = "initialism"
+    PHONETIC = "phonetic"
+    SOUNDEX = "soundex"
+
+class Field(BaseModel):
+    table_name: str
+    field_name: str
+    field_transform: FieldTransform = None
+
+class MatchEntityRequest(BaseModel):
+    entity_name: str
+    select_fields: List[Field]
+    top_n: int = 20
+    max_prefilter: int = 1000
