3a47b08d886069f459dc49dbffce6b6f53088a24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:37 +0000

    Add package kstflow/0.1.0/kstflow-0.1.0.tar.gz

---
+++ packages/kstflow/kstflow-0.1.0.tar.gz/kstflow-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kstflow',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'tqdm',
+    ],
+    include_package_data=True,
+    description='KST AI Utilities for medical datasets',
+    author='Watcharaphong Yookwan',
+    author_email='kst@informatics.buu.ac.th',
+    url='https://github.com/yourusername/kst-ai',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
