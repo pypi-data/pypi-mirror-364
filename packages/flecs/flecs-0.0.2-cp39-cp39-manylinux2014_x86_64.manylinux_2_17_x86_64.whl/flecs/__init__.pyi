b802039d9ab1670ed12bba927d2626bc46ed4c26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:51 +0000

    Add package flecs/0.0.2/flecs-0.0.2-cp39-cp39-manylinux2014_x86_64.manylinux_2_17_x86_64.whl

---
+++ packages/flecs/flecs-0.0.2-cp39-cp39-manylinux2014_x86_64.manylinux_2_17_x86_64.whl/flecs/__init__.pyi
@@ -0,0 +1,19 @@
+"""
+Pybind11 example plugin
+-----------------------
+
+.. currentmodule:: flecs
+
+.. autosummary::
+    :toctree: _generate
+
+    add
+    subtract
+"""
+
+def add(i: int, j: int) -> int:
+    """
+    Add two numbers
+
+    Some other explanation about the add function.
+    """
