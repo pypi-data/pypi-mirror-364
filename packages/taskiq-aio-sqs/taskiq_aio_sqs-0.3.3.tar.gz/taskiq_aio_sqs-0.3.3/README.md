3e64d40a13669c8da3d66035ad8634284cae2068
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:18 +0000

    Add package taskiq-aio-sqs/0.3.3/taskiq_aio_sqs-0.3.3.tar.gz

---
+++ packages/taskiq-aio-sqs/taskiq_aio_sqs-0.3.3.tar.gz/taskiq_aio_sqs-0.3.3/README.md
@@ -0,0 +1,196 @@
+# TaskIQ SQS/S3 aiobotocore
+
+[![PyPI](https://img.shields.io/pypi/v/taskiq-aio-sqs)](https://pypi.org/project/taskiq-aio-sqs/)
+[![Python Versions](https://img.shields.io/pypi/pyversions/taskiq-aio-sqs)](https://pypi.org/project/taskiq-aio-sqs/)
+[![Ruff](https://img.shields.io/endpoint?url=https://raw.githubusercontent.com/astral-sh/ruff/main/assets/badge/v2.json)](https://github.com/astral-sh/ruff)
+[![pre-commit](https://img.shields.io/badge/pre--commit-enabled-brightgreen?logo=pre-commit&logoColor=white)](https://github.com/pre-commit/pre-commit)
+[![Coverage Status](./coverage-badge.svg?dummy=8484744)](./coverage.xml)
+
+This library provides you with a fully asynchronous SQS broker and S3 backend for TaskIQ using aiobotocore.
+Inspired by the [taskiq-sqs](https://github.com/ApeWorX/taskiq-sqs) broker.
+
+Besides the SQS broker, this library also provides an S3 backend for the results, this is useful when the results are too large for SQS.
+Addidionally, the broker itself can be configured to use S3 + SQS for messages that are too large for SQS,
+replicating the behaviour of the [Amazon Extended Client Library](https://docs.aws.amazon.com/AWSSimpleQueueService/latest/SQSDeveloperGuide/sqs-managing-large-messages.html).
+
+## Installation
+
+```bash
+pip install taskiq-aio-sqs
+```
+
+## General Usage:
+Here is an example of how to use the SQS broker with the S3 backend:
+
+```python
+# broker.py
+import asyncio
+from taskiq_aio_sqs import SQSBroker, S3Backend
+
+s3_result_backend = S3Backend(
+    endpoint_url="http://localhost:4566",
+    bucket_name="response-bucket",  # bucket must exist
+)
+
+broker = SQSBroker(
+    endpoint_url="http://localhost:4566",
+    sqs_queue_name="my-queue",
+).with_result_backend(s3_result_backend)
+
+
+@broker.task
+async def i_love_aws() -> None:
+    """I hope my cloud bill doesn't get too high!"""
+    await asyncio.sleep(5.5)
+    print("Hello there!")
+
+
+async def main():
+    task = await i_love_aws.kiq()
+    print(await task.wait_result())
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+```
+### Delayed Tasks:
+
+Delayed tasks can be created in 3 ways:
+ - by using the `delay` parameter in the task decorator
+ - by using the kicker with the `delay` label
+ - by setting the `delay_seconds` parameter in the broker, which will apply to all tasks processed by the broker.
+
+Here's an example of how to use delayed tasks:
+
+```python
+broker = SQSBroker(
+    endpoint_url="http://localhost:4566",
+    delay_seconds=3,
+    sqs_queue_name="my-queue",
+)
+
+@broker.task()
+async def general_task() -> int:
+    return 1
+
+@broker.task(delay=7)
+async def delayed_task() -> int:
+    return 1
+
+async def main():
+    await broker.startup()
+    # This message will be received by workers after 3 seconds
+    # delay using the delay_seconds parameter in the broker init.
+    await general_task.kiq()
+
+    # This message will be received by workers after 7 seconds delay.
+    await delayed_task.kiq()
+
+    # This message is going to be received after the delay in 4 seconds.
+    # Since we overriden the `delay` label using kicker.
+    await delayed_task.kicker().with_labels(delay=4).kiq()
+
+```
+
+### Extended Messages with S3:
+
+You can also use S3 to store messages that are too large for SQS. To do this, you need to set the `s3_extended_bucket_name` parameter in the broker configuration.
+
+Here's an example of this behaviour:
+```python
+pub_broker = SQSBroker(
+    endpoint_url="http://localhost:4566",
+    sqs_queue_name="my-queue",
+    s3_extended_bucket_name="response-bucket",
+)
+
+sub_broker = SQSBroker(
+    endpoint_url="http://localhost:4566",
+    sqs_queue_name="my-queue",
+)
+
+LARGE_MESSAGE = b"x" * (256 * 1024 + 1)  # 256 KB is the limit for SQS
+
+@pub_broker.task()
+async def large_task() -> bytes:
+    return LARGE_MESSAGE
+
+
+async def main():
+    await pub_broker.startup()
+    await sub_broker.startup()
+    # This message will store data in S3 and send a reference to SQS
+    # This reference will include the S3 bucket and key.
+    await large_task.kiq()
+
+    async for msg in sub_broker.listen():
+        message = msg
+        break  # Stop after receiving one message
+
+    # The message will be automatically retrieved from S3
+    # and the full data will be available in the message.
+    assert message.data == LARGE_MESSAGE
+
+
+```
+
+## Configuration:
+
+SQS Broker parameters:
+* `endpoint_url` - url to access sqs, this is particularly useful if running on ECS.
+* `sqs_queue_name` - name of the sqs queue.
+* `region_name` - region name, defaults to `us-east-1`.
+* `aws_access_key_id` - aws access key id (Optional).
+* `aws_secret_access_key` - aws secret access key (Optional).
+* `use_task_id_for_deduplication` - use task_id for deduplication, this is useful when using a Fifo queue without content based deduplication, defaults to False.
+* `wait_time_seconds` - wait time in seconds for long polling, defaults to 0.
+* `max_number_of_messages` - maximum number of messages to receive, defaults to 1 (max 10).
+* `s3_extended_bucket_name` - extended bucket name for the s3 objects,
+  adding this will allow the broker to kick messages that are too large for SQS by using S3 as well,
+  by default the listen function handles this behaviour, defaults to None.
+* `task_id_generator` - custom task_id generator (Optional).
+* `result_backend` - custom result backend (Optional).
+
+
+S3 Result Backend parameters:
+* `bucket_name` - name of the s3 bucket.
+* `base_path` - base path for the s3 objects, defaults to "".
+* `endpoint_url` - url to access s3, this is particularly useful if running on ECS.
+* `region_name` - region name, defaults to `us-east-1`.
+* `aws_access_key_id` - aws access key id (Optional).
+* `aws_secret_access_key` - aws secret access key (Optional).
+* `serializer` - custom serializer, defaults to `OrjsonSerializer`.
+
+# Local Development:
+We use make to handle the commands for the project, you can see the available commands by running this in the root directory:
+```bash
+make
+```
+
+## Setup
+To setup the project, you can run the following commands:
+```bash
+make install
+```
+This will install the required dependencies for the project just using pip.
+
+## Linting
+We use pre-commit to do linting locally, this will be included in the dev dependencies.
+We use ruff for linting and formatting, and pyright for static type checking.
+To install the pre-commit hooks, you can run the following command:
+```bash
+pre-commit install
+```
+If you for some reason hate pre-commit, you can run the following command to lint the code:
+```bash
+make check
+```
+
+## Testing
+To run tests, you can use the following command:
+```bash
+make test
+```
+In the background this will setup localstack to replicate the AWS services, and run the tests.
+It will also generate the coverage report and the badge.
---
+++ packages/taskiq-aio-sqs/taskiq_aio_sqs-0.3.3.tar.gz/taskiq_aio_sqs-0.3.3/taskiq_aio_sqs/sqs_broker.py
@@ -0,0 +1,307 @@
+from __future__ import annotations
+
+import contextlib
+import json
+import logging
+from typing import (
+    TYPE_CHECKING,
+    Annotated,
+    AsyncGenerator,
+    Awaitable,
+    Callable,
+    Generator,
+)
+
+from aiobotocore.session import get_session
+from annotated_types import Ge, Le
+from botocore.exceptions import ClientError
+from pydantic import TypeAdapter
+from taskiq import AsyncBroker
+from taskiq.acks import AckableMessage
+from taskiq.message import BrokerMessage
+
+from taskiq_aio_sqs import constants, exceptions
+
+if TYPE_CHECKING:  # pragma: no cover
+    from types_aiobotocore_s3.client import S3Client
+    from types_aiobotocore_sqs.client import SQSClient
+    from types_aiobotocore_sqs.type_defs import (
+        GetQueueUrlResultTypeDef,
+        MessageTypeDef,
+        SendMessageRequestTypeDef,
+    )
+
+logger = logging.getLogger(__name__)
+DelaySeconds = TypeAdapter(Annotated[int, Le(900), Ge(0)])
+MaxNumberOfMessages = TypeAdapter(Annotated[int, Le(10), Ge(0)])
+
+
+class SQSBroker(AsyncBroker):
+    """AWS SQS TaskIQ broker."""
+
+    def __init__(
+        self,
+        endpoint_url: str,
+        sqs_queue_name: str,
+        region_name: str = constants.DEFAULT_REGION,
+        aws_access_key_id: str | None = None,
+        aws_secret_access_key: str | None = None,
+        use_task_id_for_deduplication: bool = False,
+        wait_time_seconds: int = 10,
+        max_number_of_messages: int = 1,
+        delay_seconds: int = 0,
+        s3_extended_bucket_name: str | None = None,
+    ) -> None:
+        """Initialize the SQS broker.
+
+        :param endpoint_url: The SQS endpoint URL.
+        :param sqs_queue_name: The name of the SQS queue.
+        :param region_name: The AWS region name.
+        :param aws_access_key_id: The AWS access key ID.
+        :param aws_secret_access_key: The AWS secret access key.
+        :param use_task_id_for_deduplication: Whether to use task ID for deduplication.
+        :param wait_time_seconds: The wait time for long polling.
+        :param max_number_of_messages: The maximum number of messages to retrieve
+        (0-10).
+        :param delay_seconds: The delay for message delivery (0-900), this will
+        configure a default.
+        :param s3_extended_bucket_name: The S3 bucket name for extended storage.
+
+        :raises BrokerInputConfigError: If the configuration is invalid.
+        """
+        super().__init__()
+
+        self._aws_region = region_name
+        self._aws_access_key_id = aws_access_key_id
+        self._aws_secret_access_key = aws_secret_access_key
+        self._aws_endpoint_url = endpoint_url
+        self._sqs_queue_name = sqs_queue_name
+        self._is_fifo_queue = True if ".fifo" in sqs_queue_name else False
+        self._sqs_queue_url: str | None = None
+        self._session = get_session()
+
+        try:
+            self.max_number_of_messages = MaxNumberOfMessages.validate_python(
+                max_number_of_messages,
+            )
+        except ValueError:
+            raise exceptions.BrokerInputConfigError(
+                attribute="MaxNumberOfMessages",
+                number=max_number_of_messages,
+            ) from None
+        try:
+            self.delay_seconds = DelaySeconds.validate_python(delay_seconds)
+        except ValueError:
+            raise exceptions.BrokerInputConfigError(
+                attribute="DelaySeconds",
+                min_number=0,
+                max_number=900,
+                number=delay_seconds,
+            ) from None
+
+        self.wait_time_seconds = wait_time_seconds
+        self.use_task_id_for_deduplication = use_task_id_for_deduplication
+        self.s3_extended_bucket_name = s3_extended_bucket_name
+
+    @contextlib.contextmanager
+    def handle_exceptions(self) -> Generator[None, None, None]:
+        """Handle exceptions raised by the SQS client."""
+        try:
+            yield
+        except ClientError as e:
+            error = e.response.get("Error", {})
+            code = error.get("Code")
+            error_message = error.get("Message")
+            if code == "AWS.SimpleQueueService.NonExistentQueue":
+                raise exceptions.QueueNotFoundError(
+                    queue_name=self._sqs_queue_name,
+                ) from e
+            elif code in ["InvalidParameterValue", "NoSuchBucket"]:
+                raise exceptions.BrokerConfigError(error=error_message) from e
+            else:
+                raise exceptions.SQSBrokerError(error=code) from e  # pragma: no cover
+
+    async def _get_s3_client(self) -> "S3Client":
+        """
+        Retrieves the S3 client, creating it if necessary.
+
+        Returns:
+            SQSClient: The initialized SQS client.
+        """
+        self._s3_client_context_creator = self._session.create_client(
+            "s3",
+            region_name=self._aws_region,
+            endpoint_url=self._aws_endpoint_url,
+            aws_access_key_id=self._aws_access_key_id,
+            aws_secret_access_key=self._aws_secret_access_key,
+        )
+        return await self._s3_client_context_creator.__aenter__()
+
+    async def _get_sqs_client(self) -> "SQSClient":
+        """
+        Retrieves the SQS client, creating it if necessary.
+
+        Returns:
+            SQSClient: The initialized SQS client.
+        """
+        self._client_context_creator = self._session.create_client(
+            "sqs",
+            region_name=self._aws_region,
+            endpoint_url=self._aws_endpoint_url,
+            aws_access_key_id=self._aws_access_key_id,
+            aws_secret_access_key=self._aws_secret_access_key,
+        )
+        return await self._client_context_creator.__aenter__()
+
+    async def _close_client(self) -> None:
+        """Closes the SQS/S3 client."""
+        await self._client_context_creator.__aexit__(None, None, None)
+        if self.s3_extended_bucket_name:
+            await self._s3_client_context_creator.__aexit__(None, None, None)
+
+    async def _get_queue_url(self) -> str:
+        if not self._sqs_queue_url:
+            with self.handle_exceptions():
+                queue_result: "GetQueueUrlResultTypeDef" = (
+                    await self._sqs_client.get_queue_url(QueueName=self._sqs_queue_name)
+                )
+                self._sqs_queue_url = queue_result["QueueUrl"]
+
+        return self._sqs_queue_url
+
+    async def startup(self) -> None:
+        """Starts the SQS broker."""
+        self._sqs_client = await self._get_sqs_client()
+        self._s3_client = await self._get_s3_client()
+        await self._get_queue_url()
+        await super().startup()
+
+    async def shutdown(self) -> None:
+        """Shuts down the SQS broker."""
+        await self._close_client()
+        await super().shutdown()
+
+    async def build_kick_kwargs(
+        self,
+        message: BrokerMessage,
+    ) -> "SendMessageRequestTypeDef":
+        """Build the kwargs for the SQS client kick method.
+
+        This function can be extended by the end user to
+        add additional kwargs in the message delivery.
+        :param message: BrokerMessage object.
+        """
+        queue_url = await self._get_queue_url()
+
+        kwargs: "SendMessageRequestTypeDef" = {
+            "QueueUrl": queue_url,
+            "MessageBody": message.message.decode("utf-8"),
+        }
+
+        if delay_seconds_raw := message.labels.get("delay", self.delay_seconds):
+            try:
+                delay_seconds = DelaySeconds.validate_python(delay_seconds_raw)
+            except ValueError:
+                raise exceptions.TaskLabelConfigError(
+                    attribute="DelaySeconds",
+                    min_number=0,
+                    max_number=900,
+                    number=delay_seconds_raw,
+                ) from None
+            else:
+                kwargs["DelaySeconds"] = delay_seconds
+
+        if self._is_fifo_queue:
+            kwargs["MessageGroupId"] = message.task_name
+            if self.use_task_id_for_deduplication:
+                kwargs["MessageDeduplicationId"] = message.task_id
+        return kwargs
+
+    async def kick(self, message: BrokerMessage) -> None:
+        """Kick tasks out from current program to configured SQS queue.
+
+        :param message: BrokerMessage object.
+        """
+        kwargs = await self.build_kick_kwargs(message)
+        with self.handle_exceptions():
+            if len(kwargs["MessageBody"]) >= constants.MAX_SQS_MESSAGE_SIZE:
+                if not self.s3_extended_bucket_name:
+                    raise exceptions.ExtendedBucketNameMissingError
+                s3_key = f"{message.task_id}.json"
+                await self._s3_client.put_object(
+                    Body=message.message,
+                    Bucket=self.s3_extended_bucket_name,
+                    Key=s3_key,
+                )
+                kwargs["MessageBody"] = json.dumps(
+                    {"s3_bucket": self.s3_extended_bucket_name, "s3_key": s3_key},
+                )
+                kwargs["MessageAttributes"] = {
+                    "s3_extended_message": {
+                        "StringValue": "True",
+                        "DataType": "String",
+                    },
+                }
+
+            await self._sqs_client.send_message(**kwargs)
+
+    def build_ack_fnx(
+        self,
+        queue_url: str,
+        receipt_handle: str,
+    ) -> Callable[[], Awaitable[None]]:
+        """
+        This method is used to build an ack for the message.
+
+        :param queue_url: queue url where the message is located
+        :param receipt_handle: message to build ack for.
+        """
+
+        async def ack() -> None:
+            with self.handle_exceptions():
+                await self._sqs_client.delete_message(
+                    QueueUrl=queue_url,
+                    ReceiptHandle=receipt_handle,
+                )
+
+        return ack
+
+    async def listen(self) -> AsyncGenerator[AckableMessage, None]:
+        """
+        This function listens to new messages and yields them.
+
+        :yield: incoming AckableMessages.
+        :return: nothing.
+        """
+        queue_url = await self._get_queue_url()
+
+        while True:
+            results = await self._sqs_client.receive_message(
+                QueueUrl=queue_url,
+                MaxNumberOfMessages=self.max_number_of_messages,
+                MessageAttributeNames=["All"],
+                WaitTimeSeconds=self.wait_time_seconds,
+            )
+            messages: list["MessageTypeDef"] = results.get("Messages", [])
+
+            for message in messages:
+                body = message.get("Body")
+                receipt_handle = message.get("ReceiptHandle")
+                attributes = message.get("MessageAttributes", {})
+                if body and receipt_handle:
+                    if attributes.get("s3_extended_message"):
+                        loaded_data = json.loads(body)
+                        s3_object = await self._s3_client.get_object(
+                            Bucket=loaded_data["s3_bucket"],
+                            Key=loaded_data["s3_key"],
+                        )
+                        async with s3_object["Body"] as s3_body:
+                            yield AckableMessage(
+                                data=await s3_body.read(),
+                                ack=self.build_ack_fnx(queue_url, receipt_handle),
+                            )
+                    else:
+                        yield AckableMessage(
+                            data=body.encode("utf-8"),
+                            ack=self.build_ack_fnx(queue_url, receipt_handle),
+                        )
