0288a8ebfe0cba75ff4cb33b8e54838cc02e8845
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:31 +0000

    Add package cibuildwheel/3.1.0/cibuildwheel-3.1.0.tar.gz

---
+++ packages/cibuildwheel/cibuildwheel-3.1.0.tar.gz/cibuildwheel-3.1.0/unit_test/wheel_print_test.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+
+import pytest
+
+from cibuildwheel.logger import BuildInfo, Logger
+from cibuildwheel.options import CommandLineArguments, Options
+
+OPTIONS_DEFAULTS = Options("linux", CommandLineArguments.defaults(), {}, defaults=True)
+FILE = Path(__file__)
+
+
+def test_printout_wheels(capsys):
+    log = Logger()
+    log.fold_mode = "disabled"
+    log.colors_enabled = False
+
+    with log.print_summary(options=OPTIONS_DEFAULTS):
+        log.summary = [
+            BuildInfo(identifier="id1", filename=None, duration=3),
+            BuildInfo(identifier="id2", filename=FILE, duration=2),
+        ]
+
+    captured = capsys.readouterr()
+    assert captured.err == ""
+
+    assert "id1" in captured.out
+    assert "id2" in captured.out
+    assert "wheels produced in" in captured.out
+    assert "SHA256=" in captured.out
+
+
+def test_no_printout_on_error(capsys):
+    log = Logger()
+    with pytest.raises(RuntimeError), log.print_summary(options=OPTIONS_DEFAULTS):
+        raise RuntimeError()
+
+    captured = capsys.readouterr()
+    assert captured.err == ""
+    assert captured.out == ""
