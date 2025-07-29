f725f20028e6d20a676d13579a370ca71fcdd6b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:40 +0000

    Add package kenshi-translator-toolkit/0.2.0/kenshi_translator_toolkit-0.2.0-py3-none-any.whl

---
+++ packages/kenshi-translator-toolkit/kenshi_translator_toolkit-0.2.0-py3-none-any.whl/kenshi_translator_toolkit/domain/entities/__init__.py
@@ -0,0 +1,5 @@
+from kenshi_translator_toolkit.domain.entities.data import Data
+from kenshi_translator_toolkit.domain.entities.dialog import Dialog
+from kenshi_translator_toolkit.domain.entities.record import Record
+
+__all__ = ['Data', 'Record', 'Dialog']
