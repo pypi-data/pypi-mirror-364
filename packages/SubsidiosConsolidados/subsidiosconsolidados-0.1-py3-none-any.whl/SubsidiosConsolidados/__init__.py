f1198f11c94fdfe459443b5a666427e5ce96a242
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:20 +0000

    Add package SubsidiosConsolidados/0.1/subsidiosconsolidados-0.1-py3-none-any.whl

---
+++ packages/SubsidiosConsolidados/subsidiosconsolidados-0.1-py3-none-any.whl/SubsidiosConsolidados/__init__.py
@@ -0,0 +1,12 @@
+from Consolidados_Subsidios import Consolidados
+
+consolidado = Consolidados()
+
+leer=consolidado.leer
+match_producto=consolidado.match_producto
+limpiar_municipios=consolidado.limpiar_municipios
+Limpieza_Transformacion=consolidado.Limpieza_Transformacion
+Asignados=consolidado.Asignados
+Reajuste=consolidado.Reajuste
+Pagados=consolidado.Pagados
+importar=consolidado.importar
