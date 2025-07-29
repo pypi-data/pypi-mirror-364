f5e86123ad087adceb40fd6b214445274d1206ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:47 +0000

    Add package lcrpm/0.0.1/lcrpm-0.0.1.tar.gz

---
+++ packages/lcrpm/lcrpm-0.0.1.tar.gz/lcrpm-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lcrpm",
+    version="0.0.1",
+    description="Lung Cancer Risk Profiling Module for OMOP CDM with time-to-event modeling",
+    author="Yusuf Brima",
+    author_email="your.email@example.com",
+    packages=find_packages(),
+    python_requires=">=3.7",
+    install_requires=[
+        "python-dotenv",
+        "sqlalchemy",
+        "psycopg2-binary",
+        "pandas",
+        "matplotlib",
+        "seaborn",
+        "lifelines",
+        "scikit-survival"
+    ],
+    include_package_data=True,
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
