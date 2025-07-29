fb69fae36b293210dfaab1e9dfbaf0f204cb5cac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:19 +0000

    Add package kimkit/1.0.2/kimkit-1.0.2.tar.gz

---
+++ packages/kimkit/kimkit-1.0.2.tar.gz/kimkit-1.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+from distutils.command.install import INSTALL_SCHEMES
+
+for scheme in INSTALL_SCHEMES.values():
+    scheme["data"] = scheme["purelib"]
+
+setup(
+    name="kimkit",
+    version="1.0.2",
+    author="Claire Waters",
+    author_email="bwaters@umn.edu",
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=["pytz", "kim_edn", "packaging", "pygments", "pymongo", "numpy"],
+    setup_requires=["pytz", "kim_edn", "packaging", "pygments", "pymongo", "numpy"],
+)
