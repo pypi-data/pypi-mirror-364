b9e024253d28a075ad2ffcedfe0d93fd8b4e31d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:41 +0000

    Add package activity-browser/3.0.0b887/activity_browser-3.0.0b887.tar.gz

---
+++ packages/activity-browser/activity_browser-3.0.0b887.tar.gz/activity_browser-3.0.0b887/activity_browser/layouts/panes/__init__.py
@@ -0,0 +1,23 @@
+from .database_explorer import DatabaseExplorerPane
+from .database_products import DatabaseProductsPane
+from .project_manager import ProjectManagerPane
+from .databases import DatabasesPane
+from .impact_categories import ImpactCategoriesPane
+from .calculation_setups import CalculationSetupsPane
+
+
+registered_panes = [
+    DatabaseExplorerPane,
+    DatabaseProductsPane,
+    ProjectManagerPane,
+    DatabasesPane,
+    ImpactCategoriesPane,
+    CalculationSetupsPane,
+]
+
+default_panes = [
+    DatabasesPane,
+    ImpactCategoriesPane,
+    CalculationSetupsPane,
+]
+
