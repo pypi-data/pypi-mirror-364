f48fa700ea56d0e79cb91c18973ba32314cca14a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:50 +0000

    Add package django-creator/0.0.8/django_creator-0.0.8.tar.gz

---
+++ packages/django-creator/django_creator-0.0.8.tar.gz/django_creator-0.0.8/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+setup(
+    name='django-creator',
+    version='0.0.8',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT',
+    description='Générateur de structure d\'app Django personnalisée',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    author='Meite',
+    author_email='meitetieba@gmail.com',
+    entry_points={
+        'console_scripts': [
+            'django-creator = django_creator.cli:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Framework :: Django',
+        'License :: OSI Approved :: MIT License',
+    ],
+    install_requires=[],
+)
