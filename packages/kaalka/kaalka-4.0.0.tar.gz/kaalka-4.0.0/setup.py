a3c3a9cb1584b97e7e2d1e3aa61edfec6f881efe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:51 +0000

    Add package kaalka/4.0.0/kaalka-4.0.0.tar.gz

---
+++ packages/kaalka/kaalka-4.0.0.tar.gz/kaalka-4.0.0/setup.py
@@ -0,0 +1,22 @@
+# setup.py
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='kaalka',
+    version='4.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'ntplib'
+    ],
+    # Metadata
+    author='PIYUSH-MISHRA-00',
+    author_email='piyushmishra.professional@gmail.com',
+    description='Kaalka Encryption Library',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/PIYUSH-MISHRA-00/Kaalka-Encryption-Algorithm/tree/python',
+    license='Custom',
+)
