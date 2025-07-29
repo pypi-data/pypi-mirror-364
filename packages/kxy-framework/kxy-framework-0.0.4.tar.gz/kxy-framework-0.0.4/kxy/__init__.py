a2185d13821a859894568c1b22f1b5040d33a1a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:35 +0000

    Add package kxy-framework/0.0.4/kxy-framework-0.0.4.tar.gz

---
+++ packages/kxy-framework/kxy-framework-0.0.4.tar.gz/kxy-framework-0.0.4/kxy/__init__.py
@@ -0,0 +1,5 @@
+from kxy.framework.base_config import BaseConfig
+from kxy.framework.base_dal import BaseDal
+from kxy.framework.base_entity import BaseEntity
+from kxy.framework.base_service import BaseService
+from kxy.framework.friendly_exception import FriendlyException
