2a650f71e98613f4f77abe85e8f35b6fef8c18af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:58 +0000

    Add package canonmap/0.3.21/canonmap-0.3.21.tar.gz

---
+++ packages/canonmap/canonmap-0.3.21.tar.gz/canonmap-0.3.21/canonmap/__init__.py
@@ -0,0 +1,16 @@
+from .core import CanonMap
+from .services.db_mysql.config import CanonMapMySQLConfig
+from .services.db_mysql.core import CanonMapMySQLClient
+from .services.db_mysql.schemas import EntityMappingRequest, EntityMappingResponse, SingleMappedEntity, TableField, FieldTransform, FieldTransformType
+
+__all__ = [
+    "CanonMap",
+    "CanonMapMySQLConfig",
+    "CanonMapMySQLClient",
+    "EntityMappingRequest",
+    "EntityMappingResponse",
+    "SingleMappedEntity",
+    "TableField",
+    "FieldTransform",
+    "FieldTransformType",
+]
