09903526ce91f8f41e6db9b9ff30893c3b172f1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:07:09 +0000

    Add package Ecode-cmd/2/ecode_cmd-2.tar.gz

---
+++ packages/Ecode-cmd/ecode_cmd-2.tar.gz/ecode_cmd-2/Ecode/test.py
@@ -0,0 +1,2 @@
+from termcolor import colored
+choices=["black", "red", "green", "yellow", "blue", "magenta", "cyan", "white", "light_grey", "dark_grey", "light_red", "light_green", "light_yellow", "light_blue","light_magenta", "light_cyan"]
