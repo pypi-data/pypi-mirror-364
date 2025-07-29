3925146f31d8887bc00ec35fcf699c9d664674fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:50 +0000

    Add package pybmdope/1.1.1/pybmdope-1.1.1.tar.gz

---
+++ packages/pybmdope/pybmdope-1.1.1.tar.gz/pybmdope-1.1.1/pybmdope/__init__.py
@@ -0,0 +1,2 @@
+from .bmdope import BMDOPE
+__version__ = "1.1.1"
---
+++ packages/pybmdope/pybmdope-1.1.1.tar.gz/pybmdope-1.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pybmdope",
+    version="1.1.1",
+    author="Rozan Ghosani",
+    author_email="ghosanirozan1@gmail.com",
+    description="A Block Metadata-Driven Order-Preserving Encryption (BMDOPE)",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/zshnrg/pybmdope",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    install_requires=["cryptography>=41.0.0"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
