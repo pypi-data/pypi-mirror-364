5246d0a8bc8b532918f3ee385f86b662459429cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:27 +0000

    Add package ust-gen/0.2.0/ust_gen-0.2.0.tar.gz

---
+++ packages/ust-gen/ust_gen-0.2.0.tar.gz/ust_gen-0.2.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ust_gen",
+    version="0.2.0",
+    description="User Story Generator plugin from requirements PDF/Excel using Azure OpenAI",
+    author="Samuel Rozario",
+    author_email="riosam005@gmail.com",
+    packages=find_packages(include=["ust_gen", "ust_gen.*"]),
+    install_requires=[
+        "openai",
+        "PyPDF2",
+        "pandas",
+        "openpyxl",
+        "python-dotenv"
+    ],
+    python_requires=">=3.8",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
