c4547d519930a39f102d98bfcbdbb436357a4b04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:06:49 +0000

    Add package wildberries-api/1.0.0/wildberries_api-1.0.0.tar.gz

---
+++ packages/wildberries-api/wildberries_api-1.0.0.tar.gz/wildberries_api-1.0.0/wb_api/feedback/const.py
@@ -0,0 +1,26 @@
+from enum import Enum
+
+
+class QuestionState(Enum):
+	REJECTED = "none"
+	ACCEPTED = "wbRu"
+	NEW = "suppliersPortalSynch"
+
+
+class FeedbackState(Enum):
+	NEW = "none"
+	PROCESSED = "wbRu"
+
+
+class FeedbackAnswerState(Enum):
+	NEW = "none"
+	ACCEPTED = "wbRu"
+	ON_REVIEW = "reviewRequired"
+	REJECTED = "rejected"
+
+
+class MatchingSize(Enum):
+	DIMENSIONLESS = ""
+	MATCH = "ok"
+	UNDERSIZED = "smaller"
+	OVERSIZED = "bigger"
