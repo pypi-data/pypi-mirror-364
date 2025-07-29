7f3956e1954e3dea6968c394301be90b5ce0e230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:06 +0000

    Add package pystages/1.3/pystages-1.3-py3-none-any.whl

---
+++ packages/pystages/pystages-1.3-py3-none-any.whl/pystages/__init__.py
@@ -0,0 +1,43 @@
+# This file is part of pystages
+#
+# pystages is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#
+# Copyright 2018 Ledger SAS, written by Olivier Hériveaux
+
+from .stage import Stage
+from .corvus import Corvus
+from .m3fs import M3FS
+from .smc100 import SMC100
+from .pi import PI, PIError, PIReferencingMethod
+from .autofocus import Autofocus
+from .vector import Vector
+from .tic import Tic, TicDirection
+from .cncrouter import CNCRouter, CNCStatus
+
+__all__ = [
+    "Stage",
+    "Corvus",
+    "M3FS",
+    "SMC100",
+    "Autofocus",
+    "Vector",
+    "Tic",
+    "TicDirection",
+    "CNCRouter",
+    "CNCStatus",
+    "PI",
+    "PIError",
+    "PIReferencingMethod",
+]
