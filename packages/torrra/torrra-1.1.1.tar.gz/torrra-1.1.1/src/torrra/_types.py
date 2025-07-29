107ebc6d08342e4065ea3ef042a0b533cc9cd92d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:48 +0000

    Add package torrra/1.1.1/torrra-1.1.1.tar.gz

---
+++ packages/torrra/torrra-1.1.1.tar.gz/torrra-1.1.1/src/torrra/_types.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+from typing import Literal, Optional
+
+
+@dataclass
+class Torrent:
+    title: str
+    size: float
+    seeders: int
+    leechers: int
+    source: str
+    magnet_uri: Optional[str]
+
+
+@dataclass
+class Provider:
+    name: Literal["Jackett", "Prowlarr"]
+    url: str
+    api_key: str
