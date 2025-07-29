586f7c5f824f0984db3daa91311f13ae91ac589a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:26 +0000

    Add package numpy-typing-compat/2.3.20250724/numpy_typing_compat-2.3.20250724.tar.gz

---
+++ packages/numpy-typing-compat/numpy_typing_compat-2.3.20250724.tar.gz/numpy_typing_compat-2.3.20250724/src/numpy_typing_compat/__init__.py
@@ -0,0 +1,22 @@
+from typing import Final, Literal
+
+__all__ = (
+    "NUMPY_GE_1_22",
+    "NUMPY_GE_1_25",
+    "NUMPY_GE_2_0",
+    "NUMPY_GE_2_1",
+    "NUMPY_GE_2_2",
+    "NUMPY_GE_2_3",
+)
+
+def __dir__() -> tuple[str, ...]:
+    return __all__
+
+
+NUMPY_GE_1_22: Final[Literal[True]] = True  # numpy >= 1.22
+NUMPY_GE_1_25: Final[Literal[True]] = True  # numpy >= 1.25
+NUMPY_GE_2_0: Final[Literal[True]] = True  # numpy >= 2.0
+NUMPY_GE_2_1: Final[Literal[True]] = True  # numpy >= 2.1
+NUMPY_GE_2_2: Final[Literal[True]] = True  # numpy >= 2.2
+NUMPY_GE_2_3: Final[Literal[True]] = True  # numpy >= 2.3
+
