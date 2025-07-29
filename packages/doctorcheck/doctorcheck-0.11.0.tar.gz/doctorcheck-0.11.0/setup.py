8640e1b0014bffa7fff8628bf3d22d8ba0bb8cfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:33 +0000

    Add package doctorcheck/0.11.0/doctorcheck-0.11.0.tar.gz

---
+++ packages/doctorcheck/doctorcheck-0.11.0.tar.gz/doctorcheck-0.11.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="doctorcheck",
+    version="0.11.0",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "django>=3.2",
+    ],
+    description="A reusable Django app for health check logic.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Keita",
+    author_email="keitamohamed1432@email.com",
+    license="MIT",
+    classifiers=[
+        "Framework :: Django",
+        "Framework :: Django :: 3.2",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
