c1af826aeccd80b70a2edc1e3bf8d3fb1cc84950
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:19:30 +0000

    Add package griffe/1.8.0/griffe-1.8.0.tar.gz

---
+++ packages/griffe/griffe-1.8.0.tar.gz/griffe-1.8.0/docs/reference/api/models.md
@@ -0,0 +1,35 @@
+# Models
+
+Griffe stores information extracted from Python source code into data models.
+
+These models represent trees of objects, starting with modules, and containing classes, functions, and attributes.
+
+Modules can have submodules, classes, functions and attributes. Classes can have nested classes, methods and attributes. Functions and attributes do not have any members.
+
+Indirections to objects declared in other modules are represented as "aliases". An alias therefore represents an imported object, and behaves almost exactly like the object it points to: it is a light wrapper around the object, with special methods and properties that allow to access the target's data transparently.
+
+The 5 models:
+
+- [`Module`][griffe.Module]
+- [`Class`][griffe.Class]
+- [`Function`][griffe.Function]
+- [`Attribute`][griffe.Attribute]
+- [`Alias`][griffe.Alias]
+
+## **Model kind enumeration**
+
+::: griffe.Kind
+
+## **Models base classes**
+
+::: griffe.GetMembersMixin
+
+::: griffe.SetMembersMixin
+
+::: griffe.DelMembersMixin
+
+::: griffe.SerializationMixin
+
+::: griffe.ObjectAliasMixin
+
+::: griffe.Object
---
+++ packages/griffe/griffe-1.8.0.tar.gz/griffe-1.8.0/tests/test_encoders.py
@@ -0,0 +1,57 @@
+"""Tests for the `encoders` module."""
+
+from __future__ import annotations
+
+import json
+
+import pytest
+from jsonschema import ValidationError, validate
+
+from griffe import Function, GriffeLoader, Module, Object
+
+
+def test_minimal_data_is_enough() -> None:
+    """Test serialization and de-serialization.
+
+    This is an end-to-end test that asserts
+    we can load back a serialized tree and
+    infer as much data as within the original tree.
+    """
+    loader = GriffeLoader()
+    module = loader.load("griffe")
+    minimal = module.as_json(full=False)
+    full = module.as_json(full=True)
+    reloaded = Module.from_json(minimal)
+    assert reloaded.as_json(full=False) == minimal
+    assert reloaded.as_json(full=True) == full
+
+    # Also works (but will result in a different type hint).
+    assert Object.from_json(minimal)
+
+    # Won't work if the JSON doesn't represent the type requested.
+    with pytest.raises(TypeError, match="provided JSON object is not of type"):
+        Function.from_json(minimal)
+
+
+# use this function in test_json_schema to ease schema debugging
+def _validate(obj: dict, schema: dict) -> None:
+    if "members" in obj:
+        for member in obj["members"]:
+            _validate(member, schema)
+
+    try:
+        validate(obj, schema)
+    except ValidationError:
+        print(obj["path"])  # noqa: T201
+        raise
+
+
+def test_json_schema() -> None:
+    """Assert that our serialized data matches our JSON schema."""
+    loader = GriffeLoader()
+    module = loader.load("griffe")
+    loader.resolve_aliases()
+    data = json.loads(module.as_json(full=True))
+    with open("docs/schema.json") as f:  # noqa: PTH123
+        schema = json.load(f)
+    validate(data, schema)
