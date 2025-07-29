d4a4da78ff2c0bfc49a21a2602685c833948bd37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:02 +0000

    Add package invenio-subjects-lcsh/2025.6.4.2/invenio_subjects_lcsh-2025.6.4.2-py3-none-any.whl

---
+++ packages/invenio-subjects-lcsh/invenio_subjects_lcsh-2025.6.4.2-py3-none-any.whl/invenio_subjects_lcsh/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021-2024 Northwestern University.
+#
+# invenio-subjects-lcsh is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""LCSH subject terms for InvenioRDM."""
