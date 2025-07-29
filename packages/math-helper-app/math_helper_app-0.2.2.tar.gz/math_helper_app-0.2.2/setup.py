ac96eeac4efbe31ecb04b7604f36f7bac744fa8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:19:27 +0000

    Add package math-helper-app/0.2.2/math_helper_app-0.2.2.tar.gz

---
+++ packages/math-helper-app/math_helper_app-0.2.2.tar.gz/math_helper_app-0.2.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='math-helper-app',
+    version='0.2.2',
+    packages=find_packages(),
+    description='Package simple pour vérifier si un nombre est pair ou impair',
+    author='christellelou904',
+    author_email='christellelou4@gmail.com',
+    entry_points={
+        'console_scripts': [
+            'math-helper-app = app_math.cli:main',   # commande CLI à lancer dans le terminal
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+    ],
+    install_requires=[],
+)
