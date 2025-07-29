1cb39d7695a4aa6e6102eafde3a5f9659181943d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:07:04 +0000

    Add package monpackage-addition/0.2.0/monpackage_addition-0.2.0.tar.gz

---
+++ packages/monpackage-addition/monpackage_addition-0.2.0.tar.gz/monpackage_addition-0.2.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='monpackage-addition',
+    version='0.2.0',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'addition-cli=monpackage.cli:main',
+        ],
+    },
+    description='Package avec addition et CLI',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='ParfaiteSekongo',
+    author_email='p88971582@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.6',
+)
