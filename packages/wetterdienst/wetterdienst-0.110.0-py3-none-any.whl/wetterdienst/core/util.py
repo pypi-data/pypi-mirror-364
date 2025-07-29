ec2160a9ca54f24b1a44a8f31fc1fa98ab2b28f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:03 +0000

    Add package wetterdienst/0.110.0/wetterdienst-0.110.0-py3-none-any.whl

---
+++ packages/wetterdienst/wetterdienst-0.110.0-py3-none-any.whl/wetterdienst/core/util.py
@@ -0,0 +1,74 @@
+# Copyright (C) 2018-2025, earthobservations developers.
+# Distributed under the MIT License. See LICENSE for more info.
+"""Tools for timeseries."""
+
+from __future__ import annotations
+
+import polars as pl
+
+
+class _ParameterData:
+    def __init__(
+        self,
+        values: pl.DataFrame,
+        station_ids: list[str] | None = None,
+        extra_station_counter: int = 0,
+    ) -> None:
+        self.station_ids = station_ids or []
+        self.extra_station_counter = extra_station_counter
+        self.values = values
+        self.finished = False
+
+
+def extract_station_values(
+    param_data: _ParameterData,
+    result_series_param: pl.Series,
+    *,
+    valid_station_groups_exists: bool,
+) -> None:
+    """Extract station values."""
+    # Three rules:
+    # 1. only add further stations if not a minimum of 4 stations is reached OR
+    # 2. a gain of 10% of timestamps with at least 4 existing values over all stations is seen OR
+    # 3. an additional stations_counter is below 3 (used if a station has really no or few values)
+    cond1 = param_data.values.shape[1] < 4
+    cond2 = not cond1 and gain_of_value_pairs(param_data.values, result_series_param) > 0.10
+    if (
+        not valid_station_groups_exists or cond1 or cond2 or param_data.extra_station_counter < 3
+    ):  # timestamps + 4 stations
+        if not (cond1 or cond2):
+            param_data.extra_station_counter += 1
+        # "S" is added to station id titles to prevent bug with pandas that somehow doesn't allow column name "02000"
+        # under certain circumstances
+        param_data.values = param_data.values.with_columns(
+            pl.lit(result_series_param).alias(f"S{result_series_param.name}"),
+        )
+    else:
+        param_data.finished = True
+
+
+def gain_of_value_pairs(old_values: pl.DataFrame, new_values: pl.Series) -> float:
+    """Calculate the gain of value pairs.
+
+    The gain of value pairs is calculated by the following formula:
+
+    number of value pairs with at least 4 values in old_values and new_values /
+    number of value pairs with at least 4 values in old_values - 1
+
+    """
+    old_score = (
+        old_values.select(pl.fold(acc=0, function=lambda acc, s: acc + s.is_not_null(), exprs=pl.all()) >= 4)
+        .sum()
+        .item()
+    )  # 5: dates plus 4 values
+    old_values = old_values.with_columns(pl.lit(new_values).alias(f"S{new_values.name}"))
+    new_score = (
+        old_values.select(pl.fold(acc=0, function=lambda acc, s: acc + s.is_not_null(), exprs=pl.all()) >= 4)
+        .sum()
+        .item()
+    )  # 5: dates plus 4 values
+    if old_score == 0:
+        if new_score == 0:
+            return 0.0
+        return 1.0
+    return new_score / old_score - 1.0
---
+++ packages/wetterdienst/wetterdienst-0.110.0-py3-none-any.whl/wetterdienst/settings.py
@@ -0,0 +1,118 @@
+# Copyright (C) 2018-2025, earthobservations developers.
+# Distributed under the MIT License. See LICENSE for more info.
+"""Settings for the wetterdienst package."""
+
+from __future__ import annotations
+
+import json
+import logging
+import platform
+from pathlib import Path  # noqa: TC003
+from typing import Literal
+
+import platformdirs
+from pydantic import Field, field_validator, model_validator
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from wetterdienst.metadata.parameter import Parameter
+from wetterdienst.model.unit import UnitConverter
+
+log = logging.getLogger(__name__)
+
+_UNIT_CONVERTER_TARGETS = UnitConverter().targets.keys()
+
+
+class Settings(BaseSettings):
+    """Settings for the wetterdienst package."""
+
+    model_config = SettingsConfigDict(env_ignore_empty=True, env_prefix="WD_")
+
+    cache_disable: bool = Field(default=False)
+    cache_dir: Path = Field(default_factory=lambda: platformdirs.user_cache_dir(appname="wetterdienst"))
+    fsspec_client_kwargs: dict = Field(
+        default_factory=lambda: {
+            "headers": {"User-Agent": f"wetterdienst/{__import__('wetterdienst').__version__} ({platform.system()})"},
+        },
+    )
+    ts_humanize: bool = True
+    ts_shape: Literal["wide", "long"] = "long"
+    ts_convert_units: bool = True
+    ts_unit_targets: dict[str, str] = Field(default_factory=dict)
+    ts_skip_empty: bool = False
+    ts_skip_threshold: float = 0.95
+    ts_skip_criteria: Literal["min", "mean", "max"] = "min"
+    ts_complete: bool = False
+    ts_drop_nulls: bool = True
+    ts_interpolation_station_distance: dict[str, float] = Field(
+        default_factory=lambda: {
+            "default": 40.0,
+            Parameter.PRECIPITATION_HEIGHT.value.lower(): 20.0,
+        },
+    )
+    ts_interpolation_use_nearby_station_distance: float = 1.0
+
+    @field_validator("ts_unit_targets", mode="before")
+    @classmethod
+    def validate_ts_unit_targets_before(cls, values: dict[str, str] | None) -> dict[str, str]:
+        """Validate the unit targets."""
+        return values or {}
+
+    @field_validator("ts_unit_targets", mode="after")
+    @classmethod
+    def validate_ts_unit_targets_after(cls, values: dict[str, str]) -> dict[str, str]:
+        """Validate the unit targets."""
+        if not values.keys() <= _UNIT_CONVERTER_TARGETS:
+            msg = f"Invalid unit targets: one of {set(values.keys())} not in {set(_UNIT_CONVERTER_TARGETS)}"
+            raise ValueError(msg)
+        return values
+
+    # make ts_interpolation_station_distance update but not replace the default values
+    @field_validator("ts_interpolation_station_distance", mode="before")
+    @classmethod
+    def validate_ts_interpolation_station_distance(cls, values: dict[str, float] | None) -> dict[str, float]:
+        """Validate the interpolation station distance settings."""
+        default = cls.model_fields["ts_interpolation_station_distance"].default_factory()
+        if not values:
+            return default
+        return default | values
+
+    @property
+    def ts_tidy(self) -> bool:
+        """Return whether the time series is in tidy format."""
+        return self.ts_shape == "long"
+
+    @model_validator(mode="after")
+    def validate(self) -> Settings:
+        """Validate the settings."""
+        if self.ts_shape != "long":
+            self.ts_drop_nulls = False
+            log.info(
+                "option 'ts_drop_nulls' is only available with option 'ts_shape=long' and "
+                "is thus ignored in this request.",
+            )
+        if self.ts_drop_nulls:
+            self.ts_complete = False
+            log.info(
+                "option 'ts_complete' is only available with option 'ts_drop_nulls=False' and "
+                "is thus ignored in this request.",
+            )
+        # skip empty stations
+        if not self.ts_complete:
+            self.ts_skip_empty = False
+            log.info(
+                "option 'skip_empty' is only available with options `ts_drop_nulls=False` and 'ts_complete=True' "
+                "and is thus ignored in this request.",
+            )
+        if self.cache_disable:
+            log.info("Wetterdienst cache is disabled")
+        else:
+            log.info(f"Wetterdienst cache is enabled [CACHE_DIR:{self.cache_dir}]")
+        return self
+
+    def __repr__(self) -> str:
+        """Return the settings as a JSON string."""
+        return json.dumps(self.model_dump(mode="json"))
+
+    def __str__(self) -> str:
+        """Return the settings as a string."""
+        return f"""Settings({json.dumps(self.model_dump(mode="json"), indent=4)})"""
