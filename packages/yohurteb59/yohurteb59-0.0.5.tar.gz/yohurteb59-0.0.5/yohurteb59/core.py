aebdf18063cbc878717dadbd0e43bd377b6d2d6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:47 +0000

    Add package yohurteb59/0.0.5/yohurteb59-0.0.5.tar.gz

---
+++ packages/yohurteb59/yohurteb59-0.0.5.tar.gz/yohurteb59-0.0.5/yohurteb59/core.py
@@ -0,0 +1,25 @@
+def ft_tqdm(lst: range) -> None:
+    """
+    Make a loading bar for more visual
+
+    Args:
+        lst (range): the range wich be travaled
+
+    Returns:
+        None: This function does not return a value.
+    """
+    try:
+        for i, elem in enumerate(lst):
+            progr = int(i) + 1
+            total = int(len(lst))
+            percent = int((progr / total) * 100)
+            bar_length = 98
+            progr_length = int(bar_length * progr // total)
+
+            bar = "█" * progr_length
+            bar = bar.ljust(bar_length, "=")
+
+            print(f"\r{percent}%|{bar}| {progr}/{total}", end="", flush=True)
+            yield elem
+    except Exception as err:
+        print("Error ->", err)
