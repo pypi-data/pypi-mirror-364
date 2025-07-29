098ac1c8b39365ed819af29db8c6c5dc2cf52226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:34 +0000

    Add package pelmesha/0.1.6/pelmesha-0.1.6.tar.gz

---
+++ packages/pelmesha/pelmesha-0.1.6.tar.gz/pelmesha-0.1.6/src/pelmesha/__init__.py
@@ -0,0 +1,9 @@
+"""
+pelmesha
+
+pelmesha is a Python library for loading, preprocessing, processing, storage and extracting peaklists of Mass Spectrometry Imaging (MSI) data.
+"""
+# from sys import version_info
+# if version_info < 3.11
+__author__ = 'Andrey Kuzin'
+__credits__ = 'Moscow Institue of Physics and Technology'
