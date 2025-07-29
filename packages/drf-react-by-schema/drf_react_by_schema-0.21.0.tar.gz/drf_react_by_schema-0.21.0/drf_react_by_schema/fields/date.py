ddb5ad5c2c71cd571ec5079babb51f5c61806e21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:18 +0000

    Add package drf-react-by-schema/0.21.0/drf_react_by_schema-0.21.0.tar.gz

---
+++ packages/drf-react-by-schema/drf_react_by_schema-0.21.0.tar.gz/drf_react_by_schema-0.21.0/drf_react_by_schema/fields/date.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class DateField(models.DateField):
+    description = "Extended DateField"
+
+    def __init__(self, *args, views=None, ui_required=False, **kwargs):
+        super().__init__(*args, **kwargs)
+        if views:
+            self.views = views
+        self.ui_required = ui_required
+
+
+class DateTimeField(models.DateTimeField):
+    description = "Extended DateTimeField"
+
+    def __init__(self, *args, ui_required=False, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.ui_required = ui_required
