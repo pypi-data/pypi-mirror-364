d3cbadb574515c51b6bae57c0acb2ef546b56153
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:52 +0000

    Add package simulation-mpc-core/0.2.3/simulation_mpc_core-0.2.3.tar.gz

---
+++ packages/simulation-mpc-core/simulation_mpc_core-0.2.3.tar.gz/simulation_mpc_core-0.2.3/simulations/Cbc-refactor-win64-msvc14-mt/include/coin-or/CbcConfig.h
@@ -0,0 +1,25 @@
+/* src/config_cbc.h.  Generated from config_cbc.h.in by configure.  */
+/* src/config_cbc.h.in. */
+
+#ifndef __CONFIG_CBC_H__
+#define __CONFIG_CBC_H__
+
+/* Library Visibility Attribute */
+#define CBCLIB_EXPORT 
+
+/* Version number of project */
+#define CBC_VERSION "devel"
+
+/* Major Version number of project */
+#define CBC_VERSION_MAJOR 9999
+
+/* Minor Version number of project */
+#define CBC_VERSION_MINOR 9999
+
+/* Release Version number of project */
+#define CBC_VERSION_RELEASE 9999
+
+/* Define to 1 if Nauty is available. */
+/* #undef CBC_HAS_NAUTY */
+
+#endif
