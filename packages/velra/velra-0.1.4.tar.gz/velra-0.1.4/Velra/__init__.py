768248001a33bafe608f49cc3136e1dd5560fff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:24 +0000

    Add package velra/0.1.4/velra-0.1.4.tar.gz

---
+++ packages/velra/velra-0.1.4.tar.gz/velra-0.1.4/Velra/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from .core import custom_render
+from .loader import custom_html_loader
+
+class VelraApp:
+    def __init__(self, import_name):
+        self.app = Flask(import_name)
+        
+        # Use your custom HTML loader for templates
+        self.app.jinja_loader = custom_html_loader()
+        
+        self._setup_routes()
+
+    def _setup_routes(self):
+        @self.app.route('/')
+        def home():
+            return "Welcome to Velra-powered Flask app!"
+
+    def render(self, template_name, **context):
+        # Use your custom render function here
+        return custom_render(template_name, **context)
+
+    def route(self, *args, **kwargs):
+        return self.app.route(*args, **kwargs)
+
+    def run(self, *args, **kwargs):
+        self.app.run(*args, **kwargs)
