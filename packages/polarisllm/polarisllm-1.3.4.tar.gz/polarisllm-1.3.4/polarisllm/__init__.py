8010164c510dacf8b424665c2f910a1736b3418f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:50 +0000

    Add package polarisllm/1.3.4/polarisllm-1.3.4.tar.gz

---
+++ packages/polarisllm/polarisllm-1.3.4.tar.gz/polarisllm-1.3.4/polarisllm/__init__.py
@@ -0,0 +1,11 @@
+"""
+PolarisLLM Runtime Engine Package
+"""
+
+__version__ = "1.3.4"
+__author__ = "PolarisLLM Team"
+__email__ = "elon@polariscloud.ai"
+
+from .main import sync_main as main
+
+__all__ = ["main"]
