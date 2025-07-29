d68de0502ad15a2326b027af8c6890c4875ff2cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:26 +0000

    Add package bluecore-models/0.8.0/bluecore_models-0.8.0-py3-none-any.whl

---
+++ packages/bluecore-models/bluecore_models-0.8.0-py3-none-any.whl/bluecore_models/models/instance.py
@@ -0,0 +1,80 @@
+"""Module for BIBFRAME Instances"""
+
+from typing import Optional
+
+from sqlalchemy import (
+    event,
+    insert,
+    ForeignKey,
+    Integer,
+)
+
+from sqlalchemy.orm import (
+    mapped_column,
+    Mapped,
+    relationship,
+)
+
+from bluecore_models.models.resource import ResourceBase
+from bluecore_models.models.work import Work
+from bluecore_models.models.version import Version
+from bluecore_models.utils.db import add_bf_classes, update_bf_classes
+from bluecore_models.utils.graph import frame_jsonld
+
+
+class Instance(ResourceBase):
+    __tablename__ = "instances"
+
+    id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("resource_base.id"), primary_key=True
+    )
+    work_id: Mapped[int] = mapped_column(Integer, ForeignKey("works.id"), nullable=True)
+    work: Mapped["Work"] = relationship(
+        "Work", foreign_keys=work_id, backref="instances"
+    )
+
+    __mapper_args__ = {
+        "polymorphic_identity": "instances",
+    }
+
+    def __repr__(self):
+        return f"<Instance {self.uri}>"
+
+
+@event.listens_for(Instance.data, "set", propagate=True, retval=True)
+def set_jsonld(target, value, oldvalue, initiator) -> Optional[dict]:
+    """
+    Ensure that JSON-LD data is framed prior to persisting it to the database.
+    """
+    if value is not None:
+        return frame_jsonld(target.uri, value)
+    else:
+        return None
+
+
+@event.listens_for(Instance, "after_insert")
+def create_version_bf_classes(mapper, connection, target):
+    """
+    Creates a Version and associated Bibframe Classes
+    """
+    stmt = insert(Version.__table__).values(
+        resource_id=target.id,
+        data=target.data,
+        created_at=target.updated_at,
+    )
+    connection.execute(stmt)
+    add_bf_classes(connection, target)
+
+
+@event.listens_for(Instance, "after_update")
+def update_version_bf_classes(mapper, connection, target):
+    """
+    Updates a Version and associated Bibframe Classes
+    """
+    stmt = insert(Version.__table__).values(
+        resource_id=target.id,
+        data=target.data,
+        created_at=target.updated_at,
+    )
+    connection.execute(stmt)
+    update_bf_classes(connection, target)
