2cb62bbf1ad7f63803f9b111ec28efa351f82c4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:01 +0000

    Add package ecodev-front/0.0.90/ecodev_front-0.0.90.tar.gz

---
+++ packages/ecodev-front/ecodev_front-0.0.90.tar.gz/ecodev_front-0.0.90/ecodev_front/documentation.py
@@ -0,0 +1,34 @@
+import dash_mantine_components as dmc
+from dash import html
+from dash_iconify import DashIconify
+
+from ecodev_front.nav_items import action_item
+from ecodev_front.constants import TYPE, INDEX
+from ecodev_front.ids import DOCUMENTATION, ACTION_ITEM
+from ecodev_front.text import subtitle
+
+
+def documentation_icon_link(icon_width: int = 30,
+                            icon_color: str = '#8BC1E3') -> html.Div:
+    """
+    Renders the documentation action item to go to the documentation section
+    """
+    return action_item(id={TYPE: ACTION_ITEM, INDEX: DOCUMENTATION},
+                       label=DOCUMENTATION,
+                       icon='bxs:book',
+                       href=f'/{DOCUMENTATION}',
+                       icon_color=icon_color,
+                       icon_width=icon_width)
+
+
+def documentation_text(text: str = """If this is your first time using this tool, we
+                    recommend you read through the documentation.""",
+                       icon_width: int = 50) -> dmc.Group:
+    """
+    Renders the text accompanying the documentation button
+    """
+
+    return dmc.Group([
+        subtitle(text, ta='left'),
+        documentation_icon_link(icon_width)
+    ], justify='space-around')
