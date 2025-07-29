72dbf5f8336267851cc5d06030cb420cbce8fd56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:51 +0000

    Add package SigProfilerClusters/1.2.2/SigProfilerClusters-1.2.2-py3-none-any.whl

---
+++ packages/SigProfilerClusters/SigProfilerClusters-1.2.2-py3-none-any.whl/SigProfilerClusters/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILECLUSTERS SETUP.PY
+short_version = '1.2.2'
+version = '1.2.2'
+Update = 'v1.2.2: Updates to variant_caller parameter and plotting improvements'
+	
+	
