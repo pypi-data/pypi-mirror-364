38e2d3eadc679ac284dc7b3e694c4eb3969d0917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:06 +0000

    Add package fortinet-wrapper/0.2.0/fortinet_wrapper-0.2.0.tar.gz

---
+++ packages/fortinet-wrapper/fortinet_wrapper-0.2.0.tar.gz/fortinet_wrapper-0.2.0/setup.py
@@ -0,0 +1,27 @@
+"""Setup script for the Fortinet wrapper package."""
+
+from setuptools import find_packages, setup
+
+setup(
+    name="fortinet_wrapper",
+    version="0.2.0",
+    description="A Python wrapper for Fortinet API",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="HBNet Networks",
+    author_email="dev@hbnet.tech",
+    url="https://github.com/HBNetNetworks/fortinet-wrapper",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "requests>=2.32.4",
+        # Add other runtime dependencies here
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+    license="GPLv3",
+)
