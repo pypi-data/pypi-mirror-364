cde0e6ab96c71929c8e1ad1e6fa3f7cc7df04ffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:51 +0000

    Add package tencentcloud-sdk-python-intl-en/3.0.1243/tencentcloud_sdk_python_intl_en-3.0.1243-py2.py3-none-any.whl

---
+++ packages/tencentcloud-sdk-python-intl-en/tencentcloud_sdk_python_intl_en-3.0.1243-py2.py3-none-any.whl/tencentcloud/billing/v20180709/errorcodes.py
@@ -0,0 +1,72 @@
+# -*- coding: utf8 -*-
+# Copyright (c) 2017-2025 Tencent. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# No permission.
+AUTHFAILURE_UNAUTHORIZEDOPERATION = 'AuthFailure.UnauthorizedOperation'
+
+# Operation failed.
+FAILEDOPERATION = 'FailedOperation'
+
+# Invalid App ID.
+FAILEDOPERATION_INVALIDAPPID = 'FailedOperation.InvalidAppId'
+
+# The quantity exceeds the maximum limit.
+FAILEDOPERATION_NUMLIMITERROR = 'FailedOperation.NumLimitError'
+
+# Payment failed. Please contact Tencent Cloud to resolve this issue.
+FAILEDOPERATION_PAYPRICEERROR = 'FailedOperation.PayPriceError'
+
+# Failed to get the number of data entries.
+FAILEDOPERATION_QUERYCOUNTFAILED = 'FailedOperation.QueryCountFailed'
+
+# Summary is being built. Please try again later.
+FAILEDOPERATION_SUMMARYDATANOTREADY = 'FailedOperation.SummaryDataNotReady'
+
+# This cost allocation tag key does not exist.
+FAILEDOPERATION_TAGKEYNOTEXIST = 'FailedOperation.TagKeyNotExist'
+
+# Internal error.
+INTERNALERROR = 'InternalError'
+
+# Database operation failed.
+INTERNALERROR_DBOPERATERROR = 'InternalError.DbOperatError'
+
+# Gateway error.
+INTERNALERROR_GATEWAYERROR = 'InternalError.GatewayError'
+
+# An internal system error occurred.
+INTERNALERROR_INTERNALERROR = 'InternalError.InternalError'
+
+# Undefined exception.
+INTERNALERROR_UNKNOWNERROR = 'InternalError.UnknownError'
+
+# Invalid parameter.
+INVALIDPARAMETER = 'InvalidParameter'
+
+# A parameter error occurred.
+INVALIDPARAMETER_INVALIDPARAMETER = 'InvalidParameter.InvalidParameter'
+
+# Invalid parameter value.
+INVALIDPARAMETERVALUE = 'InvalidParameterValue'
+
+# Operation denied.
+OPERATIONDENIED = 'OperationDenied'
+
+# The account does not have CAM permission.
+UNAUTHORIZEDOPERATION_CAMNOAUTH = 'UnauthorizedOperation.CamNoAuth'
+
+# Operation unsupported.
+UNSUPPORTEDOPERATION = 'UnsupportedOperation'
