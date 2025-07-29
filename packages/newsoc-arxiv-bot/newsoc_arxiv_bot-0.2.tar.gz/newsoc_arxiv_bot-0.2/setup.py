ebb9c0ad50cfb19c5157ca1bc01a084f49cf5144
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:54 +0000

    Add package newsoc-arxiv-bot/0.2/newsoc_arxiv_bot-0.2.tar.gz

---
+++ packages/newsoc-arxiv-bot/newsoc_arxiv_bot-0.2.tar.gz/newsoc_arxiv_bot-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup  
+
+setup(  
+    name="newsoc-arxiv-bot",  
+    version="0.2",  
+    packages=["newsoc_arxiv_bot"],  
+    entry_points={"console_scripts": ["arxiv-bot=newsoc_arxiv_bot.arxiv_alert:main"]},  
+    install_requires=["requests", "python-telegram-bot", "pydantic", "beautifulsoup4", "lxml"]  
+)  
