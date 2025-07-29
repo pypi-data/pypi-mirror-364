1a410c8bfcfe09d0336da9c68046aa38fc96f916
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:41 +0000

    Add package cmaes/0.12.0/cmaes-0.12.0-py3-none-any.whl

---
+++ packages/cmaes/cmaes-0.12.0-py3-none-any.whl/cmaes/__init__.py
@@ -0,0 +1,11 @@
+from ._cma import CMA  # NOQA
+from ._sepcma import SepCMA  # NOQA
+from ._warm_start import get_warm_start_mgd  # NOQA
+from ._cmawm import CMAwM  # NOQA
+from ._xnes import XNES  # NOQA
+from ._dxnesic import DXNESIC  # NOQA
+from ._catcma import CatCMA  # NOQA
+from ._mapcma import MAPCMA  # NOQA
+from ._catcmawm import CatCMAwM  # NOQA
+
+__version__ = "0.12.0"
