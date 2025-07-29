e08d36b836b1dd79ffe0c5752429dfe83cba9e47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:49 +0000

    Add package impactx-noacc/25.7/impactx_noacc-25.7-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/impactx-noacc/impactx_noacc-25.7-cp311-cp311-macosx_11_0_arm64.whl/impactx/extensions/KnownElementsList.pyi
@@ -0,0 +1,17 @@
+"""
+
+This file is part of ImpactX
+
+Copyright 2025 ImpactX contributors
+Authors: Axel Huebl
+License: BSD-3-Clause-LBNL
+"""
+
+from __future__ import annotations
+
+__all__ = ["register_KnownElementsList_extension"]
+
+def register_KnownElementsList_extension(kel):
+    """
+    KnownElementsList helper methods
+    """
