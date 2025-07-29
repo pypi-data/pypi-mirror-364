2c082af992bfdd9e06b6d63112db51ff857dd5d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:59 +0000

    Add package EzQt-App/2.0.1/ezqt_app-2.0.1-py3-none-any.whl

---
+++ packages/EzQt-App/ezqt_app-2.0.1-py3-none-any.whl/ezqt_app/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# ///////////////////////////////////////////////////////////////
+
+"""
+EzQt package initialization.
+"""
+__version__ = "2.0.1"
+
+# CLI
+from .cli import *
+
+# MAIN
+from .main import *
+
+# APP
+from .app import *
+
+# KERNEL
+from .kernel import *
+
+# WIDGETS
+from .widgets import *
---
+++ packages/EzQt-App/ezqt_app-2.0.1-py3-none-any.whl/ezqt_app/kernel/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# ///////////////////////////////////////////////////////////////
+
+# MAINWINDOW
+from .ui_main import Ui_MainWindow
+
+# UI FUNCTIONS
+from .ui_functions import UIFunctions
+
+# APP FUNCTIONS
+from .app_functions import Kernel
+
+# APP SETTINGS
+from .app_settings import Settings
+
+# APP RESOURCES
+from .app_components import Fonts, SizePolicy
+from .app_resources import *
