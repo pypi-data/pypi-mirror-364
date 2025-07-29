5ef8fd932b1ba4fd68626e3a2f0eb912c8059d80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:52 +0000

    Add package niaarm/0.4.3/niaarm-0.4.3.tar.gz

---
+++ packages/niaarm/niaarm-0.4.3.tar.gz/niaarm-0.4.3/niaarm/__init__.py
@@ -0,0 +1,12 @@
+from niaarm.niaarm import NiaARM
+from niaarm.dataset import Dataset
+from niaarm.rule import Rule
+from niaarm.feature import Feature
+from niaarm.mine import get_rules
+from niaarm.rule_list import RuleList
+from niaarm.preprocessing import squash
+
+
+__all__ = ["NiaARM", "Dataset", "Feature", "Rule", "RuleList", "get_rules", "squash"]
+
+__version__ = "0.4.3"
