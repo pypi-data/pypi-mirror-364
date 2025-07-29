0d1ca94ec6c151ac2f4fba4f0af0a71430668bb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:54 +0000

    Add package spatialsurf/1.9/spatialsurf-1.9.tar.gz

---
+++ packages/spatialsurf/spatialsurf-1.9.tar.gz/spatialsurf-1.9/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="spatialsurf",
+    version="1.9",
+    author="Shuyu Liang",
+    author_email="",
+    description="A self-supervised deep learning method for reference-free deconvolution.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lllsssyyyy/SURF",
+    packages=setuptools.find_packages(),
+    install_requires=['pandas==1.5.3', 'numpy==1.23.0', 'scanpy==1.9.8', 'scipy==1.9.1',  'rpy2', 'tables', 'seaborn==0.13.2', 'matplotlib==3.7.1', 'pyarrow==14.0.1'],
+    entry_points={
+        'console_scripts': [
+            'SURF=SURF:main'
+        ],
+    },
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
