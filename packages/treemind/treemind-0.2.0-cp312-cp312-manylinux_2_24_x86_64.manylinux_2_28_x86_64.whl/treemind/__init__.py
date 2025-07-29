73581d3ca11c047dec2a96a278340e18c3d70d85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:32 +0000

    Add package treemind/0.2.0/treemind-0.2.0-cp312-cp312-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/treemind/treemind-0.2.0-cp312-cp312-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl/treemind/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.0"
+__author__ = "Samet Çopur"
+__email__ = "sametcopur@yahoo.com"
+__license__ = "MIT"
+
+
+from .algorithm import Explainer, Result
+
+__all__ = ["Explainer", "Result"]
+
