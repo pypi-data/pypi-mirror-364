2bf0f553f468343da192b386859bf2ff0ad3fdbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:19:36 +0000

    Add package CodeMutator/0.2.0/codemutator-0.2.0.tar.gz

---
+++ packages/CodeMutator/codemutator-0.2.0.tar.gz/codemutator-0.2.0/mutator/__version__.py
@@ -0,0 +1,6 @@
+"""Version information for the Mutator Framework."""
+
+__version__ = "0.2.0"
+__author__ = "Mutator Framework Team"
+__email__ = "info@codemutator.com"
+__description__ = "A comprehensive Python framework for building coding agents using GenAI" 
