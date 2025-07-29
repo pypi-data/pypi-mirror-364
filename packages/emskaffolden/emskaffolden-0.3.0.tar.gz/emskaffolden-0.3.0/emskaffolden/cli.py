baf4c943d343845b51e8679f6c86df4c64bff21c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:41 +0000

    Add package emskaffolden/0.3.0/emskaffolden-0.3.0.tar.gz

---
+++ packages/emskaffolden/emskaffolden-0.3.0.tar.gz/emskaffolden-0.3.0/emskaffolden/cli.py
@@ -0,0 +1,38 @@
+def augment_parser(parser):
+    """
+    Takes the Emrichen argument parser and makes an Emskaffolden argument
+    parser out of it.
+    """
+    parser.add_argument(
+        '--environment-name',
+        '-E',
+        default="development",
+        help=(
+            'Name of the environment. Exported as a var called "environment". Also if a '
+            'var file called kubernetes/<ENV NAME>.vars.yaml exists, it is included as a var file. '
+            'If your environment specific var files are elsewhere, use -f instead.'
+        ),
+    )
+
+    parser.add_argument(
+        '--skaffold-file',
+        '-F',
+        type=str,
+        default='skaffold.in.yaml',
+        help="Skaffold configuration file template",
+    )
+
+    parser.add_argument(
+        '--builtin-template',
+        '-T',
+        type=str,
+        nargs='?',
+        help="Instead of kubernetes/template.in.yaml, use one of the built-in templates",
+    )
+
+    parser.add_argument(
+        'skaffold_args',
+        nargs='*',
+        type=str,
+        help='Arguments to Skaffold',
+    )
