a1663da810cf0f492c3968b4cf6f1b0c2939c3c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:21 +0000

    Add package multipers/2.3.3b6/multipers-2.3.3b6-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/multipers/multipers-2.3.3b6-cp310-cp310-macosx_11_0_arm64.whl/multipers/array_api/numpy.py
@@ -0,0 +1,41 @@
+from contextlib import nullcontext
+
+import numpy as _np
+from scipy.spatial.distance import cdist
+
+backend = _np
+cat = _np.concatenate
+norm = _np.linalg.norm
+astensor = _np.asarray
+asnumpy = _np.asarray
+tensor = _np.array
+stack = _np.stack
+empty = _np.empty
+where = _np.where
+no_grad = nullcontext
+zeros = _np.zeros
+min = _np.min
+max = _np.max
+repeat_interleave = _np.repeat
+cdist = cdist  # type: ignore[no-redef]
+unique = _np.unique
+
+
+def quantile_closest(x, q, axis=None):
+    return _np.quantile(x, q, axis=axis, interpolation="closest_observation")
+
+
+def minvalues(x: _np.ndarray, **kwargs):
+    return _np.min(x, **kwargs)
+
+
+def maxvalues(x: _np.ndarray, **kwargs):
+    return _np.max(x, **kwargs)
+
+
+def is_promotable(x):
+    return isinstance(x, _np.ndarray | list | tuple)
+
+
+def has_grad(_):
+    return False
