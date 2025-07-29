1d67da0cd6895f476f746d957dd30c468133ffa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:37 +0000

    Add package types-boto3-full/1.39.12/types_boto3_full-1.39.12-py3-none-any.whl

---
+++ packages/types-boto3-full/types_boto3_full-1.39.12-py3-none-any.whl/types_boto3_socialmessaging/paginator.py
@@ -0,0 +1,66 @@
+"""
+Type annotations for socialmessaging service client paginators.
+
+[Documentation](https://youtype.github.io/types_boto3_docs/types_boto3_socialmessaging/paginators/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+
+    from types_boto3_socialmessaging.client import EndUserMessagingSocialClient
+    from types_boto3_socialmessaging.paginator import (
+        ListLinkedWhatsAppBusinessAccountsPaginator,
+    )
+
+    session = Session()
+    client: EndUserMessagingSocialClient = session.client("socialmessaging")
+
+    list_linked_whatsapp_business_accounts_paginator: ListLinkedWhatsAppBusinessAccountsPaginator = client.get_paginator("list_linked_whatsapp_business_accounts")
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+from typing import TYPE_CHECKING
+
+from botocore.paginate import PageIterator, Paginator
+
+from .type_defs import (
+    ListLinkedWhatsAppBusinessAccountsInputPaginateTypeDef,
+    ListLinkedWhatsAppBusinessAccountsOutputTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+
+__all__ = ("ListLinkedWhatsAppBusinessAccountsPaginator",)
+
+
+if TYPE_CHECKING:
+    _ListLinkedWhatsAppBusinessAccountsPaginatorBase = Paginator[
+        ListLinkedWhatsAppBusinessAccountsOutputTypeDef
+    ]
+else:
+    _ListLinkedWhatsAppBusinessAccountsPaginatorBase = Paginator  # type: ignore[assignment]
+
+
+class ListLinkedWhatsAppBusinessAccountsPaginator(_ListLinkedWhatsAppBusinessAccountsPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/socialmessaging/paginator/ListLinkedWhatsAppBusinessAccounts.html#EndUserMessagingSocial.Paginator.ListLinkedWhatsAppBusinessAccounts)
+    [Show types-boto3-full documentation](https://youtype.github.io/types_boto3_docs/types_boto3_socialmessaging/paginators/#listlinkedwhatsappbusinessaccountspaginator)
+    """
+
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[ListLinkedWhatsAppBusinessAccountsInputPaginateTypeDef]
+    ) -> PageIterator[ListLinkedWhatsAppBusinessAccountsOutputTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/socialmessaging/paginator/ListLinkedWhatsAppBusinessAccounts.html#EndUserMessagingSocial.Paginator.ListLinkedWhatsAppBusinessAccounts.paginate)
+        [Show types-boto3-full documentation](https://youtype.github.io/types_boto3_docs/types_boto3_socialmessaging/paginators/#listlinkedwhatsappbusinessaccountspaginator)
+        """
