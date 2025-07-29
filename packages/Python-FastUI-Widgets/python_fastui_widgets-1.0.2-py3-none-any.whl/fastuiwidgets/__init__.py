63ccd2ef3494bc382c98c2ac318d230c4a9a29dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:31 +0000

    Add package Python-FastUI-Widgets/1.0.2/python_fastui_widgets-1.0.2-py3-none-any.whl

---
+++ packages/Python-FastUI-Widgets/python_fastui_widgets-1.0.2-py3-none-any.whl/fastuiwidgets/__init__.py
@@ -0,0 +1,12 @@
+"""
+Python-FastUI-Widgets
+======================
+"""
+
+__version__ = "1.0.1"
+__author__ = "NumBNN"
+
+from .components import *
+from .common import *
+from .window import *
+from ._rc import resource
