cc8ff679874a687776f2d97e0784b683b94247d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:56 +0000

    Add package fidi-common-libraries/1.2.0/fidi_common_libraries-1.2.0.tar.gz

---
+++ packages/fidi-common-libraries/fidi_common_libraries-1.2.0.tar.gz/fidi_common_libraries-1.2.0/src/fidi_common_libraries/ui/__init__.py
@@ -0,0 +1,8 @@
+"""Módulo para automação de interfaces gráficas (UI).
+
+Este módulo fornece componentes e utilitários para automação de UI
+com Pywinauto, facilitando a criação de scripts de automação robustos
+e manuteníveis.
+
+Nota: Este módulo está em desenvolvimento.
+"""
