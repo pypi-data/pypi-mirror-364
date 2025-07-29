558dc389a6b6d3c80e5bb14ddb3875e22a14236e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:08 +0000

    Add package ara/1.7.3.0rc2/ara-1.7.3.0rc2.tar.gz

---
+++ packages/ara/ara-1.7.3.0rc2.tar.gz/ara-1.7.3.0rc2/setup.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2022 The ARA Records Ansible authors
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+import setuptools
+
+setuptools.setup(setup_requires=["pbr"], pbr=True, long_description_content_type="text/markdown")
