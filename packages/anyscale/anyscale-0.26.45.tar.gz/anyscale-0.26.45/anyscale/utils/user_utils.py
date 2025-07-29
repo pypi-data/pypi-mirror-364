4fe56ed49acecf5ef43f6dd79733d66892092972
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:30 +0000

    Add package anyscale/0.26.45/anyscale-0.26.45.tar.gz

---
+++ packages/anyscale/anyscale-0.26.45.tar.gz/anyscale-0.26.45/anyscale/utils/user_utils.py
@@ -0,0 +1,17 @@
+from click import ClickException
+
+from anyscale.client.openapi_client.api.default_api import DefaultApi
+
+
+def get_user_id_by_email(api_client: DefaultApi, email: str) -> str:
+    users = api_client.list_organization_collaborators_api_v2_organization_collaborators_get(
+        email=email
+    ).results
+    if len(users) == 0:
+        raise ClickException(f"No user found with email {email}.")
+
+    if len(users) > 1:
+        raise ClickException(
+            f"Multiple users found with email {email}. Please provide a unique email."
+        )
+    return users[0].user_id
