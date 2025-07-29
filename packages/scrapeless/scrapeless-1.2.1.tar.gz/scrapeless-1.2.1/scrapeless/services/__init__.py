78f5ddc383e1f265050070b84be7880647e75f36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:34 +0000

    Add package scrapeless/1.2.1/scrapeless-1.2.1.tar.gz

---
+++ packages/scrapeless/scrapeless-1.2.1.tar.gz/scrapeless-1.2.1/scrapeless/services/__init__.py
@@ -0,0 +1,23 @@
+from .actor import ActorService
+from .browser import BrowserService
+from .deepserp import DeepSerpService
+from .proxies import ProxiesService
+from .scraping import ScrapingService
+from .storage import StorageService
+from .universal import UniversalService
+from .crawl import ScrapingCrawlService
+from .captcha import CaptchaService
+from .profiles import ProfilesService
+
+__all__ = [
+    'ActorService',
+    'BrowserService',
+    'DeepSerpService',
+    'ProxiesService',
+    'ScrapingService',
+    'StorageService',
+    'UniversalService',
+    'ScrapingCrawlService',
+    'CaptchaService',
+    'ProfilesService',
+]
