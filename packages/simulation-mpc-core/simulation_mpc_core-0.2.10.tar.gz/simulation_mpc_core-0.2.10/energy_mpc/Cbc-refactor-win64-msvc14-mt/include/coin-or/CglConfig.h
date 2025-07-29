05df0e43ded981ff22b55194ae5804375ec71431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:25 +0000

    Add package simulation-mpc-core/0.2.10/simulation_mpc_core-0.2.10.tar.gz

---
+++ packages/simulation-mpc-core/simulation_mpc_core-0.2.10.tar.gz/simulation_mpc_core-0.2.10/energy_mpc/Cbc-refactor-win64-msvc14-mt/include/coin-or/CglConfig.h
@@ -0,0 +1,22 @@
+/* src/CglCommon/config_cgl.h.  Generated from config_cgl.h.in by configure.  */
+/* src/config_cgl.h.in. */
+
+#ifndef __CONFIG_CGL_H__
+#define __CONFIG_CGL_H__
+
+/* Version number of project */
+#define CGL_VERSION "devel"
+
+/* Major Version number of project */
+#define CGL_VERSION_MAJOR 9999
+
+/* Minor Version number of project */
+#define CGL_VERSION_MINOR 9999
+
+/* Release Version number of project */
+#define CGL_VERSION_RELEASE 9999
+
+/* Library Visibility Attribute */
+#define CGLLIB_EXPORT 
+
+#endif
