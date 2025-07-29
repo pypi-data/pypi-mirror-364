b6ca4bfa882811eaf66d59b6cb1fdf0eb7971d1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:40 +0000

    Add package model-mcp-sdk/1.0.1/model_mcp_sdk-1.0.1.tar.gz

---
+++ packages/model-mcp-sdk/model_mcp_sdk-1.0.1.tar.gz/model_mcp_sdk-1.0.1/src/model_mcp_sdk/core/ModelUrlConstant.py
@@ -0,0 +1,28 @@
+class ModelUrlConstant:
+    T0KEN_HEADER_NAME = "X-Access-Token"
+    TOKEN_LOGIN_TYPE_HEADER_NAME = "Login-Type"
+    TOKEN_URL = "/jeecg-system/sys/sysApplication/getAppToken"
+    SERVICE_GET_NC_SCHEMA_URL = "/szrs-model/model/slbModelServiceArgo/schSvcNcInfo"
+    SERVICE_WRITE_SERVICE_NC_URL = (
+        "/szrs-model/model/slbModelServiceArgo/writeServiceNcFile"
+    )
+    SERVICE_EXEC_SCHEME_SERVICE_URL = "/szrs-model/model/slbModelServiceArgo/schSvc"
+    CORE_NC_TO_JSON_URL = "/szrs-model/model/slbModelCores/ncToJson"
+    POSTPROCESS_GET_ALL_ACHIEVEMENTS_URL = (
+        "/szrs-postprocess/baseAchievements/getAllAchievementBySchemeId"
+    )
+    POSTPROCESS_GET_LATEST_ACHIEVEMENTS_URL = (
+        "/szrs-postprocess/baseAchievements/getAchievementBySchemeId"
+    )
+    POSTPROCESS_GET_ACHIEVEMENTS_FULL_URL = (
+        "/szrs-postprocess/baseAchievements/getAchievementList"
+    )
+    POSTPROCESS_GET_ACHIEVEMENTS_BASE_GEO_URL = (
+        "/szrs-postprocess/baseAchievements/getAchievementsBaseGeo"
+    )
+    POSTPROCESS_GET_ACHIEVEMENTS_DETAIL_URL = (
+        "/szrs-postprocess/baseAchievements/getAchievementsElementDetail"
+    )
+    POSTPROCESS_GET_ACHIEVEMENTS_TIMELINE_URL = (
+        "/szrs-postprocess/baseAchievements/getAchievementsElementTimeLine"
+    )
