6419bcf762e458d83e4426896498553599b05179
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:28 +0000

    Add package bitswan/0.0.0/bitswan-0.0.0-py2.py3-none-any.whl

---
+++ packages/bitswan/bitswan-0.0.0-py2.py3-none-any.whl/bspump/__version__.py
@@ -0,0 +1,12 @@
+# THIS-FILE-WILL-BE-REPLACED !!! DO NOT CHANGE OR MODIFY THIS FILE !!!
+
+# During `setup.py build_py`, this file is overwritten
+# __version__ = "2025.07.23.6"
+# __build__ = '[git rev-parse HEAD]'
+
+# PEP 440 -- Version Identification and Dependency Specification
+# https://www.python.org/dev/peps/pep-0440/
+
+
+__version__ = "2025.07.23.6"
+__build__ = "devel"
