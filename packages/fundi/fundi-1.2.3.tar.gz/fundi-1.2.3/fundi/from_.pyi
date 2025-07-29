d3930b6d720397979946875a8fed0a22abea03ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:55 +0000

    Add package fundi/1.2.3/fundi-1.2.3.tar.gz

---
+++ packages/fundi/fundi-1.2.3.tar.gz/fundi-1.2.3/fundi/from_.pyi
@@ -0,0 +1,28 @@
+import typing
+from typing import overload
+from collections.abc import Generator, AsyncGenerator, Awaitable
+from contextlib import AbstractAsyncContextManager, AbstractContextManager
+
+T = typing.TypeVar("T", bound=type)
+R = typing.TypeVar("R")
+
+@overload
+def from_(
+    dependency: typing.Callable[..., AbstractContextManager[R]], caching: bool = True
+) -> R: ...
+@overload
+def from_(
+    dependency: typing.Callable[..., AbstractAsyncContextManager[R]], caching: bool = True
+) -> R: ...
+@overload
+def from_(dependency: T, caching: bool = True) -> T: ...
+@overload
+def from_(
+    dependency: typing.Callable[..., Generator[R, None, None]], caching: bool = True
+) -> R: ...
+@overload
+def from_(dependency: typing.Callable[..., AsyncGenerator[R, None]], caching: bool = True) -> R: ...
+@overload
+def from_(dependency: typing.Callable[..., Awaitable[R]], caching: bool = True) -> R: ...
+@overload
+def from_(dependency: typing.Callable[..., R], caching: bool = True) -> R: ...
