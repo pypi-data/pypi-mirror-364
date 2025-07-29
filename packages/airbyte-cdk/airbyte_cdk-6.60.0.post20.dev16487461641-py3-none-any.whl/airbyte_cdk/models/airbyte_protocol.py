afaeb141b4ffb1ba4822741bed1e17d5d7685a20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:36 +0000

    Add package airbyte-cdk/6.60.0.post20.dev16487461641/airbyte_cdk-6.60.0.post20.dev16487461641-py3-none-any.whl

---
+++ packages/airbyte-cdk/airbyte_cdk-6.60.0.post20.dev16487461641-py3-none-any.whl/airbyte_cdk/models/airbyte_protocol.py
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+#
+
+import sys
+from dataclasses import InitVar, dataclass
+from typing import Annotated, Any, Dict, List, Mapping, Optional, Union
+
+from airbyte_protocol_dataclasses.models import *  # noqa: F403  # Allow '*'
