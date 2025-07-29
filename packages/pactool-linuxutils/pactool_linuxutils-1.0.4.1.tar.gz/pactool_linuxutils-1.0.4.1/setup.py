a7b247f5a478fb5e1b906e55b21dbc66b892913f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:52 +0000

    Add package pactool-linuxutils/1.0.4.1/pactool_linuxutils-1.0.4.1.tar.gz

---
+++ packages/pactool-linuxutils/pactool_linuxutils-1.0.4.1.tar.gz/pactool_linuxutils-1.0.4.1/setup.py
@@ -0,0 +1,57 @@
+# ==============================================================================
+#
+#  Pactool - A Cross-Distro Package Management Helper
+#  Copyright 2025 The Linux Utils (https://github.com/LinuxUtils/pactool)
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#  This software is provided for free and open use, but attribution is
+#  REQUIRED when redistributing or modifying this code. Any derivative
+#  works must include this license header and must clearly indicate all
+#  modifications that have been made.
+#
+#  For third-party code integrations, ensure you comply with both the
+#  Pactool license and the license of the third-party code.
+#
+#  DISCLAIMER:
+#  Pactool is provided "as is," without any warranties of any kind,
+#  whether express or implied, including but not limited to warranties
+#  of merchantability or fitness for a particular purpose.
+#
+# ==============================================================================
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name="pactool-linuxutils",
+    version="1.0.4.1",
+    author="LinuxUtils",
+    author_email="thelinuxutils@gmail.com",
+    description="A cross-distro package management helper for Linux systems.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/LinuxUtils/pactool",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires=">=3.8",
+    entry_points={
+        'console_scripts': [
+            'pactool=pactool:main',
+        ],
+    },
+)
