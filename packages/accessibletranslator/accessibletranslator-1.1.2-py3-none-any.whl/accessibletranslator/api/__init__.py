f1dfb330acb2e29130e57c9403474d192b069527
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:38 +0000

    Add package accessibletranslator/1.1.2/accessibletranslator-1.1.2-py3-none-any.whl

---
+++ packages/accessibletranslator/accessibletranslator-1.1.2-py3-none-any.whl/accessibletranslator/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from accessibletranslator.api.system_api import SystemApi
+from accessibletranslator.api.translation_api import TranslationApi
+from accessibletranslator.api.user_management_api import UserManagementApi
+
