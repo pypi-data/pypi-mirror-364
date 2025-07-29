ef0f36ed5ceb0bfd3364119b9becdc2ff74ee364
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:06 +0000

    Add package m5tlprovetomegz2/0.1.0/m5tlprovetomegz2-0.1.0.tar.gz

---
+++ packages/m5tlprovetomegz2/m5tlprovetomegz2-0.1.0.tar.gz/m5tlprovetomegz2-0.1.0/setup.py
@@ -0,0 +1,11 @@
+# filepath: contextamar/setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="m5tlprovetomegz2",
+    version="0.1.0",
+    description="A simple example library",
+    author="M5TL",
+    packages=find_packages(),
+    install_requires=[],
+)
