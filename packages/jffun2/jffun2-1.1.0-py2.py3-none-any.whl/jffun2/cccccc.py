c8f98dcfa53f2b0faf5f4149e40a9e1e1c07281d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:32 +0000

    Add package jffun2/1.1.0/jffun2-1.1.0-py2.py3-none-any.whl

---
+++ packages/jffun2/jffun2-1.1.0-py2.py3-none-any.whl/jffun2/cccccc.py
@@ -0,0 +1,3 @@
+class DDDDDD:
+    def say_hello(self):
+        print(f"Hello from {__file__}")
---
+++ packages/jffun2/jffun2-1.1.0-py2.py3-none-any.whl/jffun2/hello.py
@@ -0,0 +1,12 @@
+from .cccccc import DDDDDD
+from os import get_terminal_size
+
+
+def EEEEEE():
+    WIDTH = get_terminal_size()[0]
+    print("恭喜发财恭喜发财".center(WIDTH - 8, "="))
+    DDDDDD().say_hello()
+
+
+if __name__ == "__main__":
+    EEEEEE()
