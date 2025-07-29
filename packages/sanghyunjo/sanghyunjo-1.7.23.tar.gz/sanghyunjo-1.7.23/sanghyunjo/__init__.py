cdda9e0754caf406ff0a49c2e4727cfb80ccd34d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:11 +0000

    Add package sanghyunjo/1.7.23/sanghyunjo-1.7.23.tar.gz

---
+++ packages/sanghyunjo/sanghyunjo-1.7.23.tar.gz/sanghyunjo-1.7.23/sanghyunjo/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (C) 2025 * Ltd. All rights reserved.
+# author: Sanghyun Jo <shjo.april@gmail.com>
+
+__version__ = '1.7.23'
+
+from .misc import *
+from .cv_utils import *
+from .json_utils import *
+from .xml_utils import *
+# from .ai_utils import * # excluded from default imports to avoid requiring torch for non-GPU users
