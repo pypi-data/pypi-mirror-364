98e973c4dc37f6a24e4de72c9c08e00b404fa550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:34 +0000

    Add package spatialsurf/1.4/spatialsurf-1.4-py3-none-any.whl

---
+++ packages/spatialsurf/spatialsurf-1.4-py3-none-any.whl/spatialsurf/utils/get_data_from_h5.py
@@ -0,0 +1,29 @@
+import collections
+import tables
+import scipy.sparse as sp_sparse
+
+
+def get_data_from_h5(filename):
+    CountMatrix = collections.namedtuple('CountMatrix', ['feature_ref', 'barcodes', 'matrix'])
+    with tables.open_file(filename, 'r') as f:
+        mat_group = f.get_node(f.root, 'matrix')
+        barcodes = f.get_node(mat_group, 'barcodes').read()
+        data = getattr(mat_group, 'data').read()
+        indices = getattr(mat_group, 'indices').read()
+        indptr = getattr(mat_group, 'indptr').read()
+        shape = getattr(mat_group, 'shape').read()
+        matrix = sp_sparse.csc_matrix((data, indices, indptr), shape=shape)
+
+        feature_ref = {}
+        feature_group = f.get_node(mat_group, 'features')
+        feature_ids = getattr(feature_group, 'id').read()
+        feature_names = getattr(feature_group, 'name').read()
+        feature_types = getattr(feature_group, 'feature_type').read()
+        feature_ref['id'] = feature_ids
+        feature_ref['name'] = feature_names
+        feature_ref['feature_type'] = feature_types
+        tag_keys = getattr(feature_group, '_all_tag_keys').read()
+        for key in tag_keys:
+            feature_ref[key.decode('utf-8')] = getattr(feature_group, key.decode('utf-8')).read()
+
+        return CountMatrix(feature_ref, barcodes, matrix)
