7f592a354cd325a1222120436fbeb4dd7270c71b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:00 +0000

    Add package monopoly-core/0.18.4/monopoly_core-0.18.4.tar.gz

---
+++ packages/monopoly-core/monopoly_core-0.18.4.tar.gz/monopoly_core-0.18.4/src/monopoly/banks/zkb/zkb.py
@@ -0,0 +1,35 @@
+import logging
+from re import compile as regex
+
+from monopoly.banks.base import BankBase
+from monopoly.config import MultilineConfig, StatementConfig
+from monopoly.constants import ISO8601, BankNames, DebitTransactionPatterns, EntryType
+from monopoly.identifiers import MetadataIdentifier
+
+logger = logging.getLogger(__name__)
+
+
+class ZurcherKantonalBank(BankBase):
+    name = BankNames.ZKB
+
+    debit = StatementConfig(
+        statement_type=EntryType.DEBIT,
+        statement_date_pattern=regex(rf"Balance as of: ({ISO8601.DD_MM_YYYY})"),
+        header_pattern=regex(r"(Date.*Booking text.*Debit CHF.*Credit CHF)"),
+        transaction_pattern=DebitTransactionPatterns.ZKB,
+        multiline_config=MultilineConfig(multiline_descriptions=True),
+        safety_check=False,
+        transaction_date_format="%d.%m.%Y",
+    )
+
+    identifiers = [
+        [
+            MetadataIdentifier(
+                format="PDF 1.7",
+                title="SLK_Vermoegensinfo_Group",
+                creator="Designer",
+                producer="PDFlib+PDI",
+            )
+        ]
+    ]
+    statement_configs = [debit]
