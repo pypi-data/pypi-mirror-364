fa9ac6135cf05417de0aee993c0ecbe45ddb1114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:38 +0000

    Add package starburst-data-products-client/0.1.1/starburst_data_products_client-0.1.1.tar.gz

---
+++ packages/starburst-data-products-client/starburst_data_products_client-0.1.1.tar.gz/starburst_data_products_client-0.1.1/starburst_data_products_client/sep/data/MaterializedViewProperties.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+from typing import Optional
+from starburst_data_products_client.shared.models import JsonDataClass
+
+
+@dataclass
+class MaterializedViewProperties(JsonDataClass):
+    refresh_interval: Optional[str]
+    grace_period: Optional[str]
