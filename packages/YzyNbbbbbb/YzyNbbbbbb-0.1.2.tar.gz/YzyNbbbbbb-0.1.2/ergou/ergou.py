396a564fa5dca287fd30382fb9ee817899dac21e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:26 +0000

    Add package YzyNbbbbbb/0.1.2/YzyNbbbbbb-0.1.2.tar.gz

---
+++ packages/YzyNbbbbbb/YzyNbbbbbb-0.1.2.tar.gz/YzyNbbbbbb-0.1.2/ergou/ergou.py
@@ -0,0 +1,3 @@
+# my_functions.py
+def get_info(a, b):
+    print(f"wocao,yzy nb!")
---
+++ packages/YzyNbbbbbb/YzyNbbbbbb-0.1.2.tar.gz/YzyNbbbbbb-0.1.2/setup.py
@@ -0,0 +1,24 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="YzyNbbbbbb",  # 包名，必须是PyPI上唯一的
+    version="0.1.2",         # 版本号
+    author="nigulasi_ergou",
+    author_email="ergou@email.com",
+
+    description="A simple package with my custom functions",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
+
+
