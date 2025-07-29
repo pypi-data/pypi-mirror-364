5d6d7e5a13c5dc6aaa42c376969fcaf2433486c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:47 +0000

    Add package torchic/0.5.24/torchic-0.5.24.tar.gz

---
+++ packages/torchic/torchic-0.5.24.tar.gz/torchic-0.5.24/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='torchic',
+    version='0.5.24',  # Initial version
+    author='Giorgio Alberto Lucia, Roberta Ferioli',
+    author_email='giogioalberto@gmail.com',
+    description='A Python library for data analysis in python and ROOT (tailored for high energy physics)',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',  # Format of README
+    url='https://github.com/GiorgioAlbertoLucia/torchic',
+    packages=find_packages(),  # Automatically finds your package
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # Replace with your license
+        'Operating System :: OS Independent',
+    ],
+    package_data={'torchic': [
+        'physics/BetheBloch.hh',
+        'roopdf/RooCustomPdfs/RooGausExp.hh',
+        'roopdf/RooCustomPdfs/RooGausExp.cxx',
+        'roopdf/RooCustomPdfs/RooSillPdf.hh',
+        'physics/simulations/ExponentialDecaySimulation.cpp',
+        'physics/simulations/TwoBodyDecaySimulation.cpp',
+        ]},  # Include header files
+    python_requires='>=3.6',  # Minimum Python version
+    install_requires=[
+        # List your package dependencies here
+        'numpy<2.0',
+        'pandas',
+        'scikit-learn',
+        'pyYAML',
+        'uproot',
+    ],
+    extras_require={
+        'ROOT': ['ROOT'],
+    },
+)
