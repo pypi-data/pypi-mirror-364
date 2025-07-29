b9953b601784611626c015c102fc55040d5eb984
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:10 +0000

    Add package wotclientdetection/2025.7.23/wotclientdetection-2025.7.23.tar.gz

---
+++ packages/wotclientdetection/wotclientdetection-2025.7.23.tar.gz/wotclientdetection-2025.7.23/wotclientdetection/constants.py
@@ -0,0 +1,58 @@
+import collections
+
+class LauncherFlavour:
+    UNKNOWN = 0
+    WG = 1
+    CHINA_360 = 2
+    STEAM = 3
+    LESTA = 4
+    STANDALONE = 5
+    DEFAULT = WG
+
+_LauncherMetadata = collections.namedtuple('LauncherMetadata', ('flavour', 'path', 'pointer', 'executable'))
+
+LAUNCHERS_METADATA = (
+    _LauncherMetadata(LauncherFlavour.WG, 'Wargaming.net\\GameCenter', 'data\\wgc_path.dat', 'wgc.exe'),
+    _LauncherMetadata(LauncherFlavour.CHINA_360, '360 Wargaming\\GameCenter', 'data\\wgc_path.dat', 'wgc.exe'),
+    _LauncherMetadata(LauncherFlavour.STEAM, 'Wargaming.net\\GameCenter for Steam', 'data\\wgc_path.dat', 'wgc.exe'),
+    _LauncherMetadata(LauncherFlavour.LESTA, 'Lesta\\GameCenter', 'data\\lgc_path.dat', 'lgc.exe'),
+    _LauncherMetadata(LauncherFlavour.STANDALONE, '', '', '')
+)
+
+class ClientBranch:
+    UNKNOWN = 0
+    RELEASE = 1
+    COMMON_TEST = 2
+    SUPERTEST = 3
+    SANDBOX = 4
+
+
+class ClientExecutableName:
+    DEFAULT = 'WorldOfTanks.exe'
+    LESTA = 'Tanki.exe'
+
+
+class ClientType:
+    UNKNOWN = 0
+    SD = 1
+    HD = 2
+
+
+class ClientRealm:
+    EU = 'EU'
+    NA = 'NA'
+    ASIA = 'ASIA'
+    CT = 'CT'
+    CN = 'CN'
+    RU = 'RU'
+    RPT = 'RPT'
+
+CLIENT_REALM_TO_LAUNCHER_FLAVOUR = {
+    ClientRealm.EU: LauncherFlavour.WG,
+    ClientRealm.NA: LauncherFlavour.WG,
+    ClientRealm.ASIA: LauncherFlavour.WG,
+    ClientRealm.CT: LauncherFlavour.WG,
+    ClientRealm.CN: LauncherFlavour.CHINA_360,
+    ClientRealm.RU: LauncherFlavour.LESTA,
+    ClientRealm.RPT: LauncherFlavour.LESTA,
+}
