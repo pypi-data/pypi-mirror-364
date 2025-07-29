921c925cf1f4be6455ca94a1f6ee7f909dc9ff05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:09:22 +0000

    Add package create-django-app-irwin/0.0.5/create_django_app_irwin-0.0.5.tar.gz

---
+++ packages/create-django-app-irwin/create_django_app_irwin-0.0.5.tar.gz/create_django_app_irwin-0.0.5/setup.py
@@ -0,0 +1,26 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='create_django_app_irwin',  # ← nom unique
+    version='0.0.5',  # ← incrémente bien à chaque build
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'create-app=create_django_app_irwin.cli:main',
+        ],
+    },
+    author='DJRIGA IRWIN JESSY',
+    author_email='irwindjriga@gmail.com',
+    description='Génère une structure d’app Django automatiquement',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
