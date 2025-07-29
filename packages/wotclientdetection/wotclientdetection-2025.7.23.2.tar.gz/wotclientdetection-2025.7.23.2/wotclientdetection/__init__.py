1fe352c7fd49f51adf98f0fa6cba6e95a7f2b748
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:29 +0000

    Add package wotclientdetection/2025.7.23.2/wotclientdetection-2025.7.23.2.tar.gz

---
+++ packages/wotclientdetection/wotclientdetection-2025.7.23.2.tar.gz/wotclientdetection-2025.7.23.2/wotclientdetection/__init__.py
@@ -0,0 +1,14 @@
+__title__ = 'wotclientdetection'
+__version__ = '2025.7.23.2'
+
+from wotclientdetection.manager import LauncherManager
+from wotclientdetection.constants import LauncherFlavour, ClientExecutableName, ClientBranch, ClientRealm
+
+__all__ = (
+    'LauncherManager',
+    'LauncherFlavour',
+    'ClientExecutableName',
+    'ClientBranch',
+    'ClientBranch',
+    'ClientRealm',
+)
