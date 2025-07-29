b2e63fd4b76a8d236855dab9000a338e93ae85d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:45 +0000

    Add package TuModeloDeClientes/0.1/tumodelodeclientes-0.1.tar.gz

---
+++ packages/TuModeloDeClientes/tumodelodeclientes-0.1.tar.gz/tumodelodeclientes-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TuModeloDeClientes",
+    version="0.1",
+    packages=find_packages(),
+    description="Modelo de Clientes para una página de compras",
+    author="Tu Nombre Apellido",
+    author_email="tunombre@correo.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
