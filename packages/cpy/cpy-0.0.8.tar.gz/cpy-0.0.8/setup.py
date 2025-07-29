6d1c2265c6994c89c47ffd6272391b215ec1025e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:44 +0000

    Add package cpy/0.0.8/cpy-0.0.8.tar.gz

---
+++ packages/cpy/cpy-0.0.8.tar.gz/cpy-0.0.8/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cpy",
+    version="0.0.8",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "setuptools>=70.3.0",
+        "deprecated",
+        "cryptography>=45.0.4",
+    ],
+    entry_points={
+        'console_scripts': [
+            'ccrypt = cpy.ccrypt.cli:main',
+        ],
+    },
+    author="CHARZ",
+    author_email="your.email@example.com",
+    description="Charz Python Library",
+    long_description_content_type="text/markdown",
+    url="",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
