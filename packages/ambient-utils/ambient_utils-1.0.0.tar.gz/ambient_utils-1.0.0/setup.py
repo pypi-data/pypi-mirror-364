aa9997e119697d1c4c7e2247bb22279aca3b2069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:54 +0000

    Add package ambient-utils/1.0.0/ambient_utils-1.0.0.tar.gz

---
+++ packages/ambient-utils/ambient_utils-1.0.0.tar.gz/ambient_utils-1.0.0/setup.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+import setuptools
+
+root = Path(__file__).parent.resolve()
+
+with open(root / "README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open(root / "requirements.txt", "r", encoding="utf-8") as f:
+    requirements = [line.strip() for line in f.readlines()]
+
+setuptools.setup(
+    name="ambient_utils",
+    version="1.0.0",
+    author="giannisdaras",
+    author_email="daras.giannhs@gmail.com",
+    description="Utility functions for learning generative models from corrupted data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/giannisdaras/ambient_utils",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Intended Audience :: Science/Research",
+    ],
+    license="GPL-3.0",
+    python_requires='>=3.6',
+)
