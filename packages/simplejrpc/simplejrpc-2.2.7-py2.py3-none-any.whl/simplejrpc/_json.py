60c0937575d17a23729b1305fa7a2f477db22d79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:40 +0000

    Add package simplejrpc/2.2.7/simplejrpc-2.2.7-py2.py3-none-any.whl

---
+++ packages/simplejrpc/simplejrpc-2.2.7-py2.py3-none-any.whl/simplejrpc/_json.py
@@ -0,0 +1,22 @@
+# -*- encoding: utf-8 -*-
+import json
+import http
+from typing import Any, Optional, Callable
+
+
+def _jsonify(
+    code: int = http.HTTPStatus.OK.value,
+    msg: Optional[str] = None,
+    data: Any = None,
+    method: Optional[str] = None,
+):
+    """ """
+
+    res_data = {
+        "code": code,
+        "meta": {"endpoint": method, "close": 1},
+        "data": data,
+        "msg": msg,
+    }
+
+    return res_data
