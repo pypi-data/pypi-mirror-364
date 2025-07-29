4c0cca8e2409f7dd79020f2022e2cdd8d47b7764
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:10 +0000

    Add package canonmap/0.3.16/canonmap-0.3.16.tar.gz

---
+++ packages/canonmap/canonmap-0.3.16.tar.gz/canonmap-0.3.16/canonmap/services/db_mysql/schemas.py
@@ -0,0 +1,37 @@
+from typing import List, Optional
+from enum import Enum
+from pydantic import BaseModel
+
+class CreateDDLResponse(BaseModel):
+    ddl: str
+    ddl_path: Optional[str] = None
+
+class FieldTransformTypeType(str, Enum):
+    INITIALISM = "initialism"
+    PHONETIC = "phonetic"
+    SOUNDEX = "soundex"
+
+class FieldTransformTypeRequest(BaseModel):
+    original_field_name: str
+    transformed_field_name: str
+    field_transform: FieldTransformTypeType
+
+class TableField(BaseModel):
+    table_name: str
+    field_name: str
+
+class EntityMappingRequest(BaseModel):
+    entity_name: str
+    select_field: TableField
+    top_n: int = 20
+    max_prefilter: int = 1000
+
+class SingleMappedEntity(BaseModel):
+    raw_entity: str
+    canonical_entity: str
+    canonical_table_name: str
+    canonical_field_name: str
+    score: float
+
+class EntityMappingResponse(BaseModel):
+    results: List[SingleMappedEntity]
