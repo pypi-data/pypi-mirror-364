81e9ee4eac3766de5ffef781adf0bc86d8fd61e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:54 +0000

    Add package gshock-api/2.0.13/gshock_api-2.0.13.tar.gz

---
+++ packages/gshock-api/gshock_api-2.0.13.tar.gz/gshock_api-2.0.13/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gshock_api',
+    version='2.0.13',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    install_requires=[
+        'pytz',
+        'bleak',
+    ],
+    entry_points={
+        'console_scripts': [
+            'gshock_server=examples.gshock_server:main',
+        ],
+    },
+)
