6c3a775dc366c6f51c2795a7f6b1ebeb22050f06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:01 +0000

    Add package ddd-cli/1.7.23/ddd_cli-1.7.23.tar.gz

---
+++ packages/ddd-cli/ddd_cli-1.7.23.tar.gz/ddd_cli-1.7.23/ddd/management/templates/repository/class.py
@@ -0,0 +1,241 @@
+from typing import List, Optional
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
+from django.forms import ValidationError
+
+# importa las entidades utilizadas aqui
+from ..models import [[ entity_name.capitalize() ]]
+from ..utils.mappers import Mapper
+from ..utils.clean_dict_of_keys import clean_dict_of_keys
+from ..domain.entities import [[ entity_name.capitalize() ]]Entity
+from ..domain.exceptions import EntityNotFoundError
+
+
+class [[ entity_name.capitalize() ]]Repository:
+    """
+    Repositorio para manejar la persistencia de datos de la entidad: [[ entity_name.lower() ]].
+    
+    Este repositorio incluye:
+    - Validación de existencia de registros.
+    - Control de unicidad.
+    - Métodos básicos.
+    """
+
+    @staticmethod
+    def get_all(filters: Optional[dict] = None) -> List[ [[ entity_name.capitalize() ]]Entity ]:
+        """
+        Obtiene todos los registros de la entidad.
+        :return: Lista de registros de la entidad.
+        """
+
+        instance_list = [[ entity_name.capitalize() ]].objects.all()    
+
+        # Aplicar filtros si se proporcionan
+        if filters:
+            if "nombre" in filters and filters["nombre"] != "":
+                instance_list = instance_list.filter(nombre__icontains=filters["nombre"])      
+                
+        # Tener en cuenta los campos reales que se necesitan en el listado
+        instance_list = instance_list.only("id", "nombre", "created_at")
+
+        # Convertir a entidades usando el Mapper genérico
+        return [Mapper.model_to_entity(instance, [[ entity_name.capitalize() ]]Entity) for instance in instance_list]        
+
+
+    @staticmethod
+    def get_by_id(id) -> Optional[ [[ entity_name.capitalize() ]]Entity ]:
+        """
+        Obtiene un registro por su ID.
+        
+        :param id: ID del registro a recuperar.
+        :return: El entidad encontrada o None si no existe.
+        """
+
+        try:
+            instance = [[ entity_name.capitalize() ]].objects.get(id=id)
+            return Mapper.model_to_entity(instance, [[ entity_name.capitalize() ]]Entity)
+
+        except [[ entity_name.capitalize() ]].DoesNotExist:
+            return None
+
+
+    @staticmethod
+    def exists_by_field(field_name, value) -> bool:
+        """
+        Verifica si existe un registro con un valor específico para un campo dado.
+
+        :param field_name: Nombre del campo a buscar.
+        :param value: Valor del campo a verificar.
+        :return: True si existe un registro con el valor dado, False en caso contrario.
+        """
+
+        return [[ entity_name.capitalize() ]].objects.filter(**{field_name: value}).exists()
+
+
+    @staticmethod
+    def count_all(filters: Optional[dict] = None) -> int:
+        """
+        Cuenta registros que cumplen con ciertas condiciones.
+
+        :param conditions: Condiciones de filtro como clave-valor.
+        :return: Número de registros que cumplen las condiciones.
+        """
+
+        instance_list = [[ entity_name.capitalize() ]].objects.all()    
+
+        # Aplicar filtros si se proporcionan
+        if filters:
+            if "nombre" in filters and filters["nombre"] != "":
+                instance_list = instance_list.filter(nombre__icontains=filters["nombre"])            
+
+        return instance_list.count()            
+
+
+    @staticmethod
+    def create(entity: [[ entity_name.capitalize() ]]Entity, external_id: Optional[int], externals: Optional[List[int]], adicionalData=None) -> [[ entity_name.capitalize() ]]Entity:
+        """
+        Crea un nuevo registro.
+
+        :param entity: Entidad con los datos necesarios para crear el registro.
+        :param external_id: ID del padre si es necesario (opcional).
+        :param externals: Lista de IDs de entidades relacionadas (opcional).
+        :param adicionalData: Datos adicionales a incluir en la creación.
+        :return: La entidad creada.
+        :raises ValueError: Si los datos no son válidos.
+        """
+        #convertir a dict
+        data = entity.to_dict()        
+
+        # Eliminar de la data las propiedades que requieren un tratamiento especial
+        data = clean_dict_of_keys(data, keys=entity.SPECIAL_FIELDS)
+
+        # Crear el registro a partir de los campos presentes en la 'data'
+        instance = [[ entity_name.capitalize() ]](**data)
+
+        # Si se proporciona un ID de otra entidad, actualizarlo
+        # django crea el campo 'external_id' automáticamente si la relación es ForeignKey => otherEntity
+        if external_id is not None:
+            instance.external_id = external_id
+
+        # Si se proporcionan IDs de entidades relacionadas, agregarlos
+        if externals is not None:
+            # Asignar directamente los IDs
+            instance.externals.set(externals)
+
+        # Si adicionalData, agregar datos adicionales
+        if adicionalData:
+            # Aquí puedes agregar lógica para manejar datos adicionales específicos
+            # Por ejemplo, guardar una foto, un password, o cualquier otro campo especial
+            pass
+
+        # Validar y guardar
+        try:
+            instance.full_clean()  # Validaciones del modelo
+            instance.save()
+
+        except ValueError as e:
+            raise ValueError(f"An error occurred while processing the value: {e}")
+        
+        return Mapper.model_to_entity(instance, [[ entity_name.capitalize() ]]Entity)
+
+
+    @staticmethod
+    def save(entity: [[ entity_name.capitalize() ]]Entity, external_id: Optional[int], externals: Optional[List[int]], adicionalData=None) -> [[ entity_name.capitalize() ]]Entity:
+        """
+        Guarda los cambios en una entidad existente.
+
+        :param entity: Entidad con los datos a actualizar (debe traer el id en los campos).
+        :param external_id: ID del padre si es necesario (opcional).
+        :param externals: Lista de IDs de entidades relacionadas (opcional).
+        :param adicionalData: Datos adicionales a incluir en la actualización.
+        :return: La entidad guardada.
+        :raises EntityNotFoundError: Si no existe el registro con el ID dado.
+        :raises ValueError: Si los datos no son válidos.
+        """
+
+        try:
+            # Recuperar el modelo existente basado en el ID de la entidad
+            instance = [[ entity_name.capitalize() ]].objects.get(id=entity.id)
+
+            # Actualizar cada campo de la entidad en el modelo
+            for key, value in entity.to_dict().items():
+                if hasattr(instance, key):
+                    if not key in instance.SPECIAL_FIELDS: # No actualizar campos especiales
+                        setattr(instance, key, value)
+
+            # Si se proporciona un ID de otra entidad, actualizarlo
+            if external_id is not None:
+                instance.external_id = external_id
+
+            # Si se proporcionan IDs de entidades relacionadas, actualizarlos
+            if externals is not None:
+                # Asignar directamente los IDs
+                instance.externals.set(externals)
+
+            # Si adicionalData, agregar datos adicionales
+            if adicionalData:
+                # Aquí puedes agregar lógica para manejar datos adicionales específicos
+                # Por ejemplo, guardar una foto, un password, o cualquier otro campo especial
+                pass
+
+            # Validar y guardar
+            instance.full_clean()  # Validaciones del modelo Django
+            instance.save()
+            
+            # Convertir el modelo actualizado de vuelta a una entidad
+            return Mapper.model_to_entity(instance, [[ entity_name.capitalize() ]]Entity)
+
+        except [[ entity_name.capitalize() ]].DoesNotExist:
+            raise EntityNotFoundError(f"No [[ entity_name.lower() ]] found with ID {id}")
+
+        except ValidationError as e:
+            raise ValueError(f"Validation error occurred: {e.message_dict}")
+
+
+    @staticmethod
+    def delete(id) -> bool:
+        """
+        Elimina un registro por su ID.
+
+        :param id: ID del registro a eliminar.
+        :raises EntityNotFoundError: Si no existe el registro con el ID dado.
+        """
+
+        try:
+            instance = [[ entity_name.capitalize() ]].objects.get(id=id)
+            instance.delete()
+            return True
+
+        except [[ entity_name.capitalize() ]].DoesNotExist:
+            raise EntityNotFoundError(f"No [[ entity_name.lower() ]] found with ID {id}")
+
+
+'''
+En Django ORM los campos de relación se definen como ForeignKey, ManyToManyField o OneToOneField.
+Para la traduccion de relaciones entre entidades, se pueden utilizar los siguientes campos:
+
+- `external_id`: 
+    Para relaciones de clave externa (ForeignKey) o uno a uno (OneToOneField)
+        ej: external = models.ForeignKey(OtherEntity, on_delete=models.CASCADE, related_name='related_entities')
+        o    ej: external = models.OneToOneField(OtherEntity, on_delete=models.CASCADE, related_name='related_entity')
+            
+    el model de Django crea automáticamente el campo `external_id` este campo es accesible como un atributo de la entidad.
+
+- `external_uuid`: Para relaciones basadas en un UUID adicional aparte del ID.
+        ej: external = models.UUIDField(default=uuid.uuid4, editable=False)
+     es necesario definir en el model de Django una propiedad 'external_uuid' que retorne el UUID relacionado.
+    @property
+    def external_uuid(self):
+        return str(self.external.uuid) if self.external else None
+
+- `externals`: Para relaciones de muchos a muchos (ManyToManyField).
+    Para relaciones de muchos a muchos:
+        ej: externals = models.ManyToManyField(OtherEntity, related_name='related_entities')
+
+- `externals_uuids`: Para relaciones de muchos a muchos basadas en UUID adicional aparte del ID.
+        ej: externals = models.ManyToManyField(OtherEntity, related_name='related_entities')
+    es necesario definir en el model de Django una propiedad 'external_uuids' que retorne una lista de UUIDs relacionados.
+    @property
+    def externals_uuids(self):
+        return list(self.externals.values_list('uuid', flat=True))
+'''
