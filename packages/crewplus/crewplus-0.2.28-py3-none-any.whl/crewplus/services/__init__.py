404f0bc56399f98c8d64bb907d46b2e8c54e08f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:32 +0000

    Add package crewplus/0.2.28/crewplus-0.2.28-py3-none-any.whl

---
+++ packages/crewplus/crewplus-0.2.28-py3-none-any.whl/crewplus/services/__init__.py
@@ -0,0 +1,13 @@
+from .gemini_chat_model import GeminiChatModel
+from .init_services import init_load_balancer, get_model_balancer
+from .model_load_balancer import ModelLoadBalancer
+from .azure_chat_model import TracedAzureChatOpenAI
+
+__all__ = [
+    "GeminiChatModel", 
+    "init_load_balancer", 
+    "get_model_balancer", 
+    "ModelLoadBalancer", 
+    "init_services", 
+    "TracedAzureChatOpenAI"
+]
