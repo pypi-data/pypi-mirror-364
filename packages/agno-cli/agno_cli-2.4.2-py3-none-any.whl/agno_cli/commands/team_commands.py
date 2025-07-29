f653b19dee8e203dfc12ba54b7d8a267c3ddd0d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:11 +0000

    Add package agno-cli/2.4.2/agno_cli-2.4.2-py3-none-any.whl

---
+++ packages/agno-cli/agno_cli-2.4.2-py3-none-any.whl/agno_cli/commands/team_commands.py
@@ -0,0 +1,14 @@
+"""
+Team management commands
+"""
+
+class TeamCommands:
+    """Team management commands"""
+    
+    def __init__(self):
+        pass
+    
+    def main():
+        """Main entry point for team commands"""
+        pass
+
