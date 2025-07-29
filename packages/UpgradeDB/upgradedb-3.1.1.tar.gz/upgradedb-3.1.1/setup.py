f99b6e22456cbe6a0d6cc724fce7b0877aa59fe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:48 +0000

    Add package UpgradeDB/3.1.1/upgradedb-3.1.1.tar.gz

---
+++ packages/UpgradeDB/upgradedb-3.1.1.tar.gz/upgradedb-3.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="UpgradeDB",
+    version="3.1.1",
+    author="Seu Nome",
+    author_email="upgradedb@gmail.com",
+    description="Banco de dados leve baseado em JSON para Python",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/seuusuario/upgrade_db_py",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        
+    ],
+)
