69f40e42dfd777f3aca22a276ac82676cd0b7897
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:06:16 +0000

    Add package liste-etudiants/0.2.1/liste_etudiants-0.2.1.tar.gz

---
+++ packages/liste-etudiants/liste_etudiants-0.2.1.tar.gz/liste_etudiants-0.2.1/README.md
@@ -0,0 +1,9 @@
+un package qui genere le nom des etudiants
+# liste_etudiants
+
+Package Python pour générer une liste d'étudiants fictifs.
+
+## Installation
+
+```bash
+pip install liste_etudiants
---
+++ packages/liste-etudiants/liste_etudiants-0.2.1.tar.gz/liste_etudiants-0.2.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='liste_etudiants',  # Nom unique sur PyPI
+    version='0.2.1',
+    description='Package pour générer des noms d''étudiants aléatoires',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    author='Monique',
+    author_email='ton.email@example.com',
+    url='https://github.com/tongithub/liste_etudiants',  # optionnel
+    packages=find_packages(),
+    install_requires=[
+        'Faker',
+    ],
+    entry_points={
+        'console_scripts': [
+            'gen-etudiants=liste_etudiants.cli:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
