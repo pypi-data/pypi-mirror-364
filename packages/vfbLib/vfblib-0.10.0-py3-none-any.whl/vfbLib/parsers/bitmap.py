cb95cd3d063117fe277da454dfbb4c3c433d1d15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:08 +0000

    Add package vfbLib/0.10.0/vfblib-0.10.0-py3-none-any.whl

---
+++ packages/vfbLib/vfblib-0.10.0-py3-none-any.whl/vfbLib/parsers/bitmap.py
@@ -0,0 +1,106 @@
+from __future__ import annotations
+
+import logging
+
+# from math import log2
+# from struct import unpack
+from typing import Any
+
+from fontTools.misc.textTools import num2binary
+
+from vfbLib.parsers.base import BaseParser
+
+logger = logging.getLogger(__name__)
+
+
+def pprint_bitmap(bitmap, invert=False) -> list[str]:
+    print(bitmap)
+    w, h = bitmap["size_pixels"]
+    data = bitmap["data"]
+    col_bytes = w // 8
+    rest = w % 8
+    if rest > 0:
+        col_bytes += 1
+    col_bytes = max(col_bytes, 2)
+    # print(f"Cols: {w}, bytes per line: {col_bytes}")
+    if "bytes" not in data:
+        b = []
+    else:
+        b = data["bytes"]
+    gfx = []
+    for y in range(0, h * col_bytes, col_bytes):
+        col = ""
+        for x in range(col_bytes):
+            i = y + x
+            if i < len(b):
+                byte = b[i]
+            else:
+                byte = 0
+
+            # print(f"Index: {i}, value: {byte}")
+            col += num2binary(byte, bits=8).replace("0", "  ").replace("1", "██")
+        gfx.append(col[: w * 2])
+    if invert:
+        gfx.reverse()
+    print("\n".join(gfx))
+    print(gfx)
+    return gfx
+
+
+class BaseBitmapParser(BaseParser):
+    def parse_bitmap_data(self, datalen) -> dict[str, Any]:
+        bitmap: dict[str, Any] = {}
+        if datalen < 2:
+            logger.error("parse_bitmap_data: Got datalen", datalen)
+            raise ValueError
+
+        rest = 2
+
+        if datalen > 2:
+            num_bytes = self.read_uint8()
+            bitmap["num_bytes"] = num_bytes
+            data = []
+            for _ in range(num_bytes):
+                data.append(self.read_uint8())
+            bitmap["bytes"] = data
+            rest = datalen - num_bytes - 1
+
+        extra = []
+        for _ in range(rest):
+            extra.append(self.read_uint8())
+
+        if extra:
+            bitmap["extra"] = extra
+
+        return bitmap
+
+
+class BackgroundBitmapParser(BaseBitmapParser):
+    def _parse(self) -> dict[str, Any]:
+        bitmap: dict[str, Any] = {}
+        bitmap["origin"] = (self.read_value(), self.read_value())
+        bitmap["size_units"] = (self.read_value(), self.read_value())
+        bitmap["size_pixels"] = (self.read_value(), self.read_value())
+        datalen = self.read_value()
+        bitmap["len"] = datalen
+        bitmap["data"] = self.parse_bitmap_data(datalen)
+        # bitmap["preview"] = pprint_bitmap(bitmap)
+        return bitmap
+
+
+class GlyphBitmapParser(BaseBitmapParser):
+    def _parse(self) -> list[dict[str, Any]]:
+        bitmaps: list[dict[str, Any]] = []
+        num_bitmaps = self.read_value()
+        for _ in range(num_bitmaps):
+            bitmap: dict[str, Any] = {}
+            bitmap["ppm"] = self.read_value()
+            bitmap["origin"] = (self.read_value(), self.read_value())
+            bitmap["adv"] = (self.read_value(), self.read_value())
+            bitmap["size_pixels"] = (self.read_value(), self.read_value())
+            datalen = self.read_value()
+            bitmap["len"] = datalen
+            bitmap["data"] = self.parse_bitmap_data(datalen)
+            # bitmap["preview"] = pprint_bitmap(bitmap, invert=True)
+            bitmaps.append(bitmap)
+        return bitmaps
---
+++ packages/vfbLib/vfblib-0.10.0-py3-none-any.whl/vfbLib/parsers/text.py
@@ -0,0 +1,130 @@
+from __future__ import annotations
+
+import logging
+from re import search
+
+from vfbLib.parsers.base import BaseParser
+from vfbLib.typing import FeaturesDict
+
+logger = logging.getLogger(__name__)
+
+
+class NameRecordsParser(BaseParser):
+    def _parse(self) -> list[list[int | str]]:
+        num = self.read_value()
+        result = []
+        for _ in range(num):
+            nameID = self.read_value()
+            platID = self.read_value()
+            encID = self.read_value()
+            langID = self.read_value()
+            name_length = self.read_value()
+            name_codes = [self.read_value() for _ in range(name_length)]
+            name = ""
+            for c in name_codes:
+                try:
+                    char = chr(c)
+                    # Fix platform-specific encodings for Mac
+                    if platID == 1 and encID == 0:
+                        # TODO: Remove default arguments when Python < 3.11 is dropped
+                        char = c.to_bytes(length=1, byteorder="big").decode("macroman")
+                except ValueError:
+                    char = "\ufeff"
+                name += char
+            result.append([nameID, platID, encID, langID, name])
+
+        return result
+
+
+class OpenTypeStringParser(BaseParser):
+    """
+    A parser that reads data as a strings and returns it as a list.
+    """
+
+    @staticmethod
+    def build_fea_dict(data: list[str]) -> FeaturesDict:
+        """
+        Parse the OpenType feature code into a structure that is more manageable. This
+        is not used in vfbLib, but may be used in code that wants to read OpenType
+        features from a VFB.
+
+        Args:
+            data (list[str]): The OpenType feature code (AFDKO syntax) as a list of
+                lines.
+
+        Returns:
+            dict[str, list[dict]]: The feature code split into prefix and actual
+                features.
+        """
+        fea = FeaturesDict(prefix=[], features=[])
+        prefix: list[str] = []
+        feature: list[str] = []
+        tag = ""
+        in_prefix = True
+        for line in data:
+            if in_prefix:
+                if s := search(r"\s*feature\s*([a-z0-9]{4})\s*", line):
+                    # The first feature ...
+                    # Flush the prefix
+                    if prefix:
+                        fea["prefix"].extend(prefix)
+                    in_prefix = False
+                    # Extract the feature tag of the first feature
+                    tag = s.groups()[0]
+                    if "#" in line:
+                        _, comment = line.split("#", 1)
+                        feature.append(f"# {comment.strip()}")
+                    # feature.append(f"# {line}")
+                else:
+                    prefix.append(line)
+            else:
+                if s := search(r"\s*feature\s*([a-z0-9]{4})\s*", line):
+                    if tag == "aalt":
+                        feature.append(line)
+                    else:
+                        # Extract the feature tag of the next feature
+                        tag = s.groups()[0]
+                        if "#" in line:
+                            _, comment = line.split("#", 1)
+                            feature.append(f"# {comment.strip()}")
+                        # feature.append(f"# {line}")
+                elif tag:
+                    if search(r"\s*\}\s*%s\s*;" % tag, line):
+                        # feature.append(f"# {line}")
+                        fea["features"].append({"tag": tag, "code": feature})
+                        feature = []
+                        tag = ""
+                    else:
+                        feature.append(line)
+
+        return fea
+
+    def _parse(self) -> list[str]:
+        s = self.read_str_all()
+        # Filter more than 2 consecutive empty lines
+        lines = []
+        c = 0
+        for line in s.splitlines():
+            if line.strip():
+                c = 0
+                lines.append(line)
+            else:
+                if c < 2:
+                    lines.append(line)
+                c += 1
+
+        if len(lines) > 1:
+            # Remove empty lines at the end, except one
+            while not lines[-1]:
+                lines.pop()
+        lines.append("")
+        return lines
+
+
+class StringParser(BaseParser):
+    """
+    A parser that reads data as strings.
+    """
+
+    def _parse(self):
+        return self.read_str_all().strip("\00")
