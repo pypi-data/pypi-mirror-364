15ab6d724ad05978b8be184d2e30833ceafb751e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:33 +0000

    Add package magic5/0.2.0/magic5-0.2.0.tar.gz

---
+++ packages/magic5/magic5-0.2.0.tar.gz/magic5-0.2.0/README.md
@@ -0,0 +1,18 @@
+# magic5
+
+Python package that imports numpy as np, pandas as pd, matplotlib.pyplot as plt, seaborn as sns, and scikit-learn for a quick and easy use
+
+## Install
+
+```bash
+pip install magic5
+```
+
+## Use
+
+```python
+from magic5 import np, pd, plt, sns, sklearn
+
+df = pd.DataFrame(...)
+sns.catplot(...)
+```
---
+++ packages/magic5/magic5-0.2.0.tar.gz/magic5-0.2.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='magic5',
+    version='0.2.0',
+    packages=find_packages(),
+    description='Utility package importing numpy as np, pandas as pd, matplotlib.pyplot as plt, seaborn as sns, and scikit-learn with common aliases',
+    author='Francesco Scolz',
+    author_email='francesco.scolz@gmail.com',
+    url='https://github.com/FreyFly/magic5',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'seaborn',
+        'scikit-learn',
+    ],
+)
