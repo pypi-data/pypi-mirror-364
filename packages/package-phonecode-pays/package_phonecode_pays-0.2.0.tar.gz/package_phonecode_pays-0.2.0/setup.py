c6ecad94c59e32c9d7a8e6ff85d6caeec1926549
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:34:27 +0000

    Add package package-phonecode-pays/0.2.0/package_phonecode_pays-0.2.0.tar.gz

---
+++ packages/package-phonecode-pays/package_phonecode_pays-0.2.0.tar.gz/package_phonecode_pays-0.2.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='package_phonecode_pays',
+    version='0.2.0',
+    packages=find_packages(),
+    include_package_data=True,
+    description='Package Django pour obtenir l’indicatif téléphonique selon le pays',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='ParfaiteSekongo',
+    author_email='p88971582@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Framework :: Django',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=['django>=3.2'],
+)
