a864bb6e3a120db2bce574195591ebdd59e64184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:34:00 +0000

    Add package easyfileio/1.1.3/easyfileio-1.1.3.tar.gz

---
+++ packages/easyfileio/easyfileio-1.1.3.tar.gz/easyfileio-1.1.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='easyfileio',
+    version='1.1.3',
+    packages=find_packages(),
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/plzgivemeajob/easyfileio',
+    description="an easy way to read and write files in python",
+    license='MIT',
+)
