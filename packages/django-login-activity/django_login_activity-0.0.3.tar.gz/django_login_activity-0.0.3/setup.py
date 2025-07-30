8f688ec7a716f5f3d34a970f7d4da2035b27018c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:21:20 +0000

    Add package django-login-activity/0.0.3/django_login_activity-0.0.3.tar.gz

---
+++ packages/django-login-activity/django_login_activity-0.0.3.tar.gz/django_login_activity-0.0.3/setup.py
@@ -0,0 +1,32 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='django-login-activity',
+    version='0.0.3',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'django>=3.2'
+    ],
+    description='Package Django pour enregistrer les connexions et déconnexions des utilisateurs.',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    author='Ton Nom',
+    author_email='ton.email@example.com',
+    url='https://github.com/tonprofil/django-login-activity',
+    classifiers=[
+        'Framework :: Django',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
+    python_requires='>=3.7',
+    entry_points={
+        'console_scripts': [
+            'login-activity = login_activity.cli:main',
+        ],
+    },
+)
