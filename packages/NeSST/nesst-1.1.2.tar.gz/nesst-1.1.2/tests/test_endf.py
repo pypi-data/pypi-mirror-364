b683a2d9035f76d17937d9a240eada69f83e5d63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:54 +0000

    Add package NeSST/1.1.2/nesst-1.1.2.tar.gz

---
+++ packages/NeSST/nesst-1.1.2.tar.gz/nesst-1.1.2/tests/test_endf.py
@@ -0,0 +1,14 @@
+import NeSST as nst
+import numpy as np
+
+
+def test_endf():
+    ENDF_data = nst.sm.retrieve_ENDF_data("H2.json")
+
+    assert ENDF_data["interactions"].total
+    assert ENDF_data["interactions"].elastic
+    assert ENDF_data["interactions"].n2n
+
+    assert ENDF_data["elastic_dxsec"]["legendre"]
+
+    assert np.isclose(ENDF_data["n2n_dxsec"]["Q_react"], -2.224640e6)
