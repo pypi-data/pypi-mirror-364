03b8556a2207372d8953743b25c37e84d93284b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:28 +0000

    Add package spamfilter/2.1.0/spamfilter-2.1.0-py3-none-any.whl

---
+++ packages/spamfilter/spamfilter-2.1.0-py3-none-any.whl/spamfilter/filters/email.py
@@ -0,0 +1,29 @@
+"""
+Module for the email information filter.
+
+More information about this filter can be found in its Class docstring.
+"""
+
+from .regex import Regex
+
+POSSIBLE_MODES = ["normal", "censor"]
+
+
+class Email(Regex):
+    """
+    Check if a string contains an email address.
+
+    - `Email.mode`: how to handle a failing string.
+        - `normal`: fail the string.
+        - `censor`: censor the information.
+    - `Email.regex`: the regex used to check for email addresses.
+    - `Email.replacement`: what regex to replace email addresses with.
+    """
+
+    def __init__(
+        self,
+        regex: str = r"([a-zA-Z0-9+._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)",
+        mode: str = "normal",
+        replacement: str = r"***",
+    ):
+        super().__init__(regex, mode, replacement)
