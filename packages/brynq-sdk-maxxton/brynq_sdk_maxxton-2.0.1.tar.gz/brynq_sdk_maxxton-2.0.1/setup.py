a1f369c73d4c9d0e4b7bd6eaa9b4ad3fe67ebe2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:41 +0000

    Add package brynq-sdk-maxxton/2.0.1/brynq_sdk_maxxton-2.0.1.tar.gz

---
+++ packages/brynq-sdk-maxxton/brynq_sdk_maxxton-2.0.1.tar.gz/brynq_sdk_maxxton-2.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='brynq_sdk_maxxton',
+    version='2.0.1',
+    description='Maxxton wrapper from BrynQ',
+    long_description='Maxxton wrapper from BrynQ',
+    author='BrynQ',
+    author_email='support@brynq.com',
+    packages=find_namespace_packages(include=['brynq_sdk*']),
+    package_data={'brynq_sdk_maxxton': ['templates/*']},
+    license='BrynQ License',
+    install_requires=[
+        'brynq-sdk-brynq>=4,<5',
+    ],
+    zip_safe=False,
+)
