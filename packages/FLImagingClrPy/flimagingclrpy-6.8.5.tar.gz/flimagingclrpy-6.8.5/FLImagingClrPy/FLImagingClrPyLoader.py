4bb7b862c5fe36ad2077d9532adacc4611fd7ccd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:39 +0000

    Add package FLImagingClrPy/6.8.5/flimagingclrpy-6.8.5.tar.gz

---
+++ packages/FLImagingClrPy/flimagingclrpy-6.8.5.tar.gz/flimagingclrpy-6.8.5/FLImagingClrPy/FLImagingClrPyLoader.py
@@ -0,0 +1,23 @@
+# Begin of FLImaging(R) CLR initialization
+
+import clr
+import glob
+import os
+import platform
+
+def load_dll():
+    arch = platform.architecture()[0]  # '32bit' or '64bit'
+
+    if arch == '64bit':
+        dll_pattern = "FLImaging*X64CLR.dll"
+        dll_dir = r'C:/Program Files/FLImaging/FLImaging/BinaryX64/'
+    else:
+        dll_pattern = "FLImaging*X86CLR.dll"
+        dll_dir = r'C:/Program Files/FLImaging/FLImaging/Binary/'
+
+    dllFiles = glob.glob(os.path.join(dll_dir, dll_pattern))
+
+    for path in dllFiles:
+        clr.AddReference(path)
+
+# End of FLImaging(R) CLR initialization
---
+++ packages/FLImagingClrPy/flimagingclrpy-6.8.5.tar.gz/flimagingclrpy-6.8.5/setup.py
@@ -0,0 +1,15 @@
+﻿from setuptools import setup, find_packages
+
+setup(
+    name="FLImagingClrPy",
+    version="6.8.5",
+    packages=find_packages(),
+    install_requires=[
+        "pythonnet>=3.0.5", 
+    ],
+    author="Fourth Logic Incorporated",
+    author_email="support@fourthlogic.co.kr",
+    description="FLImaging(R) CLR for Python",
+    license="MIT",
+    python_requires='>=3.8.0',
+)
