acd9307ca38732f7d584de79d3d571521d68bfcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:07:51 +0000

    Add package nnsight/0.5.0.dev11/nnsight-0.5.0.dev11-cp313-cp313-manylinux1_x86_64.manylinux_2_28_x86_64.manylinux_2_5_x86_64.whl

---
+++ packages/nnsight/nnsight-0.5.0.dev11-cp313-cp313-manylinux1_x86_64.manylinux_2_28_x86_64.manylinux_2_5_x86_64.whl/nnsight/schema/request.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+
+import io
+import zlib
+from typing import TYPE_CHECKING, Any, Callable, Dict, List, Union
+
+import dill
+from pydantic import BaseModel, ConfigDict
+from ..intervention.serialization import CustomDillPickler, CustomDillUnpickler
+if TYPE_CHECKING:
+    from .. import NNsight
+    from ..intervention.tracing.tracer import Tracer
+else:
+    Tracer = Any
+
+class RequestModel(BaseModel):
+
+    model_config = ConfigDict(arbitrary_types_allowed=True, protected_namespaces=())
+
+    interventions: Callable
+    tracer: Tracer
+
+    
+    def serialize(self, _zlib:bool) -> bytes:
+        
+        dill.settings['recurse'] = True
+                
+        with io.BytesIO() as data:
+        
+            CustomDillPickler(data).dump(self)
+
+            data.seek(0)
+
+            data = data.read()
+                
+        if _zlib:
+
+            data = zlib.compress(data)
+                
+        return data
+
+    @staticmethod
+    def deserialize(model: "NNsight", request:bytes,  _zlib:bool) -> RequestModel:
+        
+        if _zlib:
+
+            request = zlib.decompress(request)
+
+        with io.BytesIO(request) as data:
+
+            data.seek(0)
+
+            request:RequestModel = CustomDillUnpickler(data, model, None).load()
+        
+        return request
+
+RequestModel.model_rebuild()
