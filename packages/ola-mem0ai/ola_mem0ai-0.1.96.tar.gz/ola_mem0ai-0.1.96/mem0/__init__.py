c961865d31e66cb197ce29bd6844d5acf4b0040a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:16 +0000

    Add package ola-mem0ai/0.1.96/ola_mem0ai-0.1.96.tar.gz

---
+++ packages/ola-mem0ai/ola_mem0ai-0.1.96.tar.gz/ola_mem0ai-0.1.96/mem0/__init__.py
@@ -0,0 +1,8 @@
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version("mem0ai")
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.1.0-dev"  # 开发版本
+from mem0.client.main import AsyncMemoryClient, MemoryClient  # noqa
+from mem0.memory.main import AsyncMemory, Memory  # noqa
