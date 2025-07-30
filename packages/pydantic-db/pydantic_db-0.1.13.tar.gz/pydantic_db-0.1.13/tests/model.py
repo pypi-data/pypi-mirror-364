493ab3d066a7fe990f1b5b40179f3355687bf43c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:09:16 +0000

    Add package pydantic-db/0.1.13/pydantic_db-0.1.13.tar.gz

---
+++ packages/pydantic-db/pydantic_db-0.1.13.tar.gz/pydantic_db-0.1.13/tests/model.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+
+from datetime import datetime
+
+from pydantic_db import Model
+
+
+class ModelA(Model):
+    id: int
+    a: str
+
+
+class ModelB(Model):
+    id: int
+    b: str
+
+
+class ModelC(Model):
+    _eq_excluded_fields = {"updated"}
+
+    id: int
+    c: str
+    updated: datetime
+
+
+class ModelD(Model):
+    _skip_prefix_fields = {"b": "id"}
+
+    id: int
+    d: str
+    a: ModelA | None
+    b: ModelB | None
+
+
+class ModelE(Model):
+    _skip_sortable_fields = {"d__a__id", "d__b__id"}
+    id: int | float  # union not containing model to trigger test branches
+    e: str
+    d: ModelD  # Nested NestedModel
+
+
+class ModelF(Model):
+    id: int
+    models: list[ModelA]
+
+
+class ModelG(Model):
+    id: int
+    models: list[ModelA] | None
