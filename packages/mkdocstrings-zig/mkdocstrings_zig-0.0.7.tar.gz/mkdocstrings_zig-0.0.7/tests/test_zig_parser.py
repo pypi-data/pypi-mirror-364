c568c2ad9f6fc11fe3d45e5919e9d644deebb383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:50 +0000

    Add package mkdocstrings-zig/0.0.7/mkdocstrings_zig-0.0.7.tar.gz

---
+++ packages/mkdocstrings-zig/mkdocstrings_zig-0.0.7.tar.gz/mkdocstrings_zig-0.0.7/tests/test_zig_parser.py
@@ -0,0 +1,124 @@
+from mkdocstrings_handlers.zig._internal.zig_docs_extractor import (
+    _ZigDocsExtractor as ZigDocsExtractor,
+)
+
+
+def test_parser() -> None:
+    zig_code = """
+    //! This is module-level documentation
+    //! It describes the entire file
+    const std = @import("std");
+    const AutoHashMap = std.AutoHashMap;
+
+    /// Adds two numbers.
+    fn add(a: i32, b: i32) i32 {
+        return a + b;
+    }
+
+    /// A constant named PI.
+    const PI = 3.14159;
+
+    /// A 2D point struct.
+    const Point = struct {
+        /// horizontal coordinate
+        x: i32,
+        /// vertical coorinate
+        y: i32,
+
+        /// The top-left position
+        pub const zero: Point = .{ .x = 0, .y = 0 };
+    };
+
+    /// Main function
+    pub fn main() void {
+        std.print("Hello, world!\n");
+    }
+
+    /// Generic structure factory example
+    fn GenericStructure(comptime T: type) type {
+        return struct {
+            /// Contained value
+            value: T,
+        };
+    }
+    """
+
+    parsed = ZigDocsExtractor(zig_code).get_docs()
+    assert parsed == {
+        "doc": "This is module-level documentation\nIt describes the entire file",
+        "children": [
+            {
+                "node_type": "function",
+                "doc": "Adds two numbers.",
+                "name": "add",
+                "signature": "fn add(a: i32, b: i32) i32",
+                "short_signature": "fn add",
+            },
+            {
+                "node_type": "const",
+                "doc": "A constant named PI.",
+                "name": "PI",
+                "short_signature": "const PI",
+            },
+            {
+                "node_type": "struct",
+                "name": "Point",
+                "short_signature": "struct Point",
+                "doc": "A 2D point struct.",
+                "children": [
+                    {
+                        "node_type": "fields",
+                        "children": [
+                            {
+                                "doc": "horizontal coordinate",
+                                "name": "x",
+                                "type": "i32",
+                            },
+                            {
+                                "doc": "vertical coorinate",
+                                "name": "y",
+                                "type": "i32",
+                            },
+                        ],
+                    },
+                    {
+                        "node_type": "const",
+                        "name": "zero",
+                        "doc": "The top-left position",
+                        "short_signature": "pub const zero: Point",
+                    },
+                ],
+            },
+            {
+                "node_type": "function",
+                "name": "main",
+                "doc": "Main function",
+                "signature": "pub fn main() void",
+                "short_signature": "pub fn main",
+            },
+            {
+                "node_type": "function",
+                "name": "GenericStructure",
+                "doc": "Generic structure factory example",
+                "signature": "fn GenericStructure(comptime T: type) type",
+                "short_signature": "fn GenericStructure",
+                "return_struct": {
+                    "node_type": "struct",
+                    "name": "return_struct",
+                    "short_signature": "return struct",
+                    "children": [
+                        {
+                            "node_type": "fields",
+                            "children": [
+                                {
+                                    "doc": "Contained value",
+                                    "name": "value",
+                                    "type": "T",
+                                },
+                            ],
+                        },
+                    ],
+                },
+            },
+        ],
+    }
