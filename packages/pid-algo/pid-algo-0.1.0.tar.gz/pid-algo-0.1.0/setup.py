2d8a89d36d6cbc6cd8121781ee3578d5b2eaddbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:09:29 +0000

    Add package pid-algo/0.1.0/pid-algo-0.1.0.tar.gz

---
+++ packages/pid-algo/pid-algo-0.1.0.tar.gz/pid-algo-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pid-algo',
+    version='0.1.0',
+    packages=find_packages(),
+    description='AI-based visibility and media score algorithms',
+    author='Your Name',
+    author_email='abdulhoodiymuhiddinov2@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[],
+    python_requires='>=3.6',
+)
