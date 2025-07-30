d634dc4458d18f3decedc11643b695d582e0d3e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:33 +0000

    Add package omlish/0.0.0.dev369/omlish-0.0.0.dev369-py3-none-any.whl

---
+++ packages/omlish/omlish-0.0.0.dev369-py3-none-any.whl/omlish/lite/dataclasses.py
@@ -0,0 +1,85 @@
+# ruff: noqa: UP006 UP007 UP045
+import dataclasses as dc
+import typing as ta
+
+
+##
+
+
+def is_immediate_dataclass(cls: type) -> bool:
+    if not isinstance(cls, type):
+        raise TypeError(cls)
+    return dc._FIELDS in cls.__dict__  # type: ignore[attr-defined]  # noqa
+
+
+##
+
+
+def dataclass_cache_hash(
+        *,
+        cached_hash_attr: str = '__dataclass_hash__',
+):
+    def inner(cls):
+        if not isinstance(cls, type) and dc.is_dataclass(cls):
+            raise TypeError(cls)
+
+        if (
+                cls.__hash__ is object.__hash__ or
+                '__hash__' not in cls.__dict__
+        ):
+            raise TypeError(cls)
+
+        real_hash = cls.__hash__
+
+        def cached_hash(self) -> int:
+            try:
+                return object.__getattribute__(self, cached_hash_attr)
+            except AttributeError:
+                object.__setattr__(self, cached_hash_attr, h := real_hash(self))  # type: ignore[call-arg]
+            return h
+
+        cls.__hash__ = cached_hash  # type: ignore[method-assign]
+
+        return cls
+
+    return inner
+
+
+##
+
+
+def dataclass_maybe_post_init(sup: ta.Any) -> bool:
+    if not isinstance(sup, super):
+        raise TypeError(sup)
+    try:
+        fn = sup.__post_init__  # type: ignore
+    except AttributeError:
+        return False
+    fn()
+    return True
+
+
+##
+
+
+def dataclass_repr_filtered(
+        obj: ta.Any,
+        fn: ta.Callable[[ta.Any, dc.Field, ta.Any], bool],
+) -> str:
+    return (
+        f'{obj.__class__.__qualname__}(' +
+        ', '.join([
+            f'{f.name}={v!r}'
+            for f in dc.fields(obj)
+            if fn(obj, f, v := getattr(obj, f.name))
+        ]) +
+        ')'
+    )
+
+
+def dataclass_repr_omit_none(obj: ta.Any) -> str:
+    return dataclass_repr_filtered(obj, lambda o, f, v: v is not None)
+
+
+def dataclass_repr_omit_falsey(obj: ta.Any) -> str:
+    return dataclass_repr_filtered(obj, lambda o, f, v: bool(v))
