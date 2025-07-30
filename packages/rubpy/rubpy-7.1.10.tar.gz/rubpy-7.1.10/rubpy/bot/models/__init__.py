ddb5ea40875b450efbb55dabe807358e4787aa8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:30:27 +0000

    Add package rubpy/7.1.10/rubpy-7.1.10.tar.gz

---
+++ packages/rubpy/rubpy-7.1.10.tar.gz/rubpy-7.1.10/rubpy/bot/models/__init__.py
@@ -0,0 +1,40 @@
+from .button import Button
+from .keypad_row import KeypadRow
+from .keypad import Keypad
+from .inline_message import InlineMessage
+from .update import Update
+from .file import File
+from .bot import Bot
+from .message import (
+    Message,
+    MessageId,
+    ForwardedFrom,
+    Location,
+    AuxData,
+    PollStatus,
+    Poll,
+    Sticker,
+    ContactMessage,
+    LiveLocation
+)
+
+
+__all__ = [
+    "Button",
+    "KeypadRow",
+    "Keypad",
+    "InlineMessage",
+    "Message",
+    "Update",
+    "MessageId",
+    "File",
+    "Bot",
+    "ForwardedFrom",
+    "Location",
+    "AuxData",
+    "PollStatus",
+    "Poll",
+    "Sticker",
+    "ContactMessage",
+    "LiveLocation"
+]
