13d72e341d410a1e9f6937e4c190c48947113e10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:07 +0000

    Add package diffpy.structure/3.3.1rc1/diffpy_structure-3.3.1rc1-py3-none-any.whl

---
+++ packages/diffpy.structure/diffpy_structure-3.3.1rc1-py3-none-any.whl/diffpy/structure/expansion/shapeutils.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+##############################################################################
+#
+# diffpy.structure  by DANSE Diffraction group
+#                   Simon J. L. Billinge
+#                   (c) 2008 trustees of the Michigan State University.
+#                   All rights reserved.
+#
+# File coded by:    Chris Farrow
+#
+# See AUTHORS.txt for a list of people who contributed.
+# See LICENSE_DANSE.txt for license information.
+#
+##############################################################################
+"""Utilities for making shapes."""
+
+
+def findCenter(S):
+    """Find the approximate center `Atom` of a `Structure`.
+
+    The center of the `Structure` is the `Atom` closest to ``(0.5, 0.5, 0.5)``.
+
+    Parameters
+    ----------
+    S : Structure
+        A `Structure` instance.
+
+    Returns
+    -------
+    int
+        The index of the center `Atom`.
+    """
+    best = -1
+    bestd = len(S)
+    center = [0.5, 0.5, 0.5]  # the canonical center
+
+    for i in range(len(S)):
+        d = S.lattice.dist(S[i].xyz, center)
+        if d < bestd:
+            bestd = d
+            best = i
+
+    return best
