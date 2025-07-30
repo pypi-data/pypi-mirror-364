126686d0df479f435c29cefac9264405314b650e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:24 +0000

    Add package rsvp/0.1.3/rsvp-0.1.3.tar.gz

---
+++ packages/rsvp/rsvp-0.1.3.tar.gz/rsvp-0.1.3/README.md
@@ -0,0 +1,37 @@
+# RSVP
+
+RSVP is a [Python](https://python.org/) library for message based programming.
+
+[![Python package](https://github.com/tom65536/rsvp/actions/workflows/python-package.yml/badge.svg)](https://github.com/tom65536/rsvp/actions/workflows/python-package.yml)
+![GitHub License](https://img.shields.io/github/license/tom65536/rsvp)
+![Python Version from PEP 621 TOML](https://img.shields.io/python/required-version-toml?tomlFilePath=https%3A%2F%2Fraw.githubusercontent.com%2Ftom65536%2Frsvp%2Frefs%2Fheads%2Fmain%2Fpyproject.toml)
+![OSS Lifecycle](https://img.shields.io/osslifecycle?file_url=https%3A%2F%2Fraw.githubusercontent.com%2Ftom65536%2Frsvp%2Frefs%2Fheads%2Fmain%2FOSSMETADATA)
+[![pre-commit](https://img.shields.io/badge/pre--commit-enabled-brightgreen?logo=pre-commit)](https://github.com/pre-commit/pre-commit)
+[![Checked with pyright](https://microsoft.github.io/pyright/img/pyright_badge.svg)](https://microsoft.github.io/pyright/)
+[![Linter: Ruff](https://img.shields.io/badge/Linter-Ruff-brightgreen?style=flat-square)](https://github.com/charliermarsh/ruff)
+
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install RSVP.
+
+```bash
+pip install rsvp
+```
+
+## Usage
+
+```python
+import rsvp
+
+# ... more to come
+```
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
+## Project Status
+
+Early development
+
---
+++ packages/rsvp/rsvp-0.1.3.tar.gz/rsvp-0.1.3/src/rsvp/core.py
@@ -0,0 +1,131 @@
+"""Core dispatch mechanism."""
+
+import asyncio
+import inspect
+from collections.abc import Sequence
+from typing import (
+   Any,
+   Protocol,
+   TypeAlias,
+   TypeGuard,
+   Union,
+)
+
+
+Message: TypeAlias = Union[object, Sequence[object]]
+
+
+class Responsive(Protocol):
+   """Protocol for objects with ``react`` field."""
+
+   react: Any
+
+
+def is_responsive(obj: object) -> TypeGuard[Responsive]:
+   """Check whether ``obj`` has a ``reactaa member."""
+   return hasattr(obj, 'react')
+
+
+class Dispatcher:
+   """Message dispatcher.
+
+   The dispatcher carries out the following steps:
+
+   - Receive and enqueue messages.
+   - Dequeue one message.
+   - Determine ``react`` method.
+   - Collect messages from running ``react`` method.
+   - Repeat or exit.
+
+   :param maxsize: passed to queue
+   """
+
+   def __init__(self, /, maxsize: int = 0) -> None:
+      """Initialize the dispatcher."""
+      self._message_queue: asyncio.Queue[Message] = asyncio.Queue(maxsize=maxsize)
+      self._loop_task: asyncio.Task | None = None
+
+   async def dispatch(self, message: Message) -> None:
+      """Dispatch a single message.
+
+      :param message: the message for dispatch.
+      """
+      match message:
+         case [receiver, *args]:
+            await self._dispatch(receiver, args)
+         case _:
+            await self._dispatch(message, ())
+
+   async def _dispatch(
+      self,
+      receiver: object,
+      args: Sequence[object],
+   ) -> None:
+      """Dispatch a single message.
+
+      :param receiver: the receiver of the message
+      :param args: the message arguments.
+      """
+      if not is_responsive(receiver):
+         await self.dispose(receiver, args)
+         return
+      react = receiver.react
+
+      if inspect.iscoroutinefunction(react):
+         result = await receiver.react(*args)
+         await self.receive(result)
+
+      elif inspect.isasyncgenfunction(react):
+         async for message in receiver.react(*args):
+            await self.receive(message)
+
+      else:
+         result = receiver.react(*args)
+         if inspect.isgenerator(result):
+            for message in result:
+               await self.receive(message)
+         else:
+            await self.receive(result)
+
+   async def receive(self, message: Message) -> None:
+      """Receive and enqueue a single message.
+
+      :param message: the received message.
+      """
+      await self._message_queue.put(message)
+
+   async def dispose(
+      self,
+      receiver: object,
+      args: Sequence[object],
+   ) -> None:
+      """Dispose a message without ``react`` method.
+
+      Feel free to override this method in order
+      to change behavior.
+
+      :param receiver: the receiver of the message
+      :param args: the message arguments.
+      """
+      pass
+
+   def start(self) -> bool:
+      """Start the dispatcher loop as a new task.
+
+      If this dispatcher has already a loop running
+      this method does nothing.
+
+      :return: whether a new loop was started
+      """
+      if self._loop_task is not None:
+         return False
+      coro = self._loop()
+      self._loop_task = asyncio.create_task(coro)
+      return True
+
+   async def _loop(self) -> None:
+      """Run the dispatcher loop."""
+      async with asyncio.TaskGroup() as tg:
+         while True:
+            message = await self._message_queue.get()
+            tg.create_task(self.dispatch(message))
