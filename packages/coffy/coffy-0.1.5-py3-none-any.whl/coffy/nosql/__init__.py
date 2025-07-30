a784ebb722187be18749ce88d4079bdf193d4f2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:10:17 +0000

    Add package coffy/0.1.5/coffy-0.1.5-py3-none-any.whl

---
+++ packages/coffy/coffy-0.1.5-py3-none-any.whl/coffy/nosql/__init__.py
@@ -0,0 +1,7 @@
+# coffy/nosql/__init__.py
+# author: nsarathy
+
+from .engine import CollectionManager
+
+def db(collection_name: str, path: str = None):
+    return CollectionManager(collection_name, path=path)
