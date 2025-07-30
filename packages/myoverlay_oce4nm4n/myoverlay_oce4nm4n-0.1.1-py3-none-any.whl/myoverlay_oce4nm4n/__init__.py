37a568c5e7d1c2490a4afc2ba0824c97cd5d91bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:24 +0000

    Add package myoverlay_oce4nm4n/0.1.1/myoverlay_oce4nm4n-0.1.1-py3-none-any.whl

---
+++ packages/myoverlay_oce4nm4n/myoverlay_oce4nm4n-0.1.1-py3-none-any.whl/myoverlay_oce4nm4n/__init__.py
@@ -0,0 +1,4 @@
+from .side_panel_app import SidePanelApp
+
+__version__ = "0.1.0"
+__all__ = ["SidePanelApp"]
---
+++ packages/myoverlay_oce4nm4n/myoverlay_oce4nm4n-0.1.1-py3-none-any.whl/myoverlay_oce4nm4n/main.py
@@ -0,0 +1,6 @@
+# myoverlay_oce4nm4n/main.py
+
+from .side_panel_app import SidePanelApp
+
+def main():
+    SidePanelApp().run()
