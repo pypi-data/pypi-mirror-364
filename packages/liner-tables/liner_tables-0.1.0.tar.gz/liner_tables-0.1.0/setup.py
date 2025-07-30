ea7c3665c5e57aced448c68edab2b34bd5d5acc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:38 +0000

    Add package liner-tables/0.1.0/liner_tables-0.1.0.tar.gz

---
+++ packages/liner-tables/liner_tables-0.1.0.tar.gz/liner_tables-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='liner_tables',
+    version='0.1.0',
+    description='Print table',
+    author='MrSanZz',
+    author_email='drownedmrsanzz@proton.me',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
