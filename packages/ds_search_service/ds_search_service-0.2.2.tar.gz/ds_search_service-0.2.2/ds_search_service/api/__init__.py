140fb3d70859987248f5c935da4bbc8d093c5d14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:55 +0000

    Add package ds_search_service/0.2.2/ds_search_service-0.2.2.tar.gz

---
+++ packages/ds_search_service/ds_search_service-0.2.2.tar.gz/ds_search_service-0.2.2/ds_search_service/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from ds_search_service.api.decentralized_search_api import DecentralizedSearchApi
+from ds_search_service.api.local_search_api import LocalSearchApi
+from ds_search_service.api.default_api import DefaultApi
+
