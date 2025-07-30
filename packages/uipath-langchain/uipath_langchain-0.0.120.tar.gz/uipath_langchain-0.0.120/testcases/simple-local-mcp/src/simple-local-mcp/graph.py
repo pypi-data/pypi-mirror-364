8cf4af6d7e275df4fd203d4365e84ea521892b13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:34:05 +0000

    Add package uipath-langchain/0.0.120/uipath_langchain-0.0.120.tar.gz

---
+++ packages/uipath-langchain/uipath_langchain-0.0.120.tar.gz/uipath_langchain-0.0.120/testcases/simple-local-mcp/src/simple-local-mcp/graph.py
@@ -0,0 +1,26 @@
+import sys
+import os
+from contextlib import asynccontextmanager
+
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from langgraph.prebuilt import create_react_agent
+from uipath_langchain.chat import UiPathAzureChatOpenAI
+
+model = UiPathAzureChatOpenAI(model="gpt-4o-2024-08-06")
+
+@asynccontextmanager
+async def make_graph():
+    client = MultiServerMCPClient({
+            "math": {
+                "command": sys.executable,
+                "args": ["src/simple-local-mcp/math_server.py"],
+                "transport": "stdio",
+            },
+            "weather": {
+                "command": sys.executable,
+                "args": ["src/simple-local-mcp/weather_server.py"],
+                "transport": "stdio",
+            },
+        })
+    agent = create_react_agent(model, await client.get_tools())
+    yield agent
