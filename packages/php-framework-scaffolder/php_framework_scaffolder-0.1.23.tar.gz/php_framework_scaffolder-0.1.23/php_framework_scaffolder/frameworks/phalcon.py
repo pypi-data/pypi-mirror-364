1aad2cda292415f33f830adccdcef70867127c44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:02 +0000

    Add package php-framework-scaffolder/0.1.23/php_framework_scaffolder-0.1.23.tar.gz

---
+++ packages/php-framework-scaffolder/php_framework_scaffolder-0.1.23.tar.gz/php_framework_scaffolder-0.1.23/php_framework_scaffolder/frameworks/phalcon.py
@@ -0,0 +1,10 @@
+from .base import BaseFrameworkSetup
+from php_framework_detector.core.models import FrameworkType
+
+
+class PhalconSetup(BaseFrameworkSetup):
+    def __init__(self):
+        super().__init__(FrameworkType.PHALCON)
+
+    def get_setup_commands(self):
+        raise NotImplementedError("Not implemented")
