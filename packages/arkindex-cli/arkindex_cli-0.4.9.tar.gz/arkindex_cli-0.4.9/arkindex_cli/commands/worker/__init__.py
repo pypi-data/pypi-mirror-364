ff5e6a14daf2a0a6dc51b7a9e6fade2f6fc120f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:47 +0000

    Add package arkindex-cli/0.4.9/arkindex_cli-0.4.9.tar.gz

---
+++ packages/arkindex-cli/arkindex_cli-0.4.9.tar.gz/arkindex_cli-0.4.9/arkindex_cli/commands/worker/__init__.py
@@ -0,0 +1,16 @@
+from arkindex_cli.commands.worker.publish import add_publish_parser
+
+
+def add_worker_parser(subcommands) -> None:
+    export_parser = subcommands.add_parser(
+        "worker",
+        description="Manage workers and their versions.",
+        help="Manage workers and their versions.",
+    )
+    subparsers = export_parser.add_subparsers()
+    add_publish_parser(subparsers)
+
+    def subcommand_required(*args, **kwargs):
+        export_parser.error("A subcommand is required.")
+
+    export_parser.set_defaults(func=subcommand_required)
