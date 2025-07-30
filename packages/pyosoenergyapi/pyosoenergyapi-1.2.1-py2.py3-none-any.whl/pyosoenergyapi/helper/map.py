1f1ace5f4bee113264c777bf9f41246d4cd1eb07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:08 +0000

    Add package pyosoenergyapi/1.2.1/pyosoenergyapi-1.2.1-py2.py3-none-any.whl

---
+++ packages/pyosoenergyapi/pyosoenergyapi-1.2.1-py2.py3-none-any.whl/pyosoenergyapi/helper/map.py
@@ -0,0 +1,13 @@
+"""Dot notation for dictionary."""
+
+
+class Map(dict):
+    """dot.notation access to dictionary attributes.
+
+    Args:
+        dict (dict): dictionary to map.
+    """
+
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
