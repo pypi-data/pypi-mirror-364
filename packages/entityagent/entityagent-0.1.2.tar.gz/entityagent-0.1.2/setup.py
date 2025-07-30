e454b3bf52e858a9d7d6520650d7db050e2d8351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:45 +0000

    Add package entityagent/0.1.2/entityagent-0.1.2.tar.gz

---
+++ packages/entityagent/entityagent-0.1.2.tar.gz/entityagent-0.1.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='entityagent',
+    version='0.1.2',
+    description='Entity Agent: An AI assistant with platform interaction capabilities',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    author='Prakash Sellathurai',
+    author_email='prakashsellathurai@gmail.com',
+    url='https://github.com/prakashsellathurai/entity',
+    packages=find_packages(),
+    install_requires=[
+        'psutil',
+    ],
+    python_requires='>=3.8',
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'entity-agent=entityAgent.agent:runtime',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
+    project_urls={
+        'Source': 'https://github.com/prakashsellathurai/entity',
+        'Tracker': 'https://github.com/prakashsellathurai/entity/issues',
+    },
+)
