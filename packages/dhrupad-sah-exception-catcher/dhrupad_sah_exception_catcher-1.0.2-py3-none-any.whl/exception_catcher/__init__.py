78b8076dc7be1b0ada2cf0a6bcfb822e3577dceb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:59 +0000

    Add package dhrupad-sah-exception-catcher/1.0.2/dhrupad_sah_exception_catcher-1.0.2-py3-none-any.whl

---
+++ packages/dhrupad-sah-exception-catcher/dhrupad_sah_exception_catcher-1.0.2-py3-none-any.whl/exception_catcher/__init__.py
@@ -0,0 +1,39 @@
+"""
+dhrupad-sah-exception-catcher
+
+Automatically catch and report exceptions to Mira Sentinel with rich context
+and log integration for enhanced debugging capabilities.
+"""
+
+from .exception_catcher import MiraSentinelExceptionCatcher
+from .types import (
+    MiraSentinelConfig,
+    ExceptionContext,
+    SentinelResponse,
+    ReportOptions,
+)
+from .auto_init import auto_initialize, create_exception_catcher
+
+__version__ = "1.0.1"
+__all__ = [
+    "MiraSentinelExceptionCatcher",
+    "MiraSentinelConfig", 
+    "ExceptionContext",
+    "SentinelResponse",
+    "ReportOptions",
+    "auto_initialize",
+    "create_exception_catcher",
+]
+
+# Try to import framework integrations
+try:
+    from .fastapi_middleware import FastAPIMiraSentinel, setup_fastapi_mira_sentinel
+    __all__.extend(["FastAPIMiraSentinel", "setup_fastapi_mira_sentinel"])
+except ImportError:
+    pass
+
+try:
+    from .flask_middleware import FlaskMiraSentinel, setup_flask_mira_sentinel
+    __all__.extend(["FlaskMiraSentinel", "setup_flask_mira_sentinel"])
+except ImportError:
+    pass
