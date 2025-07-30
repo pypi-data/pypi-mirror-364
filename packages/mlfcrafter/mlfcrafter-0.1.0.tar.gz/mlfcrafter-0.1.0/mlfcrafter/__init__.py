3cf0149ffd4b26197a8081efcc436e47d4bd7c38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:06:42 +0000

    Add package mlfcrafter/0.1.0/mlfcrafter-0.1.0.tar.gz

---
+++ packages/mlfcrafter/mlfcrafter-0.1.0.tar.gz/mlfcrafter-0.1.0/mlfcrafter/__init__.py
@@ -0,0 +1,26 @@
+from .crafters import (
+    CleanerCrafter,
+    DataIngestCrafter,
+    DeployCrafter,
+    ModelCrafter,
+    ScalerCrafter,
+    ScorerCrafter,
+)
+from .flow_chain import MLFChain
+from .utils import setup_logger
+
+__version__ = "0.1.0"
+
+# Setup default logger when MLFCrafter is imported
+_default_logger = setup_logger()
+
+__all__ = [
+    "MLFChain",
+    "DataIngestCrafter",
+    "CleanerCrafter",
+    "ScalerCrafter",
+    "ModelCrafter",
+    "ScorerCrafter",
+    "DeployCrafter",
+    "setup_logger",
+]
