92b5cb2b55155cfbc62680999f085de0597cd479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:14:01 +0000

    Add package whiffle-client/0.4.20/whiffle_client-0.4.20.tar.gz

---
+++ packages/whiffle-client/whiffle_client-0.4.20.tar.gz/whiffle_client-0.4.20/whiffle_client/wind/mapping/__init__.py
@@ -0,0 +1,5 @@
+from whiffle_client.common.mapping.base import BaseMapping
+from whiffle_client.wind.mapping.turbine_model_specs import TurbineModelSpecsEndpoints
+from whiffle_client.wind.mapping.wind_simulation_tasks import (
+    WindSimulationTaskEndpoints,
+)
