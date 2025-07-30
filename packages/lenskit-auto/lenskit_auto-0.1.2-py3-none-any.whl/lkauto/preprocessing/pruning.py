2dd54750a52132520dc7b40ea47da67f2dfa6f84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:05 +0000

    Add package lenskit-auto/0.1.2/lenskit_auto-0.1.2-py3-none-any.whl

---
+++ packages/lenskit-auto/lenskit_auto-0.1.2-py3-none-any.whl/lkauto/preprocessing/pruning.py
@@ -0,0 +1,61 @@
+from lenskit.data import Dataset, from_interactions_df
+
+
+def min_ratings_per_user(dataset: Dataset, num_ratings: int, count_duplicates: bool = False):
+    """Prune users with less than num_ratings ratings
+
+    Parameters
+    ----------
+    dataset: Dataset
+        LensKit Dataset object containing user-item interactions with ratings
+    num_ratings: int
+        Minimum number of ratings per user
+    count_duplicates: bool = False
+        If True, all ratings are counted, otherwise only unique ratings are counted
+
+    Returns
+    -------
+    Dataset
+        Filtered Dataset with only users meeting the minimum rating threshold
+        the Dataset will contain the columns "user_id", "item_id", "rating"
+    """
+    # get the user statistics from the dataset
+    user_stats = dataset.user_stats()
+    if count_duplicates:
+        valid_users = user_stats[user_stats['count'] >= num_ratings].index  # count: total number of ratings (including duplicates)
+    else:
+        valid_users = user_stats[user_stats['item_count'] >= num_ratings].index  # item_count: number of unique items rated
+    # convert the interaction table to a pandas DataFrame and filter by valid users
+    users_of_interest = dataset.interaction_table(format='pandas', original_ids=True)
+    users_of_interest = users_of_interest[users_of_interest['user_id'].isin(valid_users)]
+    return from_interactions_df(users_of_interest)
+
+
+def max_ratings_per_user(dataset: Dataset, num_ratings: int, count_duplicates: bool = False):
+    """Prune users with more than num_ratings ratings
+
+    Parameters
+    ----------
+    dataset: Dataset
+        LensKit Dataset object containing user-item interactions with ratings
+    num_ratings: int
+        Maximum number of ratings per user
+    count_duplicates: bool = False
+        If True, all ratings are counted, otherwise only unique ratings are counted
+
+    Returns
+    -------
+    Dataset
+        Filtered Dataset with only users meeting the minimum rating threshold
+        the Dataset will contain the columns "user_id", "item_id", "rating"
+    """
+
+    user_stats = dataset.user_stats()
+    if count_duplicates:
+        valid_users = user_stats[user_stats['count'] <= num_ratings].index  # count: total number of ratings (including duplicates)
+    else:
+        valid_users = user_stats[user_stats['item_count'] <= num_ratings].index  # item_count: number of unique items rated
+    # convert the interaction table to a pandas DataFrame and filter by valid users
+    users_of_interest = dataset.interaction_table(format='pandas', original_ids=True)
+    users_of_interest = users_of_interest[users_of_interest['user_id'].isin(valid_users)]
+    return from_interactions_df(users_of_interest)
---
+++ packages/lenskit-auto/lenskit_auto-0.1.2-py3-none-any.whl/lkauto/utils/validation_split.py
@@ -0,0 +1,153 @@
+from typing import Iterator
+
+from lenskit.splitting import crossfold_records, crossfold_users, sample_records, SampleFrac, TTSplit
+from lenskit.data import Dataset
+
+
+def validation_split(data: Dataset, strategy: str = 'user_based', num_folds: int = 1,
+                     frac: float = 0.25, random_state=42) -> Iterator[TTSplit]:
+    """
+    Returns the Train-Test-Split for the given Dataset
+
+    Parameters
+    ----------
+    data : Dataset
+        Lenskit Dataset with the data to be split.
+    strategy : str
+        cross validation strategy (either "user_based" or "row_based")
+    num_folds : int
+        number of folds for the validation split cross validation
+    frac : float
+        fraction of the dataset to be used for the validation split. If num_folds > 1, the fraction value
+        will be ignored.
+    random_state : int
+        random state for the validation split
+
+    Returns
+    -------
+    Iterator[TTSplit]
+        The Train-Test-Split for the given Dataset
+    """
+    # decide which validation split strategy to use
+    if strategy == 'user_based':
+        return user_based_validation_split(data=data, num_folds=num_folds, frac=frac, random_state=random_state)
+    elif strategy == 'row_based':
+        return row_based_validation_split(data=data, num_folds=num_folds, frac=frac, random_state=random_state)
+    else:
+        raise ValueError(f"Unknown validation split strategy: {strategy}")
+
+
+def row_based_validation_split(data: Dataset, num_folds: int = 1, frac: float = 0.25, random_state=42) -> Iterator[TTSplit]:
+    """
+    Returns a Train-Test-Split for the given data.
+
+    Parameters
+    ----------
+    data : Dataset
+        Lenskit Dataset with the data to be split.
+    num_folds : int
+        number of folds for the validation split cross validation
+    frac : float
+        fraction of the dataset to be used for the validation split. If num_folds > 1, the fraction value
+        will be ignored.
+    random_state : int
+        random state for the validation split
+
+    Returns
+    -------
+    Iterator[TTSPlit]
+        Train-Tet-Split for the given data.
+    """
+
+    if num_folds < 2:
+        return __holdout_validation_split(data=data, frac=frac, random_state=random_state)
+    else:
+        return __row_based_k_fold_validation_split(data=data, num_folds=num_folds, random_state=random_state)
+
+
+def user_based_validation_split(data: Dataset, num_folds: int = 1, frac: float = 0.25, random_state=42) -> Iterator[TTSplit]:
+    """
+    Parameters
+    ----------
+    data : Dataset
+        Lenskit Dataset with the data to be split.
+    num_folds : int
+        number of folds for the validation split cross validation
+    frac : float
+        fraction of the dataset to be used for the validation split. If num_folds > 1, the fraction value
+        will be ignored.
+    random_state : int
+        random state for the validation split
+
+    Returns
+    -------
+    Iterator[TTSplit]
+        Train-Test-Split for the given data.
+    """
+
+    if num_folds < 2:
+        return __holdout_validation_split(data=data, frac=frac, random_state=random_state)
+    else:
+        return __user_based_crossfold_validation_split(data=data, num_folds=num_folds)
+
+
+def __holdout_validation_split(data: Dataset, frac: float, random_state=42):
+    """
+    Returns a Train-Test-Split for the given data.
+
+    Parameters
+    ----------
+    data : Dataset
+        Lenskit Dataset with the data to be split.
+    frac : float
+        fraction of the dataset to be used for the validation split. If num_folds > 1, the fraction value
+        will be ignored.
+    random_state : int
+        random state for the validation split
+
+    Returns
+    -------
+    Iterator[TTSplit]
+        Train-Test-Split for the given data. Should only contain one fold.
+    """
+
+    splits = sample_records(data=data, size=int(data.interaction_count * frac), rng=random_state)
+
+    if hasattr(splits, "_iter__"):
+        return splits
+    else:
+        return iter([splits])
+
+
+def __row_based_k_fold_validation_split(data: Dataset, num_folds: int, random_state):
+    """
+    Returns a Train-Test-Split for the given data.
+
+    Parameters
+    ----------
+    data : Dataset
+        Lenskit Dataset with the data to be split.
+    """
+
+    splits = crossfold_records(data=data, partitions=num_folds, rng=random_state)
+    return splits
+
+
+def __user_based_crossfold_validation_split(data: Dataset, num_folds) -> Iterator[TTSplit]:
+    """
+    Returns a Train-Test-Split for the given data.
+
+    Parameters
+    ----------
+    data : Dataset
+        Pandas Dataframe with the data to be split.
+    num_folds : int
+        number of folds for the validation split cross validation
+
+    Returns
+    -------
+    Iterator[TTSplit]
+        Train-Test-Split for the given data.
+    """
+
+    return crossfold_users(data=data, partitions=num_folds, method=SampleFrac(0.2))
