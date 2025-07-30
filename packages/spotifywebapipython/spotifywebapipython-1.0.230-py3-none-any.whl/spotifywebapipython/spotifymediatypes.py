aff3653c469284f158f3caad6cb4218a51406cf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:08:01 +0000

    Add package spotifywebapipython/1.0.230/spotifywebapipython-1.0.230-py3-none-any.whl

---
+++ packages/spotifywebapipython/spotifywebapipython-1.0.230-py3-none-any.whl/spotifywebapipython/spotifymediatypes.py
@@ -0,0 +1,41 @@
+from enum import StrEnum
+
+class SpotifyMediaTypes(StrEnum):
+    """
+    Spotify media types enumeration.
+    """
+
+    ALBUM = "album"
+    """
+    Album media type.
+    """
+
+    ARTIST = "artist"
+    """
+    Artist media type.
+    """
+
+    AUDDIOBOOK = "audiobook"
+    """
+    Audiobook media type.
+    """
+
+    EPISODE = "episode"
+    """
+    Episode media type.
+    """
+
+    PLAYLIST = "playlist"
+    """
+    Playlist media type.
+    """
+
+    SHOW = "show"
+    """
+    Show media type.
+    """
+
+    TRACK = "track"
+    """
+    Track media type.
+    """
