e044cf679b28222b2c4a8fec6f9a3e7591bd3edb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:21 +0000

    Add package casm/0.9.1/casm-0.9.1-py2.py3-none-any.whl

---
+++ packages/casm/casm-0.9.1-py2.py3-none-any.whl/casm/attribute.py
@@ -0,0 +1,10 @@
+# coding:utf-8
+
+__project__ = "casm"
+__version__ = "0.9.1"
+__urlhome__ = "https://github.com/podboy/casm/"
+__description__ = "container assembler"
+
+# author
+__author__ = "Mingzhe Zou"
+__author_email__ = "zoumingzhe@outlook.com"
