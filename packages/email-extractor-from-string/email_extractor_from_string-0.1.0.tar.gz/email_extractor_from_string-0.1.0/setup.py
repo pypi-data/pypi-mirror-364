eec12a4e7d515326ec278fe3d2bae6d85a50fb8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:42 +0000

    Add package email-extractor-from-string/0.1.0/email_extractor_from_string-0.1.0.tar.gz

---
+++ packages/email-extractor-from-string/email_extractor_from_string-0.1.0.tar.gz/email_extractor_from_string-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='email_extractor_from_string',
+    version='0.1.0',
+    description='A simple email extraction module',
+    author='Nisamudheen Mamballi',
+    author_email='nizanizam129@gmail.com',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.0',
+)
