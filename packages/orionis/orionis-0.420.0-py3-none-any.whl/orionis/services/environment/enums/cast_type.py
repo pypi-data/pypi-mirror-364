dd3d556312660a081da495241691b76f24065b37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:56 +0000

    Add package orionis/0.420.0/orionis-0.420.0-py3-none-any.whl

---
+++ packages/orionis/orionis-0.420.0-py3-none-any.whl/orionis/services/environment/enums/cast_type.py
@@ -0,0 +1,42 @@
+from enum import Enum
+
+class EnvCastType(Enum):
+    """
+    Enum representing supported environment variable cast types.
+
+    Attributes
+    ----------
+    PATH : EnvCastType
+        Cast to a file system path.
+    STR : EnvCastType
+        Cast to a string.
+    INT : EnvCastType
+        Cast to an integer.
+    FLOAT : EnvCastType
+        Cast to a floating-point number.
+    BOOL : EnvCastType
+        Cast to a boolean value.
+    LIST : EnvCastType
+        Cast to a list.
+    DICT : EnvCastType
+        Cast to a dictionary.
+    TUPLE : EnvCastType
+        Cast to a tuple.
+    SET : EnvCastType
+        Cast to a set.
+
+    Returns
+    -------
+    EnvCastType
+        An enumeration member representing the desired cast type.
+    """
+
+    PATH = 'path'   # Represents a file system path
+    STR = 'str'     # Represents a string type
+    INT = 'int'     # Represents an integer type
+    FLOAT = 'float' # Represents a floating-point type
+    BOOL = 'bool'   # Represents a boolean type
+    LIST = 'list'   # Represents a list type
+    DICT = 'dict'   # Represents a dictionary type
+    TUPLE = 'tuple' # Represents a tuple type
+    SET = 'set'     # Represents a set type
