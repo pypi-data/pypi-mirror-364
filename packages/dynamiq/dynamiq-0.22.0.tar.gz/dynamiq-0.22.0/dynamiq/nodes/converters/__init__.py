40c83d84a9f739963dacc5f8063de6aab6f2bf93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:40 +0000

    Add package dynamiq/0.22.0/dynamiq-0.22.0.tar.gz

---
+++ packages/dynamiq/dynamiq-0.22.0.tar.gz/dynamiq-0.22.0/dynamiq/nodes/converters/__init__.py
@@ -0,0 +1,7 @@
+from .csv import CSVConverter
+from .docx import DOCXFileConverter
+from .html import HTMLConverter
+from .llm_text_extractor import LLMImageConverter, LLMPDFConverter
+from .pptx import PPTXFileConverter
+from .pypdf import PyPDFConverter
+from .unstructured import UnstructuredFileConverter
