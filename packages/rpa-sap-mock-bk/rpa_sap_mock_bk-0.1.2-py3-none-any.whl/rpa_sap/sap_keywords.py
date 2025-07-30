6d81a6be7b21e10a142b2035da1b747bd4d43bfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:27 +0000

    Add package rpa-sap-mock-bk/0.1.2/rpa_sap_mock_bk-0.1.2-py3-none-any.whl

---
+++ packages/rpa-sap-mock-bk/rpa_sap_mock_bk-0.1.2-py3-none-any.whl/rpa_sap/sap_keywords.py
@@ -0,0 +1,31 @@
+from robot.api.deco import keyword
+from .sap_client import SAPClient
+from .auth import load_sap_token_from_file
+import json
+
+class SAPKeywords:
+    def __init__(self):
+        self.client = None
+
+    @keyword("Connect To SAP System")
+    def connect_from_file(self, base_url: str, token_file_path: str, verify_ssl: bool = False):
+        access_token = load_sap_token_from_file(token_file_path)
+        self.client = SAPClient(base_url, access_token)
+
+    @keyword("Get Business Partner By ID")
+    def get_business_partner(self, partner_id: str):
+        return self.client.get_business_partner(partner_id)
+
+    @keyword("Create Business Partner Address")
+    def create_address(self, partner_id: str, json_data: str):
+        data = json.loads(json_data)
+        return self.client.create_address(partner_id, data)
+
+    @keyword("Update Business Partner Address")
+    def update_address(self, partner_id: str, address_id: str, json_data: str):
+        data = json.loads(json_data)
+        return self.client.update_address(partner_id, address_id, data)
+
+    @keyword("Delete Business Partner Address")
+    def delete_address(self, partner_id: str, address_id: str):
+        return self.client.delete_address(partner_id, address_id)
