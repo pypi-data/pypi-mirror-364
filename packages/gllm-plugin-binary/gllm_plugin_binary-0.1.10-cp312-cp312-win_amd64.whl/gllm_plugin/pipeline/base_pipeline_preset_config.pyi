8b3ae09cb936f46e2fb0f12c679a567d32ee0e16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:34:06 +0000

    Add package gllm-plugin-binary/0.1.10/gllm_plugin_binary-0.1.10-cp312-cp312-win_amd64.whl

---
+++ packages/gllm-plugin-binary/gllm_plugin_binary-0.1.10-cp312-cp312-win_amd64.whl/gllm_plugin/pipeline/base_pipeline_preset_config.pyi
@@ -0,0 +1,23 @@
+from gllm_plugin.config.constant import SearchType as SearchType
+from pydantic import BaseModel
+from typing import Any
+
+class BasePipelinePresetConfig(BaseModel):
+    """A Pydantic model representing the base preset configuration of all pipelines.
+
+    Attributes:
+        pipeline_preset_id (str): The pipeline preset id.
+        supported_models (dict[str, Any]): The supported models.
+        supported_agents (list[str]): The supported agents.
+        support_pii_anonymization (bool): Whether the pipeline supports pii anonymization.
+        support_multimodal (bool): Whether the pipeline supports multimodal.
+        use_docproc (bool): Whether to use the document processor.
+        search_types (list[SearchType]): The supported search types.
+    """
+    pipeline_preset_id: str
+    supported_models: dict[str, Any]
+    supported_agents: list[str]
+    support_pii_anonymization: bool
+    support_multimodal: bool
+    use_docproc: bool
+    search_types: list[SearchType]
