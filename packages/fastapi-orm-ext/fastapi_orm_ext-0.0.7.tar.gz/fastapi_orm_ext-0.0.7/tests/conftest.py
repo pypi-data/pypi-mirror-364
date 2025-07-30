05669a8f3ac7c3fed7cb0aaca0a104823d01c58f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:07:55 +0000

    Add package fastapi-orm-ext/0.0.7/fastapi_orm_ext-0.0.7.tar.gz

---
+++ packages/fastapi-orm-ext/fastapi_orm_ext-0.0.7.tar.gz/fastapi_orm_ext-0.0.7/tests/conftest.py
@@ -0,0 +1,42 @@
+from typing import TYPE_CHECKING
+
+import pytest_asyncio
+from fastapi_orm_ext.table import TableBase
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
+
+if TYPE_CHECKING:
+    from collections.abc import AsyncGenerator
+
+    from sqlalchemy.ext.asyncio import AsyncEngine
+
+TEST_DATABASE_URL = "sqlite+aiosqlite:///:memory:"
+
+
+class Table(TableBase, DeclarativeBase):
+    __abstract__: bool = True
+
+
+@pytest_asyncio.fixture
+async def engine() -> "AsyncGenerator[AsyncEngine]":
+    engine: AsyncEngine = create_async_engine(
+        url=TEST_DATABASE_URL,
+        echo=True,
+    )
+
+    async with engine.begin() as conn:
+        await conn.run_sync(fn=Table.metadata.create_all)
+
+    try:
+        yield engine
+    finally:
+        await engine.dispose()
+
+
+@pytest_asyncio.fixture
+async def session(engine: "AsyncEngine") -> "AsyncGenerator[AsyncSession]":
+    async_session: AsyncSession = AsyncSession(bind=engine, expire_on_commit=False)
+    try:
+        yield async_session
+    finally:
+        await async_session.close()
