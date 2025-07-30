5c6a0375a1a93e8fe8c48fe6943e04089a781295
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:58 +0000

    Add package maai/0.0.16/maai-0.0.16.tar.gz

---
+++ packages/maai/maai-0.0.16.tar.gz/maai-0.0.16/maai/models/vap.py
@@ -0,0 +1,100 @@
+import torch
+import torch.nn as nn
+from torch import Tensor
+from typing import Optional, Tuple
+import torch.nn.functional as F
+
+from .config import VapConfig
+from ..encoder import EncoderCPC
+from ..modules import GPT, GPTStereo
+from ..objective import ObjectiveVAP
+
+class VapGPT(nn.Module):
+    
+    def __init__(self, conf: Optional[VapConfig] = None):
+        super().__init__()
+        if conf is None:
+            conf = VapConfig()
+        self.conf = conf
+        self.sample_rate = conf.sample_rate
+        self.frame_hz = conf.frame_hz
+
+        self.temp_elapse_time = []
+
+        # Single channel
+        self.ar_channel = GPT(
+            dim=conf.dim,
+            dff_k=3,
+            num_layers=conf.channel_layers,
+            num_heads=conf.num_heads,
+            dropout=conf.dropout,
+            context_limit=conf.context_limit,
+        )
+
+        # Cross channel
+        self.ar = GPTStereo(
+            dim=conf.dim,
+            dff_k=3,
+            num_layers=conf.cross_layers,
+            num_heads=conf.num_heads,
+            dropout=conf.dropout,
+            context_limit=conf.context_limit,
+        )
+
+        self.objective = ObjectiveVAP(bin_times=conf.bin_times, frame_hz=conf.frame_hz)
+
+        # Outputs
+        # Voice activity objective -> x1, x2 -> logits ->  BCE
+        self.va_classifier = nn.Linear(conf.dim, 1)
+        
+        if self.conf.lid_classify == 1:
+            self.lid_classifier = nn.Linear(conf.dim, conf.lid_classify_num_class)
+        
+        elif self.conf.lid_classify == 2:
+            self.lid_classifier_middle = nn.Linear(conf.dim*2, conf.lid_classify_num_class)
+        
+        if self.conf.lang_cond == 1:
+            self.lang_condition = nn.Linear(conf.lid_classify_num_class, conf.dim)
+        
+        self.vap_head = nn.Linear(conf.dim, self.objective.n_classes)
+
+    def load_encoder(self, cpc_model):
+        
+        # Audio Encoder
+        #if self.conf.encoder_type == "cpc":
+        self.encoder1 = EncoderCPC(
+            load_pretrained=True if self.conf.load_pretrained == 1 else False,
+            freeze=self.conf.freeze_encoder,
+            cpc_model=cpc_model
+        )
+        self.encoder1 = self.encoder1.eval()
+        #print(self.encoder1)
+        #self.encoder1 = self.encoder1.half()
+        
+        self.encoder2 = EncoderCPC(
+            load_pretrained=True if self.conf.load_pretrained == 1 else False,
+            freeze=self.conf.freeze_encoder,
+            cpc_model=cpc_model
+        )
+
+        self.encoder2 = self.encoder2.eval()
+        #self.encoder2 = self.encoder2.half()
+        
+        if self.conf.freeze_encoder == 1:
+            print('freeze encoder')
+            self.encoder1.freeze()
+            self.encoder2.freeze()
+
+    @property
+    def horizon_time(self):
+        return self.objective.horizon_time
+
+    def encode_audio(self, audio1: torch.Tensor, audio2: torch.Tensor) -> Tuple[Tensor, Tensor]:
+        
+        x1 = self.encoder1(audio1)  # speaker 1
+        x2 = self.encoder2(audio2)  # speaker 2
+        
+        return x1, x2
+
+    def vad_loss(self, vad_output, vad):
+        return F.binary_cross_entropy_with_logits(vad_output, vad)
