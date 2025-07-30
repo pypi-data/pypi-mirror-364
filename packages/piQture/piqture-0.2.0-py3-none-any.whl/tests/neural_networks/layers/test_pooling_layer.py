aa78699034df5c753b5b268fd731594098ed5c2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:59 +0000

    Add package piQture/0.2.0/piqture-0.2.0-py3-none-any.whl

---
+++ packages/piQture/piqture-0.2.0-py3-none-any.whl/tests/neural_networks/layers/test_pooling_layer.py
@@ -0,0 +1,291 @@
+# (C) Copyright SaashaJoshi 2024.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Tests for Quantum Pooling Layer classes"""
+
+from __future__ import annotations
+
+import pytest
+from pytest import raises
+from qiskit.circuit import QuantumCircuit
+
+from piqture.neural_networks.layers import QuantumPoolingLayer2, QuantumPoolingLayer3
+
+# pylint: disable=not-context-manager
+
+
+@pytest.fixture
+def circuit1a():
+    """Fixture for fully connected layer circuit"""
+    circuit = QuantumCircuit(2, 2)
+    circuit.h(1)
+    circuit.measure(1, 1)
+    with circuit.if_test((1, 1)):
+        circuit.z(0)
+    return circuit, [0]
+
+
+@pytest.fixture
+def circuit1b():
+    """Fixture for fully connected layer circuit with False conditional"""
+    circuit = QuantumCircuit(2, 2)
+    circuit.cz(1, 0)
+    return circuit, [0]
+
+
+@pytest.fixture
+def circuit2a():
+    """Fixture for fully connected layer circuit"""
+    circuit = QuantumCircuit(4, 4)
+    circuit.h([1, 3])
+    circuit.measure([1, 3], [1, 3])
+    with circuit.if_test([1, 1]):
+        circuit.z(0)
+    with circuit.if_test([3, 1]):
+        circuit.z(2)
+    return circuit, [0, 2]
+
+
+@pytest.fixture
+def circuit2b():
+    """Fixture for fully connected layer circuit with False conditional"""
+    circuit = QuantumCircuit(4, 4)
+    circuit.cz(1, 0)
+    circuit.cz(3, 2)
+    return circuit, [0, 2]
+
+
+@pytest.fixture
+def circuit3a():
+    """Fixture for fully connected layer circuit"""
+    circuit = QuantumCircuit(8, 8)
+    circuit.h([3, 7])
+    circuit.measure([3, 7], [3, 7])
+    with circuit.if_test([3, 1]):
+        circuit.z(2)
+    with circuit.if_test([7, 1]):
+        circuit.z(5)
+    return circuit, [2, 5]
+
+
+@pytest.fixture
+def circuit3b():
+    """Fixture for fully connected layer circuit"""
+    circuit = QuantumCircuit(8, 8)
+    circuit.cz(3, 2)
+    circuit.cz(7, 5)
+    return circuit, [2, 5]
+
+
+@pytest.fixture
+def circuit4a():
+    """Fixture for fully connected layer circuit"""
+    circuit = QuantumCircuit(6, 6)
+    circuit.h([3])
+    circuit.measure([3], [3])
+    with circuit.if_test([3, 1]):
+        circuit.z(2)
+    return circuit, [2, 5]
+
+
+@pytest.fixture
+def circuit4b():
+    """Fixture for fully connected layer circuit"""
+    circuit = QuantumCircuit(6, 6)
+    circuit.cz(3, 2)
+    return circuit, [2, 5]
+
+
+@pytest.fixture
+def circuit5a():
+    """Fixture for fully connected layer circuit"""
+    circuit = QuantumCircuit(3, 3)
+    circuit.h([0, 2])
+    circuit.measure([0, 2], [0, 2])
+    with circuit.if_test([0, 1]):
+        with circuit.if_test([2, 1]):
+            circuit.z(1)
+    return circuit, [1]
+
+
+@pytest.fixture
+def circuit5b():
+    """Fixture for fully connected layer circuit"""
+    circuit = QuantumCircuit(3, 3)
+    circuit.cz(0, 1)
+    circuit.cz(2, 1)
+    return circuit, [1]
+
+
+@pytest.fixture
+def circuit6a():
+    """Fixture for fully connected layer circuit"""
+    circuit = QuantumCircuit(6, 6)
+    circuit.h([0, 2, 4])
+    circuit.measure([0, 2, 4], [0, 2, 4])
+    with circuit.if_test([0, 1]):
+        with circuit.if_test([2, 1]):
+            circuit.z(1)
+    with circuit.if_test([2, 1]):
+        with circuit.if_test([4, 1]):
+            circuit.z(3)
+    return circuit, [1, 3, 5]
+
+
+@pytest.fixture
+def circuit6b():
+    """Fixture for fully connected layer circuit"""
+    circuit = QuantumCircuit(6, 6)
+    circuit.cz(0, 1)
+    circuit.cz(2, 1)
+    circuit.cz(2, 3)
+    circuit.cz(4, 3)
+    return circuit, [1, 3, 5]
+
+
+@pytest.fixture
+def circuit7a():
+    """Fixture for fully connected layer circuit"""
+    circuit = QuantumCircuit(8, 8)
+    circuit.h([2, 5])
+    circuit.measure([2, 5], [2, 5])
+    with circuit.if_test([2, 1]):
+        with circuit.if_test([5, 1]):
+            circuit.z(3)
+    return circuit, [3, 7]
+
+
+@pytest.fixture
+def circuit7b():
+    """Fixture for fully connected layer circuit"""
+    circuit = QuantumCircuit(8, 8)
+    circuit.cz(2, 3)
+    circuit.cz(5, 3)
+    return circuit, [3, 7]
+
+
+class TestQuantumPoolingLayer2:
+    """Tests for Quantum Pooling Layer (2) class"""
+
+    # pylint: disable=too-few-public-methods
+
+    @pytest.mark.parametrize(
+        "num_qubits, circuit, unmeasured_bits, conditional",
+        [
+            (2, None, None, None),
+            (None, QuantumCircuit(2, 2), None, 2.5),
+            (None, None, [2, 5], "abc"),
+        ],
+    )
+    def test_conditional(self, num_qubits, circuit, unmeasured_bits, conditional):
+        """Tests for the type of conditional input."""
+        with raises(TypeError, match="The input conditional must be of the type bool."):
+            _ = QuantumPoolingLayer2(num_qubits, circuit, unmeasured_bits, conditional)
+
+    @pytest.mark.parametrize(
+        "num_qubits, circuit, unmeasured_bits, conditional, resulting_circuit",
+        [
+            (2, None, None, False, "circuit1b"),
+            (2, None, None, True, "circuit1a"),
+            (None, QuantumCircuit(4, 4), None, False, "circuit2b"),
+            (None, QuantumCircuit(4, 4), None, True, "circuit2a"),
+            (None, None, [2, 3, 5, 7], False, "circuit3b"),
+            (None, None, [2, 3, 5, 7], True, "circuit3a"),
+            (None, None, [2, 3, 5], False, "circuit4b"),
+            (None, None, [2, 3, 5], True, "circuit4a"),
+        ],
+    )
+
+    # pylint: disable=R0917
+    def test_build_layer(
+        self,
+        request,
+        num_qubits,
+        circuit,
+        unmeasured_bits,
+        conditional,
+        resulting_circuit,
+    ):
+        # pylint: disable=too-many-arguments
+        """Tests the build_layer method of QuantumPoolingLayer2 class."""
+        circuit, new_unmeasured_bits = QuantumPoolingLayer2(
+            num_qubits,
+            circuit,
+            unmeasured_bits,
+            conditional,
+        ).build_layer()
+        resulting_circuit, resulting_unmeasured_bits = request.getfixturevalue(
+            resulting_circuit
+        )
+        print(circuit)
+        print(resulting_circuit)
+        assert circuit == resulting_circuit
+        assert new_unmeasured_bits == resulting_unmeasured_bits
+
+
+class TestQuantumPoolingLayer3:
+    """Tests for Quantum Pooling Layer (3) class"""
+
+    @pytest.mark.parametrize(
+        "num_qubits, circuit, unmeasured_bits",
+        [(2, None, None), (None, QuantumCircuit(2, 2), None), (None, None, [2, 3])],
+    )
+    def test_three_qubits(self, num_qubits, circuit, unmeasured_bits):
+        """Tests for presence of at least 3 qubits in the circuit."""
+        with raises(ValueError):
+            _ = QuantumPoolingLayer3(num_qubits, circuit, unmeasured_bits)
+
+    @pytest.mark.parametrize(
+        "num_qubits, circuit, unmeasured_bits, conditional",
+        [
+            (3, None, None, None),
+            (None, QuantumCircuit(3, 3), None, 2.5),
+            (None, None, [2, 3, 5], "abc"),
+        ],
+    )
+    def test_conditional(self, num_qubits, circuit, unmeasured_bits, conditional):
+        """Tests for the type of conditional input."""
+        with raises(TypeError, match="The input conditional must be of the type bool."):
+            _ = QuantumPoolingLayer3(num_qubits, circuit, unmeasured_bits, conditional)
+
+    @pytest.mark.parametrize(
+        "num_qubits, circuit, unmeasured_bits, conditional, resulting_circuit",
+        [
+            (3, None, None, False, "circuit5b"),
+            (3, None, None, True, "circuit5a"),
+            (None, QuantumCircuit(6, 6), None, False, "circuit6b"),
+            (None, QuantumCircuit(6, 6), None, True, "circuit6a"),
+            (None, None, [2, 3, 5, 7], False, "circuit7b"),
+            (None, None, [2, 3, 5, 7], True, "circuit7a"),
+        ],
+    )
+
+    # pylint: disable=R0917
+    def test_build_layer(
+        self,
+        request,
+        num_qubits,
+        circuit,
+        unmeasured_bits,
+        conditional,
+        resulting_circuit,
+    ):
+        # pylint: disable=too-many-arguments
+        """Tests the build_layer method of QuantumPoolingLayer2 class."""
+        circuit, new_unmeasured_bits = QuantumPoolingLayer3(
+            num_qubits, circuit, unmeasured_bits, conditional
+        ).build_layer()
+        resulting_circuit, resulting_unmeasured_bits = request.getfixturevalue(
+            resulting_circuit
+        )
+        print(circuit, new_unmeasured_bits)
+        print(resulting_circuit)
+        assert circuit == resulting_circuit
+        assert new_unmeasured_bits == resulting_unmeasured_bits
---
+++ packages/piQture/piqture-0.2.0-py3-none-any.whl/tests/tensor_networks/test_mera.py
@@ -0,0 +1,273 @@
+# (C) Copyright SaashaJoshi 2024.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Unit test for MERA class"""
+
+from __future__ import annotations
+
+from unittest import mock
+
+import numpy as np
+import pytest
+from pytest import raises
+from qiskit.circuit import ParameterVector, QuantumCircuit
+
+from piqture.gates.two_qubit_unitary import TwoQubitUnitary
+from piqture.tensor_networks import MERA
+
+
+@pytest.fixture(name="mera_circuit")
+def mera_circuit_fixture():
+    """Fixture to replicate a real simple two-qubit unitary block."""
+
+    # pylint: disable=duplicate-code
+    def _mera_circuit(num_qubits, parameter_vector, parameterization):
+        test_circuit = QuantumCircuit(num_qubits)
+        parameterization_callable = {
+            "real_simple": [TwoQubitUnitary().real_simple_block, 2],
+            "complex_simple": [TwoQubitUnitary().complex_simple_block, 2],
+            "real_general": [TwoQubitUnitary().real_general_block, 6],
+            "complex_general": [TwoQubitUnitary().complex_general_block, 15],
+        }
+
+        mapper = parameterization_callable[parameterization]
+        if num_qubits == 2:
+            # D-block doesn't exist
+            # U-block implementation
+            test_circuit.compose(
+                mapper[0](parameter_vector[: mapper[1]])[0],
+                qubits=[0, 1],
+                inplace=True,
+            )
+            test_circuit.ry(parameter_vector[mapper[1]], len(test_circuit.qubits) - 1)
+        elif num_qubits == 3:
+            # D-block
+            test_circuit.compose(
+                mapper[0](parameter_vector[: mapper[1]])[0],
+                qubits=[1, 2],
+                inplace=True,
+            )
+            # U-block
+            test_circuit.compose(
+                mapper[0](parameter_vector[mapper[1] : 2 * mapper[1]])[0],
+                qubits=[0, 1],
+                inplace=True,
+            )
+            # No D-block now. Skip to U-block
+            test_circuit.compose(
+                mapper[0](parameter_vector[2 * mapper[1] : 3 * mapper[1]])[0],
+                qubits=[1, 2],
+                inplace=True,
+            )
+            test_circuit.ry(
+                parameter_vector[3 * mapper[1]], len(test_circuit.qubits) - 1
+            )
+        elif num_qubits == 4:
+            # D-block
+            test_circuit.compose(
+                mapper[0](parameter_vector[: mapper[1]])[0],
+                qubits=[1, 2],
+                inplace=True,
+            )
+            # U-block
+            test_circuit.compose(
+                mapper[0](parameter_vector[mapper[1] : 2 * mapper[1]])[0],
+                qubits=[0, 1],
+                inplace=True,
+            )
+            test_circuit.compose(
+                mapper[0](parameter_vector[2 * mapper[1] : 3 * mapper[1]])[0],
+                qubits=[2, 3],
+                inplace=True,
+            )
+            # U-block again.
+            test_circuit.compose(
+                mapper[0](parameter_vector[3 * mapper[1] : 4 * mapper[1]])[0],
+                qubits=[1, 3],
+                inplace=True,
+            )
+            test_circuit.ry(
+                parameter_vector[4 * mapper[1]], len(test_circuit.qubits) - 1
+            )
+        elif num_qubits == 5:
+            # D-block
+            test_circuit.compose(
+                mapper[0](parameter_vector[: mapper[1]])[0],
+                qubits=[1, 2],
+                inplace=True,
+            )
+            test_circuit.compose(
+                mapper[0](parameter_vector[mapper[1] : 2 * mapper[1]])[0],
+                qubits=[3, 4],
+                inplace=True,
+            )
+            # U-block
+            test_circuit.compose(
+                mapper[0](parameter_vector[2 * mapper[1] : 3 * mapper[1]])[0],
+                qubits=[0, 1],
+                inplace=True,
+            )
+            test_circuit.compose(
+                mapper[0](parameter_vector[3 * mapper[1] : 4 * mapper[1]])[0],
+                qubits=[2, 3],
+                inplace=True,
+            )
+            # D-block
+            test_circuit.compose(
+                mapper[0](parameter_vector[4 * mapper[1] : 5 * mapper[1]])[0],
+                qubits=[3, 4],
+                inplace=True,
+            )
+            # U-block
+            test_circuit.compose(
+                mapper[0](parameter_vector[5 * mapper[1] : 6 * mapper[1]])[0],
+                qubits=[1, 3],
+                inplace=True,
+            )
+            # U-block again.
+            test_circuit.compose(
+                mapper[0](parameter_vector[6 * mapper[1] : 7 * mapper[1]])[0],
+                qubits=[3, 4],
+                inplace=True,
+            )
+            test_circuit.ry(
+                parameter_vector[7 * mapper[1]], len(test_circuit.qubits) - 1
+            )
+        return test_circuit
+
+    return _mera_circuit
+
+
+class TestMERA:
+    """Tests for MERA class"""
+
+    @pytest.mark.parametrize("num_qubits, layer_depth", [(8, 2), (6, None)])
+    def test_repr(self, num_qubits, layer_depth):
+        """Tests the representation of the MERA class."""
+        representation = repr(MERA(num_qubits, layer_depth))
+        if not layer_depth:
+            layer_depth = int(np.ceil(np.sqrt(num_qubits)))
+        test_representation = (
+            f"MultiScaleEntanglementRenormalizationAnsatz("
+            f"num_qubits={num_qubits}, layer_depth={layer_depth}"
+            f")"
+        )
+        assert test_representation == representation
+
+    @pytest.mark.parametrize("num_qubits, layer_depth", [(3, -0.9), (2, "abc")])
+    def test_layer_depth(self, num_qubits, layer_depth):
+        """Tests the type of layer_depth input."""
+        with raises(
+            TypeError, match="The input layer_depth must be of the type int or None."
+        ):
+            _ = MERA(num_qubits, layer_depth)
+
+    @pytest.mark.parametrize("num_qubits, layer_depth", [(3, 0)])
+    def test_layer_depth_value(self, num_qubits, layer_depth):
+        """Tests the value of layer_depth input."""
+        with raises(ValueError, match="The input layer_depth must be at least 1."):
+            _ = MERA(num_qubits, layer_depth)
+
+    @pytest.mark.parametrize("num_qubits", [8])
+    def test_circuit_property(self, num_qubits):
+        """Tests the MERA circuit initialization."""
+        test_circuit = QuantumCircuit(num_qubits)
+        assert test_circuit == MERA(num_qubits).circuit
+
+    @pytest.mark.parametrize(
+        "num_qubits, layer_depth, complex_structure",
+        [(4, 1, False), (20, None, True)],
+    )
+    def test_mera_simple(self, num_qubits, layer_depth, complex_structure):
+        # pylint: disable=line-too-long
+        """Tests the mera_backbone method call via the mera_simple function."""
+        with mock.patch(
+            "piqture.tensor_networks.mera.MERA.mera_backbone"
+        ) as mock_mera_simple:
+            with mock.patch(
+                "piqture.gates.two_qubit_unitary.TwoQubitUnitary.simple_parameterization"
+            ) as simple_parameterization:
+                _ = MERA(num_qubits, layer_depth).mera_simple(complex_structure)
+                mock_mera_simple.assert_called_once_with(
+                    simple_parameterization, mock.ANY, complex_structure
+                )
+
+    @pytest.mark.parametrize(
+        "num_qubits, layer_depth, complex_structure",
+        [(4, 1, False), (12, None, True)],
+    )
+    def test_mera_general(self, num_qubits, layer_depth, complex_structure):
+        # pylint: disable=line-too-long
+        """Tests the mera_backbone method call via the mera_general function."""
+        with mock.patch(
+            "piqture.tensor_networks.mera.MERA.mera_backbone"
+        ) as mock_mera_general:
+            with mock.patch(
+                "piqture.gates.two_qubit_unitary.TwoQubitUnitary.general_parameterization"
+            ) as general_parameterization:
+                _ = MERA(num_qubits, layer_depth).mera_general(complex_structure)
+                mock_mera_general.assert_called_once_with(
+                    general_parameterization, mock.ANY, complex_structure
+                )
+
+    @pytest.mark.parametrize(
+        "num_qubits, layer_depth, complex_structure, parameterization",
+        [
+            (2, None, False, "real_general"),
+            (3, None, False, "real_general"),
+            (4, None, False, "real_general"),
+            (2, None, False, "real_simple"),
+            (3, None, False, "real_simple"),
+            (4, None, False, "real_simple"),
+            (2, None, True, "complex_general"),
+            (3, None, True, "complex_general"),
+            (4, None, True, "complex_general"),
+            (5, None, False, "real_general"),
+            (5, None, False, "real_simple"),
+            (5, None, True, "complex_general"),
+            (5, None, True, "complex_simple"),
+        ],
+    )
+
+    # pylint: disable=R0917
+    def test_mera_backbone(
+        self, num_qubits, layer_depth, complex_structure, parameterization, mera_circuit
+    ):
+        # pylint: disable=too-many-arguments
+        """Tests the mera_backbone circuit with real and complex parameterization."""
+        # Add test cases when layer_depth is not None.
+        parameterization_mapper = {
+            "real_simple": [
+                ParameterVector("test", 10 * num_qubits - 1),
+                TwoQubitUnitary().simple_parameterization,
+            ],
+            "complex_simple": [
+                ParameterVector("test", 10 * num_qubits - 1),
+                TwoQubitUnitary().simple_parameterization,
+            ],
+            "real_general": [
+                ParameterVector("test", 20 * num_qubits - 1),
+                TwoQubitUnitary().general_parameterization,
+            ],
+            "complex_general": [
+                ParameterVector("test", 15 * 10 * num_qubits - 1),
+                TwoQubitUnitary().general_parameterization,
+            ],
+        }
+
+        test_circuit = mera_circuit(
+            num_qubits, parameterization_mapper[parameterization][0], parameterization
+        )
+        circuit = MERA(num_qubits, layer_depth).mera_backbone(
+            parameterization_mapper[parameterization][1],
+            parameterization_mapper[parameterization][0],
+            complex_structure,
+        )
+        assert circuit == test_circuit
