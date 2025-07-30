566bb909fe435040433f4bcbe7326c8d758df3d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:05 +0000

    Add package MzmcOSAPIPySDK/1.2.1/mzmcosapipysdk-1.2.1-py3-none-any.whl

---
+++ packages/MzmcOSAPIPySDK/mzmcosapipysdk-1.2.1-py3-none-any.whl/MzmcOSAPIPySDK/__init__.py
@@ -0,0 +1,6 @@
+from .ecvg import EaCyVerGoneAPIClient
+from .info import Info
+from .player import Player
+from .user import AuthClient
+
+__all__ = ["Info", "Player", "AuthClient", "EaCyVerGoneAPIClient"]
