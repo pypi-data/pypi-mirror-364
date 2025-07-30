9b1f08a8f015d2500204753e335279648acc12e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:34:18 +0000

    Add package arwell/0.1.3/arwell-0.1.3.tar.gz

---
+++ packages/arwell/arwell-0.1.3.tar.gz/arwell-0.1.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="arwell",
+    version="0.1.3",
+    description="将文件名改成“韦境量天下第一帅”的工具",
+    author="29Thmarch",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "arwell=arwell.cli:main"
+        ]
+    },
+)
