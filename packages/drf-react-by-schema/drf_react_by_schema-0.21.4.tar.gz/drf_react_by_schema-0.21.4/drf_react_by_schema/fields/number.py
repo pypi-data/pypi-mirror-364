a3a98d23117aabc736323ae3af8a31eeef50c044
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:21:07 +0000

    Add package drf-react-by-schema/0.21.4/drf_react_by_schema-0.21.4.tar.gz

---
+++ packages/drf-react-by-schema/drf_react_by_schema-0.21.4.tar.gz/drf_react_by_schema-0.21.4/drf_react_by_schema/fields/number.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class DecimalField(models.DecimalField):
+    description = "Extended DecimalField"
+
+    def __init__(
+        self, *args, is_currency=True, prefix="", suffix="", ui_required=False, **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+        self.is_currency = is_currency and prefix == "" and suffix == ""
+        self.prefix = prefix
+        self.suffix = suffix
+        self.ui_required = ui_required
+
+
+class IntegerField(models.IntegerField):
+    description = "Extended IntegerField allowing pattern_format for inputs"
+
+    def __init__(self, *args, pattern_format=None, ui_required=False, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.pattern_format = pattern_format
+        self.ui_required = ui_required
