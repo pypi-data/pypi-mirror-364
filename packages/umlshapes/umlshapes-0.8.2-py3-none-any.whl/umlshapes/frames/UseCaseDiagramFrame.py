942c05ca98fd69b8461390ec0d60cb8207a7123d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:09:18 +0000

    Add package umlshapes/0.8.2/umlshapes-0.8.2-py3-none-any.whl

---
+++ packages/umlshapes/umlshapes-0.8.2-py3-none-any.whl/umlshapes/frames/UseCaseDiagramFrame.py
@@ -0,0 +1,21 @@
+
+from logging import Logger
+from logging import getLogger
+
+from wx import Window
+
+from umlshapes.eventengine.IUmlEventEngine import IUmlEventEngine
+from umlshapes.frames.UmlFrame import UmlFrame
+
+
+class UseCaseDiagramFrame(UmlFrame):
+    def __init__(self, parent: Window, umlEventEngine: IUmlEventEngine):
+        """
+
+        Args:
+            parent:
+            umlEventEngine:
+        """
+
+        self.logger: Logger = getLogger(__name__)
+        super().__init__(parent=parent, umlEventEngine=umlEventEngine)
