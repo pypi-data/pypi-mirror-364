b5351dbabbb77d8b792046fcc531c332ab2e1bcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:06 +0000

    Add package cachew/0.20.20250726/cachew-0.20.20250726-py3-none-any.whl

---
+++ packages/cachew/cachew-0.20.20250726-py3-none-any.whl/cachew/experimental.py
@@ -0,0 +1,9 @@
+import warnings
+
+
+def enable_exceptions():
+    warnings.warn("Exceptions are not an experimental feature anymore and enabled by default.")
+
+
+def disable_exceptions():
+    warnings.warn("Exceptions are not an experimental feature anymore and enabled by default.")
---
+++ packages/cachew/cachew-0.20.20250726-py3-none-any.whl/cachew/marshall/common.py
@@ -0,0 +1,22 @@
+from abc import abstractmethod
+from typing import (
+    Any,
+    Generic,
+    TypeVar,
+    Union,
+)
+
+Json = Union[dict[str, Any], tuple[Any, ...], str, float, int, bool, None]
+
+
+T = TypeVar('T')
+
+
+class AbstractMarshall(Generic[T]):
+    @abstractmethod
+    def dump(self, obj: T) -> Json:
+        raise NotImplementedError
+
+    @abstractmethod
+    def load(self, dct: Json) -> T:
+        raise NotImplementedError
