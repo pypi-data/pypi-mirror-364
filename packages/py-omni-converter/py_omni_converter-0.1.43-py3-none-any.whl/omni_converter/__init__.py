a1746e46c456fc3900bbfa5a9882b7cfd4e32033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:31 +0000

    Add package py-omni-converter/0.1.43/py_omni_converter-0.1.43-py3-none-any.whl

---
+++ packages/py-omni-converter/py_omni_converter-0.1.43-py3-none-any.whl/omni_converter/__init__.py
@@ -0,0 +1,24 @@
+__version__ = '0.1.0'
+
+from dataclasses import dataclass
+
+from omni_converter.auto_data.i_auto_data import IAutoData
+from omni_converter.solver.rules import AutoRuleBook
+
+_GLOBAL_RULE: AutoRuleBook = None
+
+
+def init_global_rules(
+        rulebook: AutoRuleBook
+):
+    global _GLOBAL_RULE
+    _GLOBAL_RULE = rulebook
+
+
+@dataclass
+class AutoDataFactory:
+    rulebook: AutoRuleBook
+
+    def __call__(self, format, value) -> IAutoData:
+        from omni_converter.auto_data.auto_v2 import AutoData2
+        return AutoData2(value=value, format=format).with_rules(self.rulebook)
