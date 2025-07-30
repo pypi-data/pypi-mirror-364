b040aba52e1764615a2e6d42e1bac85efe9e7e51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:30:37 +0000

    Add package autodevops-cli/7.1/autodevops-cli-7.1.tar.gz

---
+++ packages/autodevops-cli/autodevops-cli-7.1.tar.gz/autodevops-cli-7.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='autodevops-cli',
+    version='7.1',
+    description='AutoDevOps CLI: Automate Git push and simulate CI/CD',
+    author='Kannan',
+    author_email='kannan@example.com',
+    packages=find_packages(),
+    install_requires=[
+        'click',
+        'rich',
+        'sh',
+        'loguru',
+        'python-dotenv',
+        'openai'
+    ],
+    entry_points={
+        'console_scripts': [
+            'autodevops=autodevops_cli.cli:autodevops',
+        ],
+    },
+
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.6',
+)
