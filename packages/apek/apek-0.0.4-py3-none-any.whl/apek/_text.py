9db2d1d459e8e318d93d26f24804eaf659fbea51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:41 +0000

    Add package apek/0.0.4/apek-0.0.4-py3-none-any.whl

---
+++ packages/apek/apek-0.0.4-py3-none-any.whl/apek/_text.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=missing-module-docstring
+# pylint: disable=line-too-long
+
+
+
+text = {
+    "en": {
+        "helps.function.setLang.languageNotSupport": "Unsupported language: ",
+        "helps.function.setLang.languageNotSupport2": "Supported languages: ",
+        "helps.function.updateLog.versionNotFound": "Version not found: ",
+        "helps.function.updateLog.versionFormatError": "The version code format is incorrect: ",
+        "helps.upgradeLogs.0_0_1": "The first version, this module was just created.",
+        "helps.upgradeLogs.0_0_2": "Refined the code style and text syntax, and provided the helps and meta modules.",
+        "helps.upgradeLogs.0_0_3": "New Feature: When converting LargeNumber to a string, custom templates are now supported.\nRemoved Features: The `__getitem__` method has been replaced with `getBase`, `getExp`, and `getConfig` methods. The `__setitem__`, `__float__`, and `__complex__` methods have been removed.\nBug Fixes: Many severe issues have been resolved."
+    },
+    "zh": {
+        "helps.function.setLang.languageNotSupport": "不支持的语言：",
+        "helps.function.setLang.languageNotSupport2": "支持以下语言：",
+        "helps.function.updateLog.versionNotFound": "无法找到版本号：",
+        "helps.function.updateLog.versionFormatError": "版本号格式错误：",
+        "helps.upgradeLogs.0_0_1": "第一个版本，这个模块刚被创建。",
+        "helps.upgradeLogs.0_0_2": "完善了代码风格和文本语法，提供了helps和meta模块。",
+        "helps.upgradeLogs.0_0_3": "新增功能：LargeNumber转换为字符串时支持自定义模板。\n移除功能：把__getitem__方法用getBase，getExp和getConfig方法替代，移除了__setitem__，__float__，__complex__方法。\n问题修复：修复了很多严重的问题。"
+    },
+    "zh-wenyan": {
+        "helps.function.setLang.languageNotSupport": "不支持之语言：",
+        "helps.function.setLang.languageNotSupport2": "支持之语言：",
+        "helps.function.updateLog.versionNotFound": "未得版本号：",
+        "helps.function.updateLog.versionFormatError": "版本号格式有误：",
+        "helps.upgradeLogs.0_0_1": "初版，此模块方始创设。",
+        "helps.upgradeLogs.0_0_2": "完善代码之风格，校正文本之语法，且增“helps”与“meta”二模块。",
+        "helps.upgradeLogs.0_0_3": "新增功能LargeNumber转换为字符串时，允以自定模板。\n移除功能：以getBase、getExp、getConfig诸法代__getitem__法，去__setitem__、__float__、__complex__诸法。\n问题修复：修多严重之问题。"
+    },
+    "fr": {
+        "helps.function.setLang.languageNotSupport": "Les langues non prises en charge: ",
+        "helps.function.setLang.languageNotSupport2": "Prend en charge les langues suivantes: ",
+        "helps.function.updateLog.versionNotFound": "Impossible de trouver le numéro de version: ",
+        "helps.function.updateLog.versionFormatError": "Le format du numéro de version est incorrect: ",
+        "helps.upgradeLogs.0_0_1": "J'ai amélioré le style de codage et la syntaxe du texte, et j'ai fourni les modules helps et meta.",
+        "helps.upgradeLogs.0_0_2": "La phrase en français est: « J'ai perfectionné le style de codage et la syntaxe du texte, et j'ai fourni les modules helps et meta. »",
+        "helps.upgradeLogs.0_0_3": "Nouvelles fonctionnalités : prise en charge des modèles personnalisés lors de la conversion de LargeNumber en chaîne de caractères.\nSuppression de fonctionnalités : les méthodes getitem ont été remplacées par les méthodes getBase, getExp et getConfig, et les méthodes setitem, float, complex ont été supprimées.\nCorrection de problèmes : de nombreux problèmes graves ont été résolus."
+    }
+}
