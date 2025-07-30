746aad781168b1b20f6e0464fb7cf9aeab80b8a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:21 +0000

    Add package pyzup/0.1.1/pyzup-0.1.1.tar.gz

---
+++ packages/pyzup/pyzup-0.1.1.tar.gz/pyzup-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# Read the README file
+this_dir = Path(__file__).parent
+long_description = (this_dir / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="pyzup",
+    version="0.1.1",
+    description="Reusable upload handler for PDFs, DOCX, PPTX, ZIPs",
+    long_description=long_description,
+    long_description_content_type="text/markdown", 
+    author="Punit Tripathi, Mohd. Amir",
+    author_email="tripathi.punit@proton.me, amir.zilli@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "Werkzeug==3.1.3",
+        "loguru==0.7.3",
+    ],
+    python_requires=">=3.7",
+)
