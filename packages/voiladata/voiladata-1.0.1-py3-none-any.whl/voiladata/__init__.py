7d51ad037fec502e69a569fbe3e95812e9aaf466
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:39 +0000

    Add package voiladata/1.0.1/voiladata-1.0.1-py3-none-any.whl

---
+++ packages/voiladata/voiladata-1.0.1-py3-none-any.whl/voiladata/__init__.py
@@ -0,0 +1,13 @@
+"""
+DataFrame Loader
+================
+
+A versatile Python library to read various file formats into a pandas DataFrame, 
+with robust handling of nested data structures.
+"""
+
+__version__ = "1.0.0"
+
+from .main import DataFrameReader
+
+__all__ = ["DataFrameReader"]
