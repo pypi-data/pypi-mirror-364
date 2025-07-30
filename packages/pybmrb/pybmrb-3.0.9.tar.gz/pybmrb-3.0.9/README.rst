71842330e84ec66cf6d4205b52ccdfc82fc17abe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:29:19 +0000

    Add package pybmrb/3.0.9/pybmrb-3.0.9.tar.gz

---
+++ packages/pybmrb/pybmrb-3.0.9.tar.gz/pybmrb-3.0.9/README.rst
@@ -0,0 +1,29 @@
+Welcome to PyBMRB!
+======================================
+.. image:: https://mybinder.org/badge_logo.svg
+ :target: https://mybinder.org/v2/gh/uwbmrb/PyBMRB/v3
+
+A Python module for visualizing Nuclear Magnetic Resonance(NMR)  chemical shift data from Biological Magnetic
+Resonance data Bank (`BMRB <http://bmrb.ip>`_) and  from NMR-STAR  format files. PyBMRB helps the
+users to view the one dimensional chemical shift list as multi-dimensional NMR spectra. Chemical shift distributions
+can be studied using this library by plotting the histograms using differnt filtering criteria.
+
+This package uses `PyBMRB <https://github.com/uwbmrb/PyNMRSTAR>`_ to parse the NMR-STAR files
+and `BMRB-API <https://github.com/uwbmrb/BMRB-API>`_ to fetch the data directly from BMRB, which avoids the hustle of
+downloading and parsing the data from the BMRB for visualizations.
+
+`Read the documents here <https://pybmrb.readthedocs.org>`__.
+
+
+.. |PythonVersions| image:: https://img.shields.io/pypi/pyversions/pynmrstar.svg
+   :target: https://github.com/uwbmrb/PyBMRB
+
+.. |License| image::  https://img.shields.io/github/license/kumar-physics/PyBMRB
+   :target: https://github.com/uwbmrb/PyBMRB
+
+
+.. |BuildStatus| image:: https://img.shields.io/github/workflow/status/kumar-physics/PyBMRB/CI/dev
+   :target: https://github.com/uwbmrb/PyBMRB
+
+
+|BuildStatus| |License| |PythonVersions|
---
+++ packages/pybmrb/pybmrb-3.0.9.tar.gz/pybmrb-3.0.9/setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import os
+
+from setuptools import setup
+
+
+def get_version():
+    internal_file_location = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'pybmrb', '__init__.py')
+
+    with open(internal_file_location, 'r') as internal_file:
+        for line in internal_file:
+            if line.startswith('__version__'):
+                delim = '"' if '"' in line else "'"
+                return line.split(delim)[1]
+        else:
+            raise RuntimeError("Unable to find version string.")
+
+
+# Should fail if the readme is missing
+long_des = open('README.rst', 'r').read()
+
+setup(name='pybmrb',
+      version=get_version(),
+      packages=['pybmrb'],
+      install_requires=['pandas', 'requests>=2.21.0,<=3', 'plotly>=4.1.0', 'pynmrstar>=3.0.4', 'numpy>1.15'],
+      python_requires='>=3.7',
+      author='Kumaran Baskaran',
+      author_email='baskaran@uchc.edu',
+      description='PyBMRB provides tools for visualizing BMRB entries and  NMR-STAR files. '
+                  'Maintained by the BMRB.',
+      long_description=long_des,
+      long_description_content_type='text/x-rst',
+      keywords=['bmrb', 'nmr', 'nmrstar', 'biomagresbank',
+                'biological magnetic resonance bank',
+                'HSQC', 'TOCSY', 'Chemical shifts',
+                'Histogram'],
+      url='https://github.com/uwbmrb/PyBMRB',
+      license='MIT',
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Programming Language :: Python :: 3 :: Only',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10',
+          'Programming Language :: Python :: 3.11',
+          'Programming Language :: Python :: 3.12',
+          'Programming Language :: Python :: 3.13',
+          'Intended Audience :: Education',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Operating System :: MacOS',
+          'Operating System :: POSIX :: Linux',
+          'Operating System :: Microsoft :: Windows',
+          'Topic :: Scientific/Engineering :: Bio-Informatics',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ]
+      )
