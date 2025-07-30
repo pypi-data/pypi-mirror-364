60b2fd947e862643d6b097719e4f42758810573d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:34:00 +0000

    Add package pytest-jubilant/0.8/pytest_jubilant-0.8-py3-none-any.whl

---
+++ packages/pytest-jubilant/pytest_jubilant-0.8-py3-none-any.whl/pytest_jubilant/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Welcome to pytest-jubilant!"""
+
+from pytest_jubilant.main import pack_charm, pack, get_resources
+
+__all__ = ["pack_charm", "pack", "get_resources"]
