4e53f6839e0d1c859d73fc32f0589a364878c5ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:42 +0000

    Add package wunderkafka/0.19.0rc5/wunderkafka-0.19.0rc5.tar.gz

---
+++ packages/wunderkafka/wunderkafka-0.19.0rc5.tar.gz/wunderkafka-0.19.0rc5/wunderkafka/config/rdkafka.py
@@ -0,0 +1,81 @@
+import os
+from enum import Enum
+from types import MappingProxyType
+from typing import Any, Union, TypeVar, Optional
+from collections.abc import Mapping
+
+from wunderkafka.logger import logger
+from wunderkafka.config.schema_registry import SRConfig
+from wunderkafka.config.generated.fields import COMMON_FIELDS, CONSUMER_FIELDS, PRODUCER_FIELDS
+from wunderkafka.config.generated.models import RDConsumerConfig, RDProducerConfig
+
+CONF_CONSUMER_FIELDS = MappingProxyType(
+    {field_name.replace(".", "_"): field_name for field_name in (*COMMON_FIELDS, *CONSUMER_FIELDS)}
+)
+
+CONF_PRODUCER_FIELDS = MappingProxyType(
+    {field_name.replace(".", "_"): field_name for field_name in (*COMMON_FIELDS, *PRODUCER_FIELDS)}
+)
+
+ConfigValues = Union[str, int, bool, float]
+
+
+def remap_properties(
+    dct: dict[str, Optional[ConfigValues]],
+    mapping: Mapping[str, str],
+) -> dict[str, ConfigValues]:
+    new_dct = {}
+    for f_name, f_value in dct.items():
+        if f_value is not None:
+            to_add = f_value
+            if isinstance(f_value, Enum):
+                to_add = f_value.value
+            new_dct[mapping[f_name]] = to_add
+    return new_dct
+
+
+# ToDo (tribunsky.kir): I need separate common place to do somthing with config before feeding it to librdkafka
+#                       or write more complex generator and class hierarchy (e.g. platform-specific base classes)
+#                       to not monkeypatch config before actually feeding it to librdkafka.
+#                       #TypeSafety!!1
+def sanitize(dct: dict[str, ConfigValues]) -> dict[str, ConfigValues]:
+    # cimpl.KafkaException: KafkaError{
+    #   ...
+    #   "Configuration property "ssl.ca.certificate.stores" not supported in this build: configuration only valid on Windows"  # noqa: E501
+    #  }
+    if os.name != "nt":
+        property_name = "ssl.ca.certificate.stores"
+        property_value = dct.pop(property_name, None)
+        if property_value is not None:
+            logger.warning(
+                "Excluding {}={} as windows-only even it was set to default".format(
+                    property_name,
+                    property_value,
+                )
+            )
+    return dct
+
+
+# I don't like mixing SR config and librdkafka config,
+# but it's more handful for producer w/o schema (no need to nest config for librdkafka)
+class ConsumerConfig(RDConsumerConfig):
+    # https://docs.pydantic.dev/latest/migration/#required-optional-and-nullable-fields
+    sr: Optional[SRConfig] = None
+
+    def dict(self, **kwargs: Any) -> dict[str, ConfigValues]:
+        dct = super().model_dump(**kwargs)
+        dct.pop("sr")
+        return sanitize(remap_properties(dct, CONF_CONSUMER_FIELDS))
+
+
+class ProducerConfig(RDProducerConfig):
+    # https://docs.pydantic.dev/latest/migration/#required-optional-and-nullable-fields
+    sr: Optional[SRConfig] = None
+
+    def dict(self, **kwargs: Any) -> dict[str, ConfigValues]:
+        dct = super().model_dump(**kwargs)
+        dct.pop("sr")
+        return sanitize(remap_properties(dct, CONF_PRODUCER_FIELDS))
+
+
+RDKafkaConfig = TypeVar("RDKafkaConfig", ConsumerConfig, ProducerConfig)
---
+++ packages/wunderkafka/wunderkafka-0.19.0rc5.tar.gz/wunderkafka-0.19.0rc5/wunderkafka/consumers/types.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+import time
+from typing import Generic, TypeVar, Optional
+
+from pydantic import Field, BaseModel, ConfigDict, model_validator
+
+M = TypeVar("M")
+T = TypeVar("T")
+
+
+class PayloadError(BaseModel):
+    description: str
+
+
+class StreamResult(BaseModel, Generic[M, T]):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+    payload: Optional[T] = None
+    error: Optional[PayloadError] = None
+    msg: M
+    t0: float = Field(default_factory=time.perf_counter)
+
+    @property
+    def ok(self) -> bool:
+        return self.error is None and self.payload is not None
+
+    @property
+    def lifetime(self) -> float:
+        return time.perf_counter() - self.t0
+
+    @model_validator(mode="after")
+    def verify_mutually_exclusive(self) -> StreamResult:
+        # The payload may be None due to Kafka tombstones:
+        # https://docs.confluent.io/kafka/design/log_compaction.html
+        # In short, these are messages with a null value, used to indicate that their key has been deleted.
+        if self.payload is not None and self.error is not None:
+            raise ValueError("payload and error cannot both be not None")
+        return self
