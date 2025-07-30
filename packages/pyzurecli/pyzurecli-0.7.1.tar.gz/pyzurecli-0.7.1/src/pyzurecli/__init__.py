4db9c5ad4399b4c9b6598e16594d53ede0b5d32a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:10:04 +0000

    Add package pyzurecli/0.7.1/pyzurecli-0.7.1.tar.gz

---
+++ packages/pyzurecli/pyzurecli-0.7.1.tar.gz/pyzurecli-0.7.1/src/pyzurecli/__init__.py
@@ -0,0 +1,7 @@
+from .factory import AzureCLI
+az = AzureCLI
+from .pyzureserver import PyzureServer
+# from .user import AzureCLIUser, UserSession
+# from .sp import AzureCLIServicePrincipal
+# from .app_registration import AzureCLIAppRegistration
+# from .util import json_to_dataclass
