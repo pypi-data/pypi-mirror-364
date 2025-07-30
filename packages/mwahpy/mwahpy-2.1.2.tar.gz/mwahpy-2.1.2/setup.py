2114015154f51917c532daad735ec16fedbcabad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:59 +0000

    Add package mwahpy/2.1.2/mwahpy-2.1.2.tar.gz

---
+++ packages/mwahpy/mwahpy-2.1.2.tar.gz/mwahpy-2.1.2/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mwahpy",
+    version="2.1.2",
+    author="Tom Donlon",
+    author_email="donlot@rpi.edu",
+    description="A python package for easily parsing and processing data from MilkyWay@home",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/thomasdonlon/mwahpy",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6.0',
+    install_requires=[
+        "numpy",
+        "galpy",
+        "matplotlib",
+        "astropy",
+        "scipy",
+        "pandas"
+        ],
+)
