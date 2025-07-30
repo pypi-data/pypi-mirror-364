2facd1dcf4ae79035025f38dc1f2fb75e022fb67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:22 +0000

    Add package senselab/0.36.5/senselab-0.36.5.tar.gz

---
+++ packages/senselab/senselab-0.36.5.tar.gz/senselab-0.36.5/src/senselab/audio/tasks/features_extraction/opensmile.py
@@ -0,0 +1,169 @@
+"""This module contains functions for extracting openSMILE features.
+
+It includes a factory class for managing openSMILE feature extractors, ensuring
+each extractor is created only once per feature set and feature level. The main
+function, `extract_opensmile_features_from_audios`, applies feature extraction
+across a list of audio samples using openSMILE, managed as a Pydra workflow
+for parallel processing. This approach supports efficient and scalable feature
+extraction across multiple audio files.
+"""
+
+try:
+    import opensmile
+
+    OPENSMILE_AVAILABLE = True
+except ModuleNotFoundError:
+    OPENSMILE_AVAILABLE = False
+
+    class DummyOpenSmile:
+        """Dummy class to represent openSMILE when it's not available."""
+
+        def __init__(self) -> None:
+            """Dummy constructor for when openSMILE is not available."""
+            self.__dict__ = {}
+
+        class Smile:
+            """Dummy class to represent openSMILE when it's not available."""
+
+            def __init__(self, *args: object, **kwargs: object) -> None:
+                """Dummy class for when openSMILE is not available."""
+                pass
+
+    opensmile = DummyOpenSmile()
+
+import os
+from typing import Any, Dict, List, Optional
+
+import numpy as np
+import pydra
+
+from senselab.audio.data_structures import Audio
+
+
+class OpenSmileFeatureExtractorFactory:
+    """A factory for managing openSMILE feature extractors.
+
+    This class creates and caches openSMILE feature extractors, allowing for
+    efficient reuse. It ensures only one instance of each feature extractor
+    exists per unique combination of `feature_set` and `feature_level`.
+    """
+
+    _extractors: Dict[str, opensmile.Smile] = {}  # Cache for feature extractors
+
+    @classmethod
+    def get_opensmile_extractor(cls, feature_set: str, feature_level: str) -> opensmile.Smile:
+        """Get or create an openSMILE feature extractor.
+
+        Args:
+            feature_set (str): The openSMILE feature set.
+            feature_level (str): The openSMILE feature level.
+
+        Returns:
+            opensmile.Smile: The openSMILE feature extractor.
+        """
+        if not OPENSMILE_AVAILABLE:
+            raise ModuleNotFoundError(
+                "`opensmile` is not installed. "
+                "Please install senselab audio dependencies using `pip install 'senselab[audio]'`"
+            )
+
+        key = f"{feature_set}-{feature_level}"  # Unique key for each feature extractor
+        if key not in cls._extractors:  # Check if extractor exists in cache
+            # Create and store a new extractor if not found in cache
+            cls._extractors[key] = opensmile.Smile(
+                feature_set=opensmile.FeatureSet[feature_set],
+                feature_level=opensmile.FeatureLevel[feature_level],
+            )
+        return cls._extractors[key]  # Return cached or newly created extractor
+
+
+def extract_opensmile_features_from_audios(
+    audios: List[Audio],
+    feature_set: str = "eGeMAPSv02",
+    feature_level: str = "Functionals",
+    plugin: str = "serial",
+    plugin_args: Optional[Dict[str, Any]] = {},
+    cache_dir: Optional[str | os.PathLike] = None,
+) -> List[Dict[str, Any]]:
+    """Extract openSMILE features from a list of audio files using Pydra workflow.
+
+    This function sets up a Pydra workflow for parallel processing of openSMILE
+    feature extraction on a list of audio samples. Each sample's features are
+    extracted and formatted as dictionaries.
+
+    Args:
+        audios (List[Audio]): The list of audio objects to extract features from.
+        feature_set (str): The openSMILE feature set (default is "eGeMAPSv02").
+        feature_level (str): The openSMILE feature level (default is "Functionals").
+        plugin (str): The Pydra plugin to use (default is "serial").
+        plugin_args (Optional[Dict[str, Any]]): Additional arguments for the Pydra plugin.
+        cache_dir (Optional[str | os.PathLike]): The path to the Pydra cache directory.
+
+    Returns:
+        List[Dict[str, Any]]: A list of dictionaries, each containing extracted features.
+    """
+    if not OPENSMILE_AVAILABLE:
+        raise ModuleNotFoundError(
+            "`opensmile` is not installed. Please install the necessary dependencies using:\n"
+            "`pip install 'senselab[audio]'`"
+        )
+
+    def _extract_feats_from_audio(sample: Audio, smile: opensmile.Smile) -> Dict[str, Any]:
+        """Extract features from a single audio sample using openSMILE.
+
+        Args:
+            sample (Audio): The audio object.
+            smile (opensmile.Smile): The openSMILE feature extractor.
+
+        Returns:
+            Dict[str, Any]: The extracted features as a dictionary.
+        """
+        # Convert audio tensor to a NumPy array for processing
+        audio_array = sample.waveform.squeeze().numpy()
+        sampling_rate = sample.sampling_rate  # Get sampling rate from Audio object
+        try:
+            # Process the audio and extract features
+            sample_features = smile.process_signal(audio_array, sampling_rate)
+            # Convert features to a dictionary and handle single-item lists
+            return {
+                k: v[0] if isinstance(v, list) and len(v) == 1 else v
+                for k, v in sample_features.to_dict("list").items()
+            }
+        except Exception as e:
+            # Log error and return NaNs if feature extraction fails
+            filepath = sample.filepath() if hasattr(sample, "filepath") and sample.filepath() else ""
+            desc = f"{sample.generate_id()}{f' ({filepath})' if filepath else ''}"
+            print(f"Error processing sample {desc}: {e}")
+            return {feature: np.nan for feature in smile.feature_names}
+
+    # Decorate the feature extraction function for Pydra
+    _extract_feats_from_audio_pt = pydra.mark.task(_extract_feats_from_audio)
+
+    # Obtain the feature extractor using the factory
+    smile = OpenSmileFeatureExtractorFactory.get_opensmile_extractor(feature_set, feature_level)
+
+    # Create a Pydra workflow, split it over the list of audio samples
+    wf = pydra.Workflow(name="wf", input_spec=["x"], cache_dir=cache_dir)
+    wf.split("x", x=audios)  # Each audio is treated as a separate task
+    # Add feature extraction task to the workflow
+    wf.add(_extract_feats_from_audio_pt(name="_extract_feats_from_audio_pt", sample=wf.lzin.x, smile=smile))
+
+    # Set workflow output to the results of each audio feature extraction
+    wf.set_output([("opensmile", wf._extract_feats_from_audio_pt.lzout.out)])
+
+    # Run the workflow using the specified Pydra plugin and arguments
+    with pydra.Submitter(plugin=plugin, **plugin_args) as sub:
+        sub(wf)
+
+    # Retrieve results from the completed workflow
+    outputs = wf.result()
+
+    # Format the outputs into a list of dictionaries
+    formatted_output: List[Dict[str, Any]] = []
+    for output in outputs:
+        # Extract features and organize into a dictionary
+        formatted_output_item = {
+            f"{feature}": output.output.opensmile[f"{feature}"] for feature in output.output.opensmile
+        }
+        formatted_output.append(formatted_output_item)  # Append to final output list
+    return formatted_output  # Return the list of formatted feature dictionaries
---
+++ packages/senselab/senselab-0.36.5.tar.gz/senselab-0.36.5/src/senselab/audio/tasks/features_extraction/torchaudio.py
@@ -0,0 +1,399 @@
+"""This module provides the implementation of torchaudio utilities for audio features extraction."""
+
+import os
+from typing import Any, Dict, List, Optional
+
+import numpy as np
+import pydra
+import torch
+
+try:
+    import torchaudio
+
+    TORCHAUDIO_AVAILABLE = True
+except ModuleNotFoundError:
+    TORCHAUDIO_AVAILABLE = False
+
+from senselab.audio.data_structures import Audio
+
+
+def extract_spectrogram_from_audios(
+    audios: List[Audio],
+    n_fft: int = 1024,
+    win_length: Optional[int] = None,
+    hop_length: Optional[int] = None,
+) -> List[Dict[str, torch.Tensor]]:
+    """Extract spectrograms from a list of audio objects.
+
+    Args:
+        audios (List[Audio]): List of Audio objects.
+        n_fft (int): Size of FFT, creates n_fft // 2 + 1 bins. Default is 1024.
+        win_length (int): Window size. Default is None, using n_fft.
+        hop_length (int): Length of hop between STFT windows. Default is None, using win_length // 2.
+
+    Returns:
+        List[Dict[str, torch.Tensor]]: List of Dict objects containing spectrograms.
+    """
+    if not TORCHAUDIO_AVAILABLE:
+        raise ModuleNotFoundError(
+            "`torchaudio` is not installed. "
+            "Please install senselab audio dependencies using `pip install 'senselab[audio]'`."
+        )
+
+    if win_length is None:
+        win_length = n_fft
+    if hop_length is None:
+        hop_length = win_length // 2
+    spectrogram = torchaudio.transforms.Spectrogram(
+        n_fft=n_fft,
+        win_length=win_length,
+        hop_length=hop_length,
+    )
+    spectrograms = []
+    for audio in audios:
+        try:
+            spectrograms.append({"spectrogram": spectrogram(audio.waveform).squeeze(0)})
+        except RuntimeError:
+            spectrograms.append({"spectrogram": np.nan})
+    return spectrograms
+
+
+def extract_mel_spectrogram_from_audios(
+    audios: List[Audio],
+    n_fft: Optional[int] = 1024,
+    win_length: Optional[int] = None,
+    hop_length: Optional[int] = None,
+    n_mels: int = 128,
+) -> List[Dict[str, torch.Tensor]]:
+    """Extract mel spectrograms from a list of audio objects.
+
+    Args:
+        audios (List[Audio]): List of Audio objects.
+        n_fft (int): Size of FFT, creates n_fft // 2 + 1 bins. Default is 1024.
+        win_length (int): Window size. Default is None, using n_fft.
+        hop_length (int): Length of hop between STFT windows. Default is None, using win_length // 2.
+        n_mels (int): Number of mel filter banks. Default is 128.
+
+    Returns:
+        List[Dict[str, torch.Tensor]]: List of Dict objects containing mel spectrograms.
+    """
+    if not TORCHAUDIO_AVAILABLE:
+        raise ModuleNotFoundError(
+            "`torchaudio` is not installed. "
+            "Please install senselab audio dependencies using `pip install 'senselab[audio]'`."
+        )
+
+    if win_length is None:
+        win_length = n_fft
+    if hop_length is None:
+        if win_length is not None:
+            hop_length = win_length // 2
+        else:
+            raise ValueError("win_length cannot be None")
+    mel_spectrograms = []
+    for audio in audios:
+        try:
+            mel_spectrogram = torchaudio.transforms.MelSpectrogram(
+                sample_rate=audio.sampling_rate,
+                n_fft=n_fft,
+                win_length=win_length,
+                hop_length=hop_length,
+                n_mels=n_mels,
+            )(audio.waveform)
+            mel_spectrograms.append({"mel_spectrogram": mel_spectrogram.squeeze(0)})
+        except RuntimeError:
+            mel_spectrograms.append({"mel_spectrogram": np.nan})
+    return mel_spectrograms
+
+
+def extract_mfcc_from_audios(
+    audios: List[Audio],
+    n_mfcc: int = 40,
+    n_ftt: Optional[int] = 400,
+    win_length: Optional[int] = None,
+    hop_length: Optional[int] = None,
+    n_mels: int = 128,
+) -> List[Dict[str, torch.Tensor]]:
+    """Extract MFCCs from a list of audio objects.
+
+    Args:
+        audios (List[Audio]): List of Audio objects.
+        n_mfcc (int): Number of MFCCs to return. Default is 40.
+        n_ftt (int): Size of FFT, creates n_ftt // 2 + 1 bins. Default is 400.
+        win_length (int): Window size. Default is None, using n_ftt.
+        hop_length (int): Length of hop between STFT windows. Default is None, using win_length // 2.
+        n_mels (int): Number of mel filter banks. Default is 128.
+
+    Returns:
+        List[Dict[str, torch.Tensor]]: List of Dict objects containing MFCCs.
+    """
+    if not TORCHAUDIO_AVAILABLE:
+        raise ModuleNotFoundError(
+            "`torchaudio` is not installed. "
+            "Please install senselab audio dependencies using `pip install 'senselab[audio]'`."
+        )
+
+    if win_length is None:
+        win_length = n_ftt
+    if hop_length is None:
+        if win_length is not None:
+            hop_length = win_length // 2
+        else:
+            raise ValueError("win_length cannot be None")
+    mfccs = []
+    for audio in audios:
+        try:
+            mfcc_transform = torchaudio.transforms.MFCC(
+                sample_rate=audio.sampling_rate,
+                n_mfcc=n_mfcc,
+                melkwargs={"n_fft": n_ftt, "win_length": win_length, "hop_length": hop_length, "n_mels": n_mels},
+            )
+            mfccs.append({"mfcc": mfcc_transform(audio.waveform).squeeze(0)})
+        except RuntimeError:
+            mfccs.append({"mfcc": np.nan})
+    return mfccs
+
+
+def extract_mel_filter_bank_from_audios(
+    audios: List[Audio],
+    n_mels: int = 128,
+    n_fft: int = 1024,
+    win_length: Optional[int] = None,
+    hop_length: Optional[int] = None,
+) -> List[Dict[str, torch.Tensor]]:
+    """Extract mel filter bank from a list of audio objects.
+
+    Args:
+        audios (List[Audio]): List of Audio objects.
+        n_mels (int): Number of mel filter banks. Default is 128.
+        n_fft (int): Size of FFT, creates n_fft // 2 + 1 bins. Default is 1024.
+        win_length (int): Window size. Default is None, using n_fft.
+        hop_length (int): Length of hop between STFT windows. Default is None, using win_length // 2.
+
+    Returns:
+        List[Dict[str, torch.Tensor]]: List of Dict objects containing mel filter banks.
+    """
+    if not TORCHAUDIO_AVAILABLE:
+        raise ModuleNotFoundError(
+            "`torchaudio` is not installed. "
+            "Please install senselab audio dependencies using `pip install 'senselab[audio]'`."
+        )
+
+    if win_length is None:
+        win_length = n_fft
+    if hop_length is None:
+        hop_length = win_length // 2
+    n_stft = n_fft // 2 + 1
+
+    spectrograms = extract_spectrogram_from_audios(audios, n_fft, win_length, hop_length)
+
+    mel_filter_banks = []
+    for i, audio in enumerate(audios):
+        try:
+            melscale_transform = torchaudio.transforms.MelScale(
+                sample_rate=audio.sampling_rate, n_mels=n_mels, n_stft=n_stft
+            )
+            mel_filter_banks.append({"mel_filter_bank": melscale_transform(spectrograms[i]["spectrogram"]).squeeze(0)})
+        except RuntimeError:
+            mel_filter_banks.append({"mel_filter_bank": np.nan})
+    return mel_filter_banks
+
+
+def extract_mel_filter_bank_from_spectrograms(
+    spectrograms: List[Dict[str, torch.Tensor]],
+    sampling_rate: int,
+    n_mels: int = 128,
+) -> List[Dict[str, torch.Tensor]]:
+    """Extract mel filter bank from a list of audio objects.
+
+    Args:
+        spectrograms (List[torch.Tensor]): List of spectrograms.
+        sampling_rate (int): Sampling rate of the audio.
+        n_mels (int): Number of mel filter banks. Default is 128.
+
+    Returns:
+        List[Dict[str, torch.Tensor]]: List of Dict objects containing mel filter banks.
+    """
+    if not TORCHAUDIO_AVAILABLE:
+        raise ModuleNotFoundError(
+            "`torchaudio` is not installed. "
+            "Please install senselab audio dependencies using `pip install 'senselab[audio]'`."
+        )
+
+    mel_filter_banks = []
+    for spectrogram in spectrograms:
+        try:
+            melscale_transform = torchaudio.transforms.MelScale(
+                sample_rate=sampling_rate, n_mels=n_mels, n_stft=spectrogram["spectrogram"].shape[0]
+            )
+            mel_filter_banks.append({"mel_filter_bank": melscale_transform(spectrogram["spectrogram"]).squeeze(0)})
+        except RuntimeError:
+            mel_filter_banks.append({"mel_filter_bank": np.nan})
+    return mel_filter_banks
+
+
+def extract_pitch_from_audios(
+    audios: List[Audio], freq_low: int = 80, freq_high: int = 500
+) -> List[Dict[str, torch.Tensor]]:
+    """Extract pitch from a list of audio objects.
+
+    Pitch is detected using the detect_pitch_frequency function from torchaudio.
+    It is implemented using normalized cross-correlation function and median smoothing.
+
+    Args:
+        audios (List[Audio]): List of Audio objects.
+        freq_low (int): Lowest frequency that can be detected (Hz). Should be bigger than 0.
+            (Default is 80).
+        freq_high (int): Highest frequency that can be detected (Hz).
+            (Default is 500).
+
+    Returns:
+        List[Dict[str, torch.Tensor]]: List of Dict objects containing pitches.
+    """
+    if not TORCHAUDIO_AVAILABLE:
+        raise ModuleNotFoundError(
+            "`torchaudio` is not installed. "
+            "Please install senselab audio dependencies using `pip install 'senselab[audio]'`."
+        )
+
+    if freq_low <= 0:
+        raise ValueError("freq_low should be bigger than 0")
+
+    pitches = []
+    for audio in audios:
+        try:
+            pitches.append(
+                {
+                    "pitch": torchaudio.functional.detect_pitch_frequency(
+                        audio.waveform, sample_rate=audio.sampling_rate, freq_low=freq_low, freq_high=freq_high
+                    ).squeeze(0)
+                }
+            )
+        except RuntimeError:
+            pitches.append({"pitch": torch.tensor(torch.nan)})
+    return pitches
+
+
+def extract_torchaudio_features_from_audios(
+    audios: List[Audio],
+    freq_low: int = 80,
+    freq_high: int = 500,
+    n_fft: int = 1024,
+    n_mels: int = 128,
+    n_mfcc: int = 40,
+    win_length: Optional[int] = None,
+    hop_length: Optional[int] = None,
+    plugin: str = "serial",
+    plugin_args: Optional[Dict[str, Any]] = {},
+    cache_dir: Optional[str | os.PathLike] = None,
+) -> List[Dict[str, Any]]:
+    """Extract torchaudio features from a list of audio objects.
+
+    Args:
+        audios (List[Audio]): The list of audio objects to extract features from.
+        freq_low (int): Lowest frequency that can be detected (Hz). Should be bigger than 0.
+            (Default is 80).
+        freq_high (int): Highest frequency that can be detected (Hz).
+            (Default is 500).
+        n_fft (int): Size of FFT, creates n_fft // 2 + 1 bins. Default is 1024.
+        n_mels (int): Number of mel filter banks. Default is 128.
+        n_mfcc (int): Number of MFCCs. Default is 40.
+        win_length (int): Window size. Default is None, using n_fft.
+        hop_length (int): Length of hop between STFT windows. Default is None, using win_length // 2.
+        plugin (str): The plugin to use. Default is "serial".
+        plugin_args (Optional[Dict[str, Any]]): The arguments to pass to the plugin. Default is {}.
+        cache_dir (Optional[str | os.PathLike]): The directory to cache the results. Default is None.
+
+    Returns:
+        List[Dict[str, Any]]: The list of feature dictionaries for each audio.
+    """
+    extract_pitch_from_audios_pt = pydra.mark.task(extract_pitch_from_audios)
+    extract_mel_filter_bank_from_spectrograms_pt = pydra.mark.task(extract_mel_filter_bank_from_spectrograms)
+    extract_mfcc_from_audios_pt = pydra.mark.task(extract_mfcc_from_audios)
+    extract_mel_spectrogram_from_audios_pt = pydra.mark.task(extract_mel_spectrogram_from_audios)
+    extract_spectrogram_from_audios_pt = pydra.mark.task(extract_spectrogram_from_audios)
+
+    def _extract_sampling_rate(audios: List[Audio]) -> int:
+        """Extract the sampling rate from an Audio object."""
+        return audios[0].sampling_rate
+
+    _extract_sampling_rate_pt = pydra.mark.task(_extract_sampling_rate)
+
+    formatted_audios = [[audio] for audio in audios]
+    wf = pydra.Workflow(name="wf", input_spec=["x"], cache_dir=cache_dir)
+    wf.split("x", x=formatted_audios)
+    wf.add(_extract_sampling_rate_pt(name="_extract_sampling_rate_pt", audios=wf.lzin.x))
+    wf.add(
+        extract_pitch_from_audios_pt(
+            name="extract_pitch_from_audios_pt", audios=wf.lzin.x, freq_low=freq_low, freq_high=freq_high
+        )
+    )
+    wf.add(
+        extract_spectrogram_from_audios_pt(
+            name="extract_spectrogram_from_audios_pt",
+            audios=wf.lzin.x,
+            n_fft=n_fft,
+            win_length=win_length,
+            hop_length=hop_length,
+        )
+    )
+    wf.add(
+        extract_mel_spectrogram_from_audios_pt(
+            name="extract_mel_spectrogram_from_audios_pt",
+            audios=wf.lzin.x,
+            n_mels=n_mels,
+            n_fft=n_fft,
+            win_length=win_length,
+            hop_length=hop_length,
+        )
+    )
+    wf.add(
+        extract_mel_filter_bank_from_spectrograms_pt(
+            name="extract_mel_filter_bank_from_spectrograms_pt",
+            spectrograms=wf.extract_spectrogram_from_audios_pt.lzout.out,
+            sampling_rate=wf._extract_sampling_rate_pt.lzout.out,
+            n_mels=n_mels,
+            n_fft=n_fft,
+        )
+    )
+    wf.add(
+        extract_mfcc_from_audios_pt(
+            name="extract_mfcc_from_audios_pt",
+            audios=wf.lzin.x,
+            n_mfcc=n_mfcc,
+            n_fft=n_fft,
+            n_mels=n_mels,
+            win_length=win_length,
+            hop_length=hop_length,
+        )
+    )
+
+    # setting multiple workflow outputs
+    wf.set_output(
+        [
+            ("pitch_out", wf.extract_pitch_from_audios_pt.lzout.out),
+            ("mel_filter_bank_out", wf.extract_mel_filter_bank_from_spectrograms_pt.lzout.out),
+            ("mfcc_out", wf.extract_mfcc_from_audios_pt.lzout.out),
+            ("mel_spectrogram_out", wf.extract_mel_spectrogram_from_audios_pt.lzout.out),
+            ("spectrogram_out", wf.extract_spectrogram_from_audios_pt.lzout.out),
+        ]
+    )
+
+    with pydra.Submitter(plugin=plugin, **plugin_args) as sub:
+        sub(wf)
+
+    outputs = wf.result()
+
+    formatted_output: List[Dict[str, Any]] = []
+    for output in outputs:
+        formatted_output_item = {
+            "pitch": output.output.pitch_out[0]["pitch"],
+            "mel_filter_bank": output.output.mel_filter_bank_out[0]["mel_filter_bank"],
+            "mfcc": output.output.mfcc_out[0]["mfcc"],
+            "mel_spectrogram": output.output.mel_spectrogram_out[0]["mel_spectrogram"],
+            "spectrogram": output.output.spectrogram_out[0]["spectrogram"],
+        }
+
+        formatted_output.append(formatted_output_item)
+
+    return formatted_output
