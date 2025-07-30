bff527b7c4875dfc8b1bd568f370354b23e28bfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:11:10 +0000

    Add package asyncz/0.13.3/asyncz-0.13.3.tar.gz

---
+++ packages/asyncz/asyncz-0.13.3.tar.gz/asyncz-0.13.3/asyncz/schedulers/utils.py
@@ -0,0 +1,15 @@
+from functools import partial, wraps
+from typing import Any, Callable
+
+
+def run_in_event_loop(fn: Callable[..., Any]) -> Any:
+    """
+    Decorator to run in an event loop.
+    """
+
+    @wraps(fn)
+    def wrapper(self, *args: Any, **kwargs: Any) -> None:  # type: ignore
+        wrapped = partial(fn, self, *args, **kwargs)
+        self.event_loop.call_soon_threadsafe(wrapped)
+
+    return wrapper
