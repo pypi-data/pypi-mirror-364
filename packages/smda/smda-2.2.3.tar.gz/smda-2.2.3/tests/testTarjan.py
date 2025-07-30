3fd59c3efd5c64edc40201f6faf6ea4a99a615b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:29:20 +0000

    Add package smda/2.2.3/smda-2.2.3.tar.gz

---
+++ packages/smda/smda-2.2.3.tar.gz/smda-2.2.3/tests/testTarjan.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import logging
+import os
+import unittest
+
+from smda.common.Tarjan import Tarjan
+
+from .context import config
+
+LOG = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logging.disable(logging.CRITICAL)
+
+
+class TarjanTestSuite(unittest.TestCase):
+    """Provoke recursion"""
+
+    def testInstructionEscaping(self):
+        test_data = {i: [] for i in range(1000)}
+        for i in range(1, 1000):
+            for j in range(i + 1, 1000, 1):
+                test_data[i].append(j)
+        test_data[1000] = []
+
+        tarjan = Tarjan(test_data)
+        tarjan.calculateScc()
+        sccs = tarjan.getResult()
+        self.assertEqual(1001, len(sccs))
+
+
+if __name__ == '__main__':
+    unittest.main()
