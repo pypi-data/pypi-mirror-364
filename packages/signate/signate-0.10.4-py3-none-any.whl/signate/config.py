0e8dbd66b38644e1e1be5396c69c5b62eae92cfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:59 +0000

    Add package signate/0.10.4/signate-0.10.4-py3-none-any.whl

---
+++ packages/signate/signate-0.10.4-py3-none-any.whl/signate/config.py
@@ -0,0 +1,10 @@
+CLOUD_URL = 'https://api.cloud.signate.jp/api'
+COMPETITION_URL = 'https://api.competition.signate.jp/api'
+DEFAULT_ORGANIZATION_ID = 1
+
+USER_AGENT = "python-requests/2.x"
+
+CONFIG_DIR = '~/.signate/'
+CONFIG_FILE_NAME = 'signate.json'
+
+JWT_COOKIE_KEY = 'cloud_user'
---
+++ packages/signate/signate-0.10.4-py3-none-any.whl/signate/info.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+
+NAME = 'SIGNATE CLI'
+VERSION = '0.10.4'
