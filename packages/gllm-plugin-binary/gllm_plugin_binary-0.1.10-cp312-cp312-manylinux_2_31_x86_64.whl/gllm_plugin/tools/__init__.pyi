cf2079f8f10d7fdfaaaabad98f9363ed3c8da0e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:34:03 +0000

    Add package gllm-plugin-binary/0.1.10/gllm_plugin_binary-0.1.10-cp312-cp312-manylinux_2_31_x86_64.whl

---
+++ packages/gllm-plugin-binary/gllm_plugin_binary-0.1.10-cp312-cp312-manylinux_2_31_x86_64.whl/gllm_plugin/tools/__init__.pyi
@@ -0,0 +1,3 @@
+from gllm_plugin.tools.decorators import get_plugin_metadata as get_plugin_metadata, is_tool_plugin as is_tool_plugin, tool_plugin as tool_plugin
+
+__all__ = ['tool_plugin', 'is_tool_plugin', 'get_plugin_metadata']
