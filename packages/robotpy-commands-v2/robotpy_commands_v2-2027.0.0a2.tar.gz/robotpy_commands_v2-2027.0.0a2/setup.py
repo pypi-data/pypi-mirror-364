4efa65f39150faeec64d2e569f65c6c6d5d00d81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:52 +0000

    Add package robotpy-commands-v2/2027.0.0a2/robotpy_commands_v2-2027.0.0a2.tar.gz

---
+++ packages/robotpy-commands-v2/robotpy_commands_v2-2027.0.0a2.tar.gz/robotpy_commands_v2-2027.0.0a2/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+setuptools.setup(
+    name="robotpy-commands-v2",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    author="RobotPy Development Team",
+    author_email="robotpy@googlegroups.com",
+    description="WPILib command framework v2",
+    url="https://github.com/robotpy/robotpy-commands-v2",
+    package_data={"commands2": ["py.typed"]},
+    packages=["commands2"],
+    install_requires=[
+        "wpilib<2028,>=2027.0.0a2",
+        "typing_extensions>=4.1.0,<5",
+    ],
+    license="BSD-3-Clause",
+    python_requires=">=3.9",
+    include_package_data=True,
+)
