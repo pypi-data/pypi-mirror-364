26ce17714562796f4a49e76fc9e0c8d2bac19119
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:22 +0000

    Add package hcai-discover-utils/1.0.5/hcai_discover_utils-1.0.5-py3-none-any.whl

---
+++ packages/hcai-discover-utils/hcai_discover_utils-1.0.5-py3-none-any.whl/discover_utils/__init__.py
@@ -0,0 +1,18 @@
+"""discover_utils
+
+Author:
+    Dominik Schiller <dominik.schiller@uni-a.de>
+Date:
+    06th September 2023
+
+"""
+
+_MAJOR_VERSION = '1'
+_MINOR_VERSION = '0'
+_PATCH_VERSION = '5'
+
+__version__ = '.'.join([
+    _MAJOR_VERSION,
+    _MINOR_VERSION,
+    _PATCH_VERSION,
+])
