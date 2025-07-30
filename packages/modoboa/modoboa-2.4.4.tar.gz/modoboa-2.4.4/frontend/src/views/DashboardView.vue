f758a94214fd95b1b84ea1a7a5265c28414df057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:29:17 +0000

    Add package modoboa/2.4.4/modoboa-2.4.4.tar.gz

---
+++ packages/modoboa/modoboa-2.4.4.tar.gz/modoboa-2.4.4/frontend/src/views/DashboardView.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-toolbar flat>
+    <v-toolbar-title>{{ $gettext('Welcome to Modoboa') }}</v-toolbar-title>
+  </v-toolbar>
+
+  <v-row>
+    <v-col sm="12" md="6">
+      <NewsFeedWidget />
+    </v-col>
+  </v-row>
+</template>
+
+<script setup>
+import NewsFeedWidget from '@/components/admin/dashboard/NewsFeedWidget'
+</script>
+
+<style scoped>
+.v-toolbar {
+  background-color: #f7f8fa !important;
+}
+
+.v-tabs-items {
+  background-color: #f7f8fa !important;
+}
+</style>
