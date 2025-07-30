5510ee3f67083b41c99a5e2d6db58d937aeff02d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:51 +0000

    Add package StreamJSME/0.1.0/streamjsme-0.1.0-py3-none-any.whl

---
+++ packages/StreamJSME/streamjsme-0.1.0-py3-none-any.whl/StreamJSME/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+For information of StreamJSME:
+    Source Code: https://github.com/ale94mleon/StreamJSME
+"""
+from StreamJSME._version import __version__
+from .app import StreamJSME
+__author__ = "Alejandro Martínez León"
+__email__ = "ale94mleon@gmail.com"
