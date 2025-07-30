adfb2b15a01ded87f419ca395582ffe6724747aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:39 +0000

    Add package FLImagingClrPy/6.8.5/flimagingclrpy-6.8.5-py3-none-any.whl

---
+++ packages/FLImagingClrPy/flimagingclrpy-6.8.5-py3-none-any.whl/FLImagingClrPy/__init__.py
@@ -0,0 +1,25 @@
+﻿# FLImagingClrPy version
+__version__ = "6.8.5"
+
+import clr
+from .FLImagingClrPyLoader import load_dll
+
+# FLImaging dll Load
+load_dll()
+
+from FLImagingCLR import *
+from FLImagingCLR.Base import *
+from FLImagingCLR.Foundation import *
+from FLImagingCLR.GUI import *
+from FLImagingCLR.ImageProcessing import *
+from FLImagingCLR.AdvancedFunctions import *
+from FLImagingCLR.ThreeDim import *
+from FLImagingCLR.AI import *
+from FLImagingCLR.Devices import *
+
+clr.AddReference("System")
+clr.AddReference("mscorlib")
+
+from System import *
+from System.Text import *
+from System.Collections.Generic import *
