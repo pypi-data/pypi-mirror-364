cad2f6d5293eadf6ff17c0dbd111421dccca7256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:46 +0000

    Add package csust-word-mcp-server/0.0.6/csust_word_mcp_server-0.0.6-py3-none-any.whl

---
+++ packages/csust-word-mcp-server/csust_word_mcp_server-0.0.6-py3-none-any.whl/csust_word_mcp_server/server.py
@@ -0,0 +1,22 @@
+from fastmcp import FastMCP
+import asyncio
+
+API_BASE = "https://word-assistant"
+
+mcp = FastMCP('word-assistant')
+
+@mcp.tool(name='word文档生成助手')
+async def generate(text: str) -> str:
+    """word生成
+
+    Args:
+        state: 模型返回的文本内容
+    """
+
+    return text
+   
+if __name__ == "__main__":
+    # asyncio.run(mcp.run_sse_async(host="0.0.0.0", port=8000))
+    mcp.run(transport="stdio")
+
+    
