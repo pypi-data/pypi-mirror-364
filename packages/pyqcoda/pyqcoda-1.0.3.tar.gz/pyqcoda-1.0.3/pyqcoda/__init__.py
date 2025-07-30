879b4f23452a9f806a744e6cd1a449644804e7dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:07:22 +0000

    Add package pyqcoda/1.0.3/pyqcoda-1.0.3.tar.gz

---
+++ packages/pyqcoda/pyqcoda-1.0.3.tar.gz/pyqcoda-1.0.3/pyqcoda/__init__.py
@@ -0,0 +1,12 @@
+"""
+pyqcoda: Temporal disaggregation of daily precipitation into hourly using Q-CODA.
+
+Main class:
+    - pyqcoda: Implements the disaggregation algorithm.
+"""
+
+from .core import pyqcoda  
+
+__all__ = ["pyqcoda"]
+__version__ = "1.0.0"
+
