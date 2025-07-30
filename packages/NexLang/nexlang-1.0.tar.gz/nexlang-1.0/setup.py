e8b98bf1f16c635c2197d430f354a889dc287f9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:37 +0000

    Add package NexLang/1.0/nexlang-1.0.tar.gz

---
+++ packages/NexLang/nexlang-1.0.tar.gz/nexlang-1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+    name='NexLang', 
+    version='1.0',
+    packages=find_packages(),
+    author='D',
+    author_email='nasr2python@gmail.com',
+    description='What',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: OS Independent',
+        ]
+) 
