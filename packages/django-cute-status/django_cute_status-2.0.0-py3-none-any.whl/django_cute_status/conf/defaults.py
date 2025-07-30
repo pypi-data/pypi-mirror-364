2b0855a48a5463dd96cd9cee0306901a61c1d71e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:10 +0000

    Add package django-cute-status/2.0.0/django_cute_status-2.0.0-py3-none-any.whl

---
+++ packages/django-cute-status/django_cute_status-2.0.0-py3-none-any.whl/django_cute_status/conf/defaults.py
@@ -0,0 +1,43 @@
+# this is added here for easy override to a new vendor in case this service is no longer available
+import os
+
+
+CUTE_STATUS_BASE_URLS = (
+    "https://http.dog/{status_code}.jpg",
+    "https://http.cat/{status_code}.jpg",
+)
+
+CUTE_STATUS_BASE_URL = CUTE_STATUS_BASE_URLS[0]
+
+CUTE_STATUS_USE_RANDOM = True
+
+CUTE_STATUS_FOR_CODES = (
+    204,
+    400,
+    401,
+    402,
+    403,
+    404,
+    405,
+    429,
+    500,
+    501,
+    502,
+    503,
+    504,
+)
+
+CUTE_STATUS_NOT_FOR_CODES = (
+    200,
+    201,
+    301,
+    302,
+    307,
+    308,
+)
+
+CUTE_STATUS_HEADER = "X-Cute-Status"
+
+CUTE_STATUS_OVERRIDE_HEADER_ON_ALL = False
+
+CUTE_STATUS_BASE_TEMPLATE = os.path.join("django_cute_status", "base.html")
