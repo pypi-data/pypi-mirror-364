48f352562182155f59a39541edd88bd39b923c06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:34:06 +0000

    Add package yuanlanlab-tool/0.0.0.6/yuanlanlab_tool-0.0.0.6.tar.gz

---
+++ packages/yuanlanlab-tool/yuanlanlab_tool-0.0.0.6.tar.gz/yuanlanlab_tool-0.0.0.6/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.0.6'
+
+setup(
+    name='yuanlanlab_tool',
+    version=VERSION,
+    author='元蓝先生@B站',
+    description='空间链接：https://space.bilibili.com/3546564903569609',
+    license='This work is licensed under a Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.',
+    packages=find_packages()
+)
