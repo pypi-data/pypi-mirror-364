8e7fa4dbaddcae25f34df6fbb78690fae820a7e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:35:04 +0000

    Add package passagemath-combinat/10.5.48/passagemath_combinat-10.5.48-cp310-cp310-musllinux_1_2_aarch64.whl

---
+++ packages/passagemath-combinat/passagemath_combinat-10.5.48-cp310-cp310-musllinux_1_2_aarch64.whl/sage/libs/symmetrica/symmetrica.pyx
@@ -0,0 +1,21 @@
+# sage_setup: distribution = sagemath-combinat
+# distutils: libraries = symmetrica
+"""
+Symmetrica library
+"""
+
+include "symmetrica.pxi"
+
+include "kostka.pxi"
+
+include "sab.pxi"
+
+include "sc.pxi"
+
+include "sb.pxi"
+
+include "part.pxi"
+
+include "schur.pxi"
+
+include "plet.pxi"
