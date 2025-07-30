3115771426a83b7e7a1d4ed2c24a376c07d58bc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:48 +0000

    Add package HamiltonIO/0.2.5/hamiltonio-0.2.5.tar.gz

---
+++ packages/HamiltonIO/hamiltonio-0.2.5.tar.gz/hamiltonio-0.2.5/HamiltonIO/__init__.py
@@ -0,0 +1,5 @@
+from HamiltonIO.hamiltonian import Hamiltonian
+from HamiltonIO.siesta import SiestaHam
+from HamiltonIO.wannier import WannierHam
+
+__all__ = ["Hamiltonian", "SiestaHam", "WannierHam"]
---
+++ packages/HamiltonIO/hamiltonio-0.2.5.tar.gz/hamiltonio-0.2.5/README.md
@@ -0,0 +1,12 @@
+# HamiltonIO
+A library of IO of Hamiltonian files of DFT codes
+
+## Installation
+```bash
+pip install hamiltonIO
+```
+
+## Usage
+
+See the examples.
+
