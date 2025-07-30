b420eb5c529c08c64f5234522857604e4d75703c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:07:46 +0000

    Add package mtbmasker/0.1.0/mtbmasker-0.1.0.tar.gz

---
+++ packages/mtbmasker/mtbmasker-0.1.0.tar.gz/mtbmasker-0.1.0/mtbmasker/__init__.py
@@ -0,0 +1,13 @@
+
+---
+
+### ✅ `mtbmasker/__init__.py`
+
+```python
+"""
+mtbmasker: Create isolate-specific genome masks for M. tuberculosis.
+Author: Etienne Ntumba, Dan Whiley
+"""
+
+__version__ = "0.1.0"
+__author__ = "Etienne Ntumba, Dan Whiley"
