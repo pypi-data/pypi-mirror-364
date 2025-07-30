8a740e568492079b488e87742142f08f3f284fa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:11 +0000

    Add package scipplan/0.2.2a0/scipplan-0.2.2a0.tar.gz

---
+++ packages/scipplan/scipplan-0.2.2a0.tar.gz/scipplan-0.2.2a0/scipplan/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.2.2alpha0"
+print(f"SCIPPlan Version: {__version__}")
+__release__ = "v0.2.2a0"
+__author__ = "Ari Gestetner, Buser Say"
+__email__ = "ari.gestetner@monash.edu, buser.say@monash.edu"
