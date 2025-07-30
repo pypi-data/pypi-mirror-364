438910e47fc5087a4b40fbbcfb930879446663b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:41 +0000

    Add package xcode-mcp-server/1.0.4/xcode_mcp_server-1.0.4.tar.gz

---
+++ packages/xcode-mcp-server/xcode_mcp_server-1.0.4.tar.gz/xcode_mcp_server-1.0.4/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+rm -rf dist
+hatch version patch
+python -m build
+twine upload dist/*
+
+echo "If the above was successful, run with: uvx xcode-mcp-server"
+echo "If it failed, you probably have to bump the version number in pyproject.toml"
+exit 0
