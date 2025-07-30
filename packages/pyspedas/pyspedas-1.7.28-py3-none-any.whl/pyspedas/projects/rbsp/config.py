1ef8e0952e75ba4f9a5d7e7c607ce53eb7f8700f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:13:22 +0000

    Add package pyspedas/1.7.28/pyspedas-1.7.28-py3-none-any.whl

---
+++ packages/pyspedas/pyspedas-1.7.28-py3-none-any.whl/pyspedas/projects/rbsp/config.py
@@ -0,0 +1,15 @@
+import os
+
+CONFIG = {'local_data_dir': 'rbsp_data/',
+          'remote_data_dir': 'https://spdf.gsfc.nasa.gov/pub/data/rbsp/'}
+
+# override local data directory with environment variables
+if os.environ.get('SPEDAS_DATA_DIR'):
+    CONFIG['local_data_dir'] = os.sep.join([os.environ['SPEDAS_DATA_DIR'], 'rbsp'])
+
+if os.environ.get('RBSP_DATA_DIR'):
+    CONFIG['local_data_dir'] = os.environ['RBSP_DATA_DIR']
+    
+    
+CONFIG_ECT = CONFIG.copy()
+CONFIG_ECT['remote_data_dir'] = "https://rbsp-ect.newmexicoconsortium.org/data_pub/"
