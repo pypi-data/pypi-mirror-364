8429bd833015929926d569190f6f35d4db7ff4ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:31 +0000

    Add package hanna-cloud/0.0.4/hanna_cloud-0.0.4-py3-none-any.whl

---
+++ packages/hanna-cloud/hanna_cloud-0.0.4-py3-none-any.whl/hanna_cloud/__init__.py
@@ -0,0 +1,10 @@
+"""
+HannaCloud API Client
+A Python client for interacting with the HannaCloud API.
+"""
+
+__version__ = "0.0.4"
+
+from .client import HannaCloudClient
+
+__all__ = ["HannaCloudClient"]
