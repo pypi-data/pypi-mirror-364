bfad1641b95c09dd15d6997e42da3991de8a345f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:20 +0000

    Add package localstack-ext/4.6.1.dev132/localstack_ext-4.6.1.dev132.tar.gz

---
+++ packages/localstack-ext/localstack_ext-4.6.1.dev132.tar.gz/localstack_ext-4.6.1.dev132/localstack/pro/core/eventstudio/utils/__init__.py
@@ -0,0 +1,4 @@
+from localstack.pro.core.eventstudio.utils.logger import get_eventstudio_logger
+from localstack.pro.core.eventstudio.utils.utils import gen_16_char_hex_string
+
+__all__ = ["get_eventstudio_logger", "gen_16_char_hex_string"]
---
+++ packages/localstack-ext/localstack_ext-4.6.1.dev132.tar.gz/localstack_ext-4.6.1.dev132/localstack/pro/core/extensions/plugins.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+import logging
+from localstack.pro.core import config
+from localstack.pro.core.runtime.plugin import ProPlatformPlugin
+from localstack.runtime import hooks
+from rolo.routing import Router
+from rolo.routing.handler import Handler as RouteHandler
+EXTENSION_HOOK_PRIORITY=-1
+LOG=logging.getLogger(__name__)
+@hooks.configure_localstack_container(should_load=config.ACTIVATE_PRO and config.EXTENSION_DEV_MODE)
+def configure_extensions_dev_container(container):from localstack.pro.core.extensions.bootstrap import run_on_configure_localstack_container_hook as A;A(container)
+@hooks.prepare_host(should_load=config.ACTIVATE_PRO and config.EXTENSION_DEV_MODE)
+def configure_extensions_dev_host():from localstack.pro.core.extensions.bootstrap import run_on_configure_host_hook as A;A()
+class ExtensionsPlugin(ProPlatformPlugin):
+	priority=EXTENSION_HOOK_PRIORITY;name='extensions'
+	def on_platform_start(H):from localstack.aws.handlers import run_custom_finalizers as B,run_custom_response_handlers as C,serve_custom_exception_handlers as D,serve_custom_service_request_handlers as E;from localstack.pro.core.extensions.manager import ExtensionsManager as F;from localstack.services import edge;A=F.get();G=A.load_all();LOG.info('loaded %s extensions',len(G));LOG.debug('calling extensions on_platform_start');A.call_on_platform_start();LOG.debug('calling extensions update_gateway_routes');A.call_update_gateway_routes(edge.ROUTER);LOG.debug('calling extensions update_request_handlers');A.call_update_request_handlers(E);LOG.debug('calling extensions update_response_handlers');A.call_update_response_handlers(C);LOG.debug('calling extensions update_exception_handlers');A.call_update_exception_handlers(D);LOG.debug('calling extensions update_finalizers');A.call_update_finalizers(B)
+	def on_platform_ready(C):from localstack.pro.core.extensions.manager import ExtensionsManager as A;B=A.get();LOG.debug('calling extensions on_platform_ready');B.call_on_platform_ready()
+	def on_platform_shutdown(C):from localstack.pro.core.extensions.manager import ExtensionsManager as A;B=A.get();LOG.debug('calling extensions on_platform_shutdown');B.call_on_platform_shutdown()
+	def update_localstack_routes(B,router):from localstack.pro.core.extensions.resource import ExtensionsApi as A;router.add(A())
