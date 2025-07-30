bc1fcb6bc8900f325618c5bc0e37f111abd1bb72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:10:34 +0000

    Add package uv/0.8.3/uv-0.8.3.tar.gz

---
+++ packages/uv/uv-0.8.3.tar.gz/uv-0.8.3/crates/uv-warnings/src/lib.rs
@@ -0,0 +1,58 @@
+use std::sync::atomic::AtomicBool;
+use std::sync::{LazyLock, Mutex};
+
+// macro hygiene: The user might not have direct dependencies on those crates
+#[doc(hidden)]
+pub use anstream;
+#[doc(hidden)]
+pub use owo_colors;
+use rustc_hash::FxHashSet;
+
+/// Whether user-facing warnings are enabled.
+pub static ENABLED: AtomicBool = AtomicBool::new(false);
+
+/// Enable user-facing warnings.
+pub fn enable() {
+    ENABLED.store(true, std::sync::atomic::Ordering::Relaxed);
+}
+
+/// Disable user-facing warnings.
+pub fn disable() {
+    ENABLED.store(false, std::sync::atomic::Ordering::Relaxed);
+}
+
+/// Warn a user, if warnings are enabled.
+#[macro_export]
+macro_rules! warn_user {
+    ($($arg:tt)*) => {{
+        use $crate::anstream::eprintln;
+        use $crate::owo_colors::OwoColorize;
+
+        if $crate::ENABLED.load(std::sync::atomic::Ordering::Relaxed) {
+            let message = format!("{}", format_args!($($arg)*));
+            let formatted = message.bold();
+            eprintln!("{}{} {formatted}", "warning".yellow().bold(), ":".bold());
+        }
+    }};
+}
+
+pub static WARNINGS: LazyLock<Mutex<FxHashSet<String>>> = LazyLock::new(Mutex::default);
+
+/// Warn a user once, if warnings are enabled, with uniqueness determined by the content of the
+/// message.
+#[macro_export]
+macro_rules! warn_user_once {
+    ($($arg:tt)*) => {{
+        use $crate::anstream::eprintln;
+        use $crate::owo_colors::OwoColorize;
+
+        if $crate::ENABLED.load(std::sync::atomic::Ordering::Relaxed) {
+            if let Ok(mut states) = $crate::WARNINGS.lock() {
+                let message = format!("{}", format_args!($($arg)*));
+                if states.insert(message.clone()) {
+                    eprintln!("{}{} {}", "warning".yellow().bold(), ":".bold(), message.bold());
+                }
+            }
+        }
+    }};
+}
