e76b1002c8799023475757e3cb178b5a22613014
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:22 +0000

    Add package quakemigrate/1.2.1/quakemigrate-1.2.1-cp39-cp39-macosx_14_0_arm64.whl

---
+++ packages/quakemigrate/quakemigrate-1.2.1-cp39-cp39-macosx_14_0_arm64.whl/quakemigrate/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+QuakeMigrate - a Python package for automatic earthquake detection and location
+using waveform migration and stacking.
+
+:copyright:
+    2020–2023, QuakeMigrate developers.
+:license:
+    GNU General Public License, Version 3
+    (https://www.gnu.org/licenses/gpl-3.0.html)
+
+"""
+
+from importlib.metadata import version
+
+import matplotlib
+import os
+import logging
+
+from quakemigrate.io.data import Archive  # NOQA
+from quakemigrate.lut import create_lut, read_nlloc, LUT  # NOQA
+from quakemigrate.signal import QuakeScan, Trigger  # NOQA
+
+
+# Set matplotlib logging level and backend
+logging.getLogger("matplotlib").setLevel(logging.INFO)
+if "DISPLAY" not in os.environ:
+    matplotlib.use("Agg")
+
+__version__ = version("quakemigrate")
