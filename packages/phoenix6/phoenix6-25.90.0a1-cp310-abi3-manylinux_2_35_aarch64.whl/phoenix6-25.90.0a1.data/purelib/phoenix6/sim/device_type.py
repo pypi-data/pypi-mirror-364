a1fd118da0928a23a234326be785ed5f71441c1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:37 +0000

    Add package phoenix6/25.90.0a1/phoenix6-25.90.0a1-cp310-abi3-manylinux_2_35_aarch64.whl

---
+++ packages/phoenix6/phoenix6-25.90.0a1-cp310-abi3-manylinux_2_35_aarch64.whl/phoenix6-25.90.0a1.data/purelib/phoenix6/sim/device_type.py
@@ -0,0 +1,27 @@
+"""
+Copyright (C) Cross The Road Electronics.  All rights reserved.
+License information can be found in CTRE_LICENSE.txt
+For support and suggestions contact support@ctr-electronics.com or file
+an issue tracker at https://github.com/CrossTheRoadElec/Phoenix-Releases
+"""
+
+from enum import Enum
+
+
+class DeviceType(Enum):
+    """
+    Enumeration of all supported device types.
+    """
+    TalonSRXType = 0
+    VictorSPXType = 1
+    PigeonIMUType = 2
+    RibbonPigeonIMUType = 3
+    CANCoderType = 4
+    P6_TalonFXType = 5
+    P6_CANcoderType = 6
+    P6_Pigeon2Type = 7
+    P6_TalonFXSType = 8
+    P6_CANrangeType = 9
+    P6_CANdiType = 10
+    P6_CANdleType = 11
+
