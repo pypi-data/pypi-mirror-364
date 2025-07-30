ac950da3d8a33323e841ac406c116a05698eece6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:02 +0000

    Add package pyrcv/1.1.12/pyrcv-1.1.12-py2.py3-none-any.whl

---
+++ packages/pyrcv/pyrcv-1.1.12-py2.py3-none-any.whl/pyrcv/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for pyrcv."""
+
+__author__ = """Chris Roat"""
+__email__ = "chris.roat@gmail.com"
+__version__ = "1.1.12"
+
+from .pyrcv import *  # noqa
+from .transform import *  # noqa
+from .types import *  # noqa
+from .viz import *  # noqa
