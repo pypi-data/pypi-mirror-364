0e247a62f48e93a74eb2d5b31a1d51e950a94967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:07:35 +0000

    Add package iiko-sdk/0.0.4/iiko_sdk-0.0.4.tar.gz

---
+++ packages/iiko-sdk/iiko_sdk-0.0.4.tar.gz/iiko_sdk-0.0.4/iiko_sdk/gateways/authorization/v1/client.py
@@ -0,0 +1,29 @@
+from http_misc import http_utils, services
+
+
+class AuthorizationGateway:
+    """ Authorization API """
+    version = 'api/1'
+
+    def __init__(self, base_url: str):
+        self.base_url = base_url
+
+    async def get_api_token(self, api_login: str) -> str:
+        """ Получить api_token
+        POST /api/1/access_token """
+        url = http_utils.join_str(self.base_url, self.version, '/access_token')
+        request = {
+            'method': 'POST',
+            'url': url,
+            'cfg': {
+                'json': {
+                    'apiLogin': api_login
+                }
+            }
+        }
+
+        response_data = await http_utils.send_and_validate(services.HttpService(), request)
+        if 'token' not in response_data:
+            raise KeyError('Invalid response. Token is absent.')
+
+        return response_data['token']
