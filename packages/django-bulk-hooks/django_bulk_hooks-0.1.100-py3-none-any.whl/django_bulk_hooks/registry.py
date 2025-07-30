c6495183dd578dc17049ba4d00d7f83dc9593e84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:34 +0000

    Add package django-bulk-hooks/0.1.100/django_bulk_hooks-0.1.100-py3-none-any.whl

---
+++ packages/django-bulk-hooks/django_bulk_hooks-0.1.100-py3-none-any.whl/django_bulk_hooks/registry.py
@@ -0,0 +1,25 @@
+from collections.abc import Callable
+from typing import Union
+
+from django_bulk_hooks.enums import Priority
+
+_hooks: dict[tuple[type, str], list[tuple[type, str, Callable, int]]] = {}
+
+
+def register_hook(
+    model, event, handler_cls, method_name, condition, priority: Union[int, Priority]
+):
+    key = (model, event)
+    hooks = _hooks.setdefault(key, [])
+    hooks.append((handler_cls, method_name, condition, priority))
+    # keep sorted by priority
+    hooks.sort(key=lambda x: x[3])
+
+
+def get_hooks(model, event):
+    return _hooks.get((model, event), [])
+
+
+def list_all_hooks():
+    """Debug function to list all registered hooks"""
+    return _hooks
