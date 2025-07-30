f424b7f598f9ace7f77afc2bcb8c9a1f3eb50bad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:44 +0000

    Add package marmel-grammar/0.1.2/marmel_grammar-0.1.2.tar.gz

---
+++ packages/marmel-grammar/marmel_grammar-0.1.2.tar.gz/marmel_grammar-0.1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="marmel-grammar",
+    version="0.1.2",
+    description="Грамматика Marmel для Python.",
+    author="Dev-Marmel",
+    packages=find_packages(),
+    python_requires=">=3.7",
+)
