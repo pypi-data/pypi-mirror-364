85659664905efe41a3bebb9429c839622daf0113
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:10:27 +0000

    Add package model-checker/0.9.30/model_checker-0.9.30.tar.gz

---
+++ packages/model-checker/model_checker-0.9.30.tar.gz/model_checker-0.9.30/src/model_checker/theory_lib/imposition/operators.py
@@ -0,0 +1,149 @@
+import z3
+
+from model_checker.utils import (
+    ForAll,
+    Exists,
+)
+from model_checker import syntactic
+
+# Import operator collections from logos subtheories for reuse
+from model_checker.theory_lib.logos.subtheories.extensional.operators import (
+    get_operators as get_extensional_operators,
+    NegationOperator,
+    TopOperator,
+)
+from model_checker.theory_lib.logos.subtheories.modal.operators import (
+    NecessityOperator,
+    PossibilityOperator,
+)
+
+##############################################################################
+####################### PRIMITIVE IMPOSITION OPERATORS #######################
+##############################################################################
+
+class ImpositionOperator(syntactic.Operator):
+    name = "\\imposition"
+    arity = 2
+
+    def true_at(self, leftarg, rightarg, eval_point):
+        sem = self.semantics
+        N = sem.N
+        x = z3.BitVec("t_imp_x", N)
+        u = z3.BitVec("t_imp_u", N)
+        eval_world = eval_point["world"]
+        return ForAll(
+            [x, u],
+            z3.Implies(
+                z3.And(
+                    sem.extended_verify(x, leftarg, eval_point),
+                    sem.imposition(x, eval_world, u)
+                ),
+                sem.true_at(rightarg, {"world": u}),
+            ),
+        )
+    
+    def false_at(self, leftarg, rightarg, eval_point):
+        sem = self.semantics
+        N = sem.N
+        x = z3.BitVec("f_imp_x", N)
+        u = z3.BitVec("f_imp_u", N)
+        eval_world = eval_point["world"]
+        return Exists(
+            [x, u],
+            z3.And(
+                sem.extended_verify(x, leftarg, eval_point),
+                sem.imposition(x, eval_world, u),
+                sem.false_at(rightarg, {"world": u})),
+            )
+
+    def extended_verify(self, state, leftarg, rightarg, eval_point):
+        # TODO: add constraint which requires state to be the null_state
+        return self.true_at(leftarg, rightarg, eval_point)
+    
+    def extended_falsify(self, state, leftarg, rightarg, eval_point):
+        # TODO: add constraint which requires state to be the null_state
+        return self.false_at(leftarg, rightarg, eval_point)
+
+    def find_verifiers_and_falsifiers(self, leftarg, rightarg, eval_point):
+        evaluate = leftarg.proposition.model_structure.z3_model.evaluate
+        N = leftarg.proposition.model_structure.semantics.N
+        nullstate = z3.BitVecVal(0, N)  # The null state is always 0
+        if bool(evaluate(self.true_at(leftarg, rightarg, eval_point))):
+            return {nullstate}, set()
+        if bool(evaluate(self.false_at(leftarg, rightarg, eval_point))):
+            return set(), {nullstate}
+        raise ValueError(
+            f"{leftarg.name} {self.name} {rightarg.name} "
+            f"is neither true nor false in the world {eval_point}."
+        )
+    
+    def print_method(self, sentence_obj, eval_point, indent_num, use_colors):
+        """Print counterfactual and the antecedent in the eval_point. Then
+        print the consequent in each alternative to the evaluation world.
+        """
+        is_outcome = self.semantics.calculate_alternative_worlds
+        model_structure = sentence_obj.proposition.model_structure
+        left_argument_obj = sentence_obj.original_arguments[0]
+        left_argument_verifiers = left_argument_obj.proposition.verifiers
+        alt_worlds = is_outcome(left_argument_verifiers, eval_point, model_structure)
+        self.print_over_worlds(sentence_obj, eval_point, alt_worlds, indent_num, use_colors)
+
+
+##############################################################################
+######################## DEFINED IMPOSITION OPERATORS ########################
+##############################################################################
+
+class MightImpositionOperator(syntactic.DefinedOperator):
+
+    name = "\\could"
+    arity = 2
+
+    def derived_definition(self, leftarg, rightarg):
+        return [
+            NegationOperator, [
+                ImpositionOperator,
+                leftarg,
+                [NegationOperator, rightarg]
+            ]
+        ]
+
+    def print_method(self, sentence_obj, eval_point, indent_num, use_colors):
+        """Print counterfactual and the antecedent in the eval_point. Then
+        print the consequent in each alternative to the evaluation world.
+        """
+        is_outcome = self.semantics.calculate_alternative_worlds
+        model_structure = sentence_obj.proposition.model_structure
+        left_argument_obj = sentence_obj.original_arguments[0]
+        left_argument_verifiers = left_argument_obj.proposition.verifiers
+        alt_worlds = is_outcome(left_argument_verifiers, eval_point, model_structure)
+        self.print_over_worlds(sentence_obj, eval_point, alt_worlds, indent_num, use_colors)
+
+def get_imposition_operators():
+    """Get imposition-specific operators."""
+    return {
+        '\\imposition': ImpositionOperator,
+        '\\could': MightImpositionOperator,
+    }
+
+def get_all_operators():
+    """Get all operators including inherited ones from logos."""
+    operators = {}
+    
+    # Import base operators from logos subtheories
+    operators.update(get_extensional_operators())  # Basic logical operators
+    
+    # Add specific modal operators (without counterfactual dependencies)
+    operators.update({
+        "\\Box": NecessityOperator,
+        "\\Diamond": PossibilityOperator,
+    })
+    
+    # Add imposition-specific operators
+    operators.update(get_imposition_operators())
+    
+    return operators
+
+# Create operator collection for backward compatibility
+imposition_operators = syntactic.OperatorCollection()
+for op_name, op_class in get_all_operators().items():
+    imposition_operators.add_operator(op_class)
