c44ac3c54a3c00362b17251d4a47e37612684b5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:29:15 +0000

    Add package ReactiveMode/0.1/ReactiveMode-0.1.tar.gz

---
+++ packages/ReactiveMode/ReactiveMode-0.1.tar.gz/ReactiveMode-0.1/ReactiveMode/__init__.py
@@ -0,0 +1,7 @@
+from .main import SimpleEvent
+from .main import Event
+from .main import AdvancedEvent
+from .main import SimplePipeLine
+from .main import PipeLine
+from .main import SimpleEventPoint
+from .main import EventPoint
---
+++ packages/ReactiveMode/ReactiveMode-0.1.tar.gz/ReactiveMode-0.1/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name="ReactiveMode", version="0.1", packages=find_packages(), author="FallingXbot", description="Small Python Library About Events.")
