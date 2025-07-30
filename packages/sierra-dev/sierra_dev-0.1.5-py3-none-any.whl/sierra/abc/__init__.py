b725ded7f015e834e12f5a7cb1c52c3e64fb3c28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:41 +0000

    Add package sierra-dev/0.1.5/sierra_dev-0.1.5-py3-none-any.whl

---
+++ packages/sierra-dev/sierra_dev-0.1.5-py3-none-any.whl/sierra/abc/__init__.py
@@ -0,0 +1,33 @@
+"""
+Sierra ABCs.
+============
+
+Abstract Base Classes (ABCs) for Sierra Dev components.
+
+# Overview
+--------
+
+This package contains abstract base classes (ABCs) for components of the
+Sierra Dev framework.
+
+# Exposed Components
+-----------------
+
+- `SierraABC`: Abstract base class for Sierra components.
+- `SierraConfig`: Top-level configuration for SIERRA invoker scripts.
+- `SierraInvokerParam`: Parameter description for invoker scripts.
+- `SierraInvokerScript`: Invoker script definition.
+
+"""
+
+from sierra.abc.base import SierraABC
+from sierra.abc.sierra import SierraConfig
+from sierra.abc.sierra import SierraInvokerParam
+from sierra.abc.sierra import SierraInvokerScript
+
+__all__ = [
+    "SierraABC",
+    "SierraConfig",
+    "SierraInvokerParam",
+    "SierraInvokerScript",
+]
