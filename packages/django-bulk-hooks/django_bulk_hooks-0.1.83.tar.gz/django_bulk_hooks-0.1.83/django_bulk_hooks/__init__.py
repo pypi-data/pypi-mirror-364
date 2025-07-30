11030e0861e83caa40eafd0dea92ed813aff0b40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:35:26 +0000

    Add package django-bulk-hooks/0.1.83/django_bulk_hooks-0.1.83.tar.gz

---
+++ packages/django-bulk-hooks/django_bulk_hooks-0.1.83.tar.gz/django_bulk_hooks-0.1.83/django_bulk_hooks/__init__.py
@@ -0,0 +1,50 @@
+from django_bulk_hooks.constants import (
+    AFTER_CREATE,
+    AFTER_DELETE,
+    AFTER_UPDATE,
+    BEFORE_CREATE,
+    BEFORE_DELETE,
+    BEFORE_UPDATE,
+    VALIDATE_CREATE,
+    VALIDATE_DELETE,
+    VALIDATE_UPDATE,
+)
+from django_bulk_hooks.conditions import (
+    ChangesTo,
+    HasChanged,
+    IsEqual,
+    IsNotEqual,
+    WasEqual,
+    safe_get_related_object,
+    safe_get_related_attr,
+    is_field_set,
+)
+from django_bulk_hooks.decorators import hook, select_related
+from django_bulk_hooks.handler import HookHandler
+from django_bulk_hooks.models import HookModelMixin
+from django_bulk_hooks.enums import Priority
+
+__all__ = [
+    "HookHandler",
+    "HookModelMixin",
+    "BEFORE_CREATE",
+    "AFTER_CREATE",
+    "BEFORE_UPDATE",
+    "AFTER_UPDATE",
+    "BEFORE_DELETE",
+    "AFTER_DELETE",
+    "VALIDATE_CREATE",
+    "VALIDATE_UPDATE",
+    "VALIDATE_DELETE",
+    "safe_get_related_object",
+    "safe_get_related_attr",
+    "is_field_set",
+    "Priority",
+    "hook",
+    "select_related",
+    "ChangesTo",
+    "HasChanged",
+    "IsEqual",
+    "IsNotEqual",
+    "WasEqual",
+]
