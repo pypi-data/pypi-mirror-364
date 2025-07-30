7f769c9ca2d7acbd9ce7f85bbfa51c142750248b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:11 +0000

    Add package google-ai-studio-utils/1.1.1/google_ai_studio_utils-1.1.1.tar.gz

---
+++ packages/google-ai-studio-utils/google_ai_studio_utils-1.1.1.tar.gz/google_ai_studio_utils-1.1.1/google_ai_studio_utils/config.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+parent_dir = Path(__file__).parent
+templates_dir = parent_dir / "templates"
+google_ai_studio_html_template = templates_dir / "google-ai-studio-conversation.html"
