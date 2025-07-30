95f9c3c044f8b8708fb068c898292795507435a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:30:26 +0000

    Add package genieclust/1.2.0/genieclust-1.2.0-cp314-cp314t-musllinux_1_2_x86_64.whl

---
+++ packages/genieclust/genieclust-1.2.0-cp314-cp314t-musllinux_1_2_x86_64.whl/genieclust/__init__.py
@@ -0,0 +1,34 @@
+"""
+genieclust Python Package
+"""
+
+
+# ############################################################################ #
+#                                                                              #
+#   Copyleft (C) 2020-2025, Marek Gagolewski <https://www.gagolewski.com>      #
+#                                                                              #
+#                                                                              #
+#   This program is free software: you can redistribute it and/or modify       #
+#   it under the terms of the GNU Affero General Public License                #
+#   Version 3, 19 November 2007, published by the Free Software Foundation.    #
+#   This program is distributed in the hope that it will be useful,            #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of             #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the               #
+#   GNU Affero General Public License Version 3 for more details.              #
+#   You should have received a copy of the License along with this program.    #
+#   If this is not the case, refer to <https://www.gnu.org/licenses/>.         #
+#                                                                              #
+# ############################################################################ #
+
+# version string, e.g., "1.0.0.9001" or "1.1.1"
+__version__ = "1.2.0"
+
+
+from . import plots
+from . import inequality
+from . import tools
+from . import compare_partitions
+from . import cluster_validity
+from . import internal
+from . import oldmst
+from .genie import MSTClusterMixin, Genie, GIc
