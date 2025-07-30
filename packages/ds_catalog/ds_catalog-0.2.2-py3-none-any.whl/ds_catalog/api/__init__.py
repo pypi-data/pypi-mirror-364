1676013a03bc8708b5417d2f20de5ca85b38e2a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:04 +0000

    Add package ds_catalog/0.2.2/ds_catalog-0.2.2-py3-none-any.whl

---
+++ packages/ds_catalog/ds_catalog-0.2.2-py3-none-any.whl/ds_catalog/api/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+
+# import apis into api package
+from ds_catalog.api.catalog_api import CatalogApi
+from ds_catalog.api.datasets_api import DatasetsApi
+from ds_catalog.api.mmio_api import MMIOApi
+from ds_catalog.api.sharing_api import SharingApi
+from ds_catalog.api.default_api import DefaultApi
+
