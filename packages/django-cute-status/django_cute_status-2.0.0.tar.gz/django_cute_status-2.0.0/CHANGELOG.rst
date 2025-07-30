428124ac8296a52dca0b89f88dccbc708d6de1a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:10 +0000

    Add package django-cute-status/2.0.0/django_cute_status-2.0.0.tar.gz

---
+++ packages/django-cute-status/django_cute_status-2.0.0.tar.gz/django_cute_status-2.0.0/CHANGELOG.rst
@@ -0,0 +1,21 @@
+2.0.0
+-----
+- `#24`_ - Add support for Django ``4.2`` - ``5.2``.
+    - Add support for Python ``3.10`` - ``3.13``.
+    - Remove support for Python ``3.8`` and ``3.9``.
+    - Remove support for Django ``3.2`` and ``4.0``.
+
+.. _#24: https://github.com/abhiabhi94/django-cute-status/pull/24
+
+1.1.0
+-----
+- `#4`_ - Change default URL for dog images for better experience.
+- `#9`_ - Add support for Django ``4.0`` and Python ``3.10``.
+
+.. _#4: https://github.com/abhiabhi94/django-cute-status/pull/4
+.. _#9: https://github.com/abhiabhi94/django-cute-status/pull/9
+
+
+1.0.0
+-----
+First release to PyPI.
---
+++ packages/django-cute-status/django_cute_status-2.0.0.tar.gz/django_cute_status-2.0.0/django_cute_status/conf/__init__.py
@@ -0,0 +1,32 @@
+import django
+from django.conf import settings as django_settings
+from django.utils.functional import LazyObject
+
+from django_cute_status.conf import defaults as app_settings
+
+
+class LazySettings(LazyObject):
+    def _setup(self):
+        self._wrapped = Settings(app_settings, django_settings)
+
+
+_django_version = django.VERSION
+
+DEPRECATED_SETTINGS = {
+    "DEFAULT_FILE_STORAGE" if (4, 2) <= _django_version < (5, 1) else None,
+    "STATICFILES_STORAGE" if (4, 2) <= _django_version < (5, 1) else None,
+    "USE_L10N" if (4, 0) <= _django_version < (5, 0) else None,
+}
+
+
+class Settings:
+    def __init__(self, *args):
+        [
+            setattr(self, attr, getattr(item, attr))
+            for item in args
+            for attr in dir(item)
+            if attr == attr.upper() and attr not in DEPRECATED_SETTINGS
+        ]
+
+
+settings = LazySettings()
