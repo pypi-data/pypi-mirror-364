26537d5d17b84ec3e6a04e25375210a9fb351a0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:18 +0000

    Add package pyswx/0.3.1/pyswx-0.3.1-py3-none-any.whl

---
+++ packages/pyswx/pyswx-0.3.1-py3-none-any.whl/pyswx/api/swconst/enumerations/sw_rebuild_on_activation_e.py
@@ -0,0 +1,14 @@
+"""
+swRebuildOnActivation_e Enumeration
+
+Reference:
+https://help.solidworks.com/2024/english/api/swconst/SOLIDWORKS.Interop.swconst~SOLIDWORKS.Interop.swconst.swRebuildOnActivation_e.html
+"""
+
+from enum import IntEnum
+
+
+class SWRebuildOnActivationOptionsE(IntEnum):
+    SW_USER_DECISION = 0  # Prompt the user whether to rebuild the activated document
+    SW_DONT_REBUILD_ACTIVE_DOC = 1  # Do not rebuild the activated document
+    SW_REBUILD_ACTIVE_DOC = 2  # Rebuild the activated document
