675dc4cc99231c89124e938cd886744f0213fd23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:35 +0000

    Add package HydrogenLib-Core/0.1.1/hydrogenlib_core-0.1.1.tar.gz

---
+++ packages/HydrogenLib-Core/hydrogenlib_core-0.1.1.tar.gz/hydrogenlib_core-0.1.1/src/_hycore/utils/lazy/lz_field.py
@@ -0,0 +1,31 @@
+from typing import Callable, Any
+
+from .lz_data import LazyData
+from ...better_descriptor import (Descriptor, DescriptorInstance)
+
+
+class LazyPropertyData(DescriptorInstance):
+    _lazydata = None
+
+    def __dspt_init__(self, inst, owner, name, dspt: 'LazyProperty'):
+        self._lazydata = LazyData(dspt.loader(), inst)
+
+    def __dspt_get__(self, instance, owner, parent: 'LazyProperty') -> Any:
+        self._lazydata.update_callable(parent.loader())
+        return self._lazydata.get(instance)  # 传递实例
+
+
+class LazyProperty(Descriptor):
+    def __init__(self, loader: Callable = None):
+        super().__init__()
+        self._loader = loader
+
+    def loader(self, loader: Callable = None):
+        if loader:
+            self._loader = loader
+            return self
+        else:
+            return self._loader
+
+    def __dspt_new__(self, inst) -> DescriptorInstance:
+        return LazyPropertyData()
