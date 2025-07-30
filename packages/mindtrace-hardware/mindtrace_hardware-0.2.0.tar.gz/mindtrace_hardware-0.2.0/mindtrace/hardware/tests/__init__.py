0d5e9bb7c5198597378244879ce2d4eab63f728b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:25 +0000

    Add package mindtrace-hardware/0.2.0/mindtrace_hardware-0.2.0.tar.gz

---
+++ packages/mindtrace-hardware/mindtrace_hardware-0.2.0.tar.gz/mindtrace_hardware-0.2.0/mindtrace/hardware/tests/__init__.py
@@ -0,0 +1,12 @@
+"""
+Hardware Unit Tests Package
+
+This package contains unit tests for the mindtrace hardware module.
+
+Test Structure:
+- unit/: Unit tests for individual components
+  - test_cameras.py: Camera backend and manager unit tests
+  - test_plcs.py: PLC backend and manager unit tests
+"""
+
+__all__ = []
