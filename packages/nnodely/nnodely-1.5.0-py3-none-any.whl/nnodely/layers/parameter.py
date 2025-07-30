972a1b8bd8216ab9b8a3f0648fd251b523ff0178
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:47 +0000

    Add package nnodely/1.5.0/nnodely-1.5.0-py3-none-any.whl

---
+++ packages/nnodely/nnodely-1.5.0-py3-none-any.whl/nnodely/layers/parameter.py
@@ -0,0 +1,245 @@
+import copy, inspect, textwrap
+import numpy as np
+
+from collections.abc import Callable
+
+from nnodely.basic.relation import NeuObj, Relation
+from nnodely.support.utils import check, enforce_types, NP_DTYPE
+
+
+def is_numpy_float(var):
+    return isinstance(var, (np.float16, np.float32, np.float64))
+
+class Constant(NeuObj, Relation):
+    """
+    Represents a constant value in the neural network model.
+
+    Parameters
+    ----------
+    name : str
+        The name of the constant.
+    values : list, float, int, or np.ndarray
+        The values of the constant.
+    tw : float or int, optional
+        The time window for the constant. Default is None.
+    sw : int, optional
+        The sample window for the constant. Default is None.
+
+    Attributes
+    ----------
+    name : str
+        The name of the constant.
+    dim : dict
+        A dictionary containing the dimensions of the constant.
+    json : dict
+        A dictionary containing the configuration of the constant.
+
+    Examples
+    --------
+    .. image:: https://colab.research.google.com/assets/colab-badge.svg
+        :target: https://colab.research.google.com/github/tonegas/nnodely/blob/main/examples/parameter.ipynb
+        :alt: Open in Colab
+
+    Example - passing a custom scalar value -> g.dim = {'dim': 1}:
+
+        >>> g = Constant('gravity',values=9.81)
+
+    Example - passing a custom vector value -> n.dim = {'dim': 4}:
+
+        >>> n = Constant('numbers', values=[1,2,3,4])
+
+    Example - passing a custom vector value with single sample window -> n.dim = {'dim': 4, 'sw': 1}:
+
+        >>> n = Constant('numbers', values=[[1,2,3,4]])
+
+    Example - passing a custom vector value with double sample window -> n.dim = {'dim': 4, 'sw': 2}:
+
+        >>> n = Constant('numbers', values=[[2,3,4],[1,2,3]])
+
+    Example - passing a custom vector value with double sample window -> n.dim = {'dim': 4, 'sw': 2}.
+    If the value of the sw is differnt from the dimension of shape[0] an error will be raised.
+
+        >>> n = Constant('numbers', sw = 2, values=[[2,3,4],[1,2,3]])
+
+    Example - passing a custom vector value with time window -> n.dim = {'dim': 4, 'tw': 4}.
+    In this case the samplingtime must be 0.5 otherwise an error will be raised. If the Constant have a time dimension,
+    the input must have a len(shape) == 2.
+
+        >>> n = Constant('numbers', tw = 4, values=[[2,3,4],[1,2,3]])
+    """
+    @enforce_types
+    def __init__(self, name:str,
+                 values:list|float|int|np.ndarray, *,
+                 tw:float|int|None = None,
+                 sw:int|None = None):
+
+        NeuObj.__init__(self, name)
+        values = np.array(values, dtype=NP_DTYPE)
+        shape = values.shape
+        values = values.tolist()
+
+        self.dim = {}
+        if tw is not None:
+            check(len(shape) >= 2, ValueError, "The dimension must be at least 2 if tw is set.")
+            check(sw is None, ValueError, "If tw is set sw must be None")
+            dimensions = shape[1] if len(shape[1:]) == 1 else list(shape[1:])
+            self.dim['tw'] = tw
+        elif sw is not None:
+            check(len(shape) >= 2, ValueError, "The dimension must be at least 2 if sw is set.")
+            self.dim['sw'] = sw
+            check(shape[0] == self.dim['sw'],ValueError, f"The sw = {sw} is different from sw = {shape[0]} of the values.")
+            dimensions = shape[1] if len(shape[1:]) == 1 else list(shape[1:])
+        else:
+            dimensions = 1 if len(shape[0:]) == 0 else shape[0] if len(shape[0:]) == 1 else list(shape[0:])
+
+        self.dim['dim'] = dimensions
+
+        # deepcopy dimention information inside Parameters
+        self.json['Constants'][self.name] = copy.deepcopy(self.dim)
+        if type(values) in (float,int):
+            self.json['Constants'][self.name]['values'] = [values]
+        else:
+            self.json['Constants'][self.name]['values'] = values
+
+class Parameter(NeuObj, Relation):
+    """
+    Represents a trainable parameter in the neural network model.
+
+    Notes
+    -----
+    .. note::
+        You can find some initialization functions for the 'init' and 'init_params' parameters inside the initializer module.
+
+    Parameters
+    ----------
+    name : str
+        The name of the parameter.
+    dimensions : int, list, tuple, or None, optional
+        The dimensions of the parameter. Default is None.
+    tw : float or int, optional
+        The time window for the parameter. Default is None.
+    sw : int, optional
+        The sample window for the parameter. Default is None.
+    values : list, float, int, np.ndarray, or None, optional
+        The values by which initialize the parameter. Default is None.
+    init : Callable, optional
+        A callable for initializing the parameter values. Default is None.
+    init_params : dict, optional
+        A dictionary of parameters for the initializer. Default is None.
+
+    Attributes
+    ----------
+    name : str
+        The name of the parameter.
+    dim : dict
+        A dictionary containing the dimensions of the parameter.
+    json : dict
+        A dictionary containing the configuration of the parameter.
+
+    Examples
+    --------
+    .. image:: https://colab.research.google.com/assets/colab-badge.svg
+        :target: https://colab.research.google.com/github/tonegas/nnodely/blob/main/examples/parameter.ipynb
+        :alt: Open in Colab
+
+    Example - basic usage:
+        >>> k = Parameter('k', dimensions=3, tw=4)
+
+    Example - initialize a parameter with values:
+        >>> x = Input('x')
+        >>> gravity = Parameter('g', dimensions=(4,1),values=[[[1],[2],[3],[4]]])
+        >>> out = Output('out', Linear(W=gravity)(x.sw(3)))
+
+    Example - initialize a parameter with a function:
+        >>> x = Input('x').last()
+        >>> p = Parameter('param', dimensions=1, sw=1, init=init_constant, init_params={'value':1})
+        >>> relation = Fir(parameter=param)(x)
+    """
+    @enforce_types
+    def __init__(self, name:str,
+                 dimensions:int|list|tuple|None = None, *,
+                 tw:float|int|None = None,
+                 sw:int|None = None,
+                 values:list|float|int|np.ndarray|None = None,
+                 init:Callable|str|None = None,
+                 init_params:dict|None = None):
+
+        NeuObj.__init__(self, name)
+        dimensions = list(dimensions) if type(dimensions) is tuple else dimensions
+        if values is None:
+            if dimensions is None:
+                dimensions = 1
+            self.dim = {'dim': dimensions}
+            if tw is not None:
+                check(sw is None, ValueError, "If tw is set sw must be None")
+                self.dim['tw'] = tw
+            elif sw is not None:
+                self.dim['sw'] = sw
+
+            # deepcopy dimention information inside Parameters
+            self.json['Parameters'][self.name] = copy.deepcopy(self.dim)
+        else:
+            values = np.array(values, dtype=NP_DTYPE)
+            shape = values.shape
+            values = values.tolist()
+
+            self.dim = {}
+            if tw is not None:
+                check(len(shape) >= 2, ValueError, "The dimension must be at least 2 if tw is set.")
+                check(sw is None, ValueError, "If tw is set sw must be None")
+                dimensions = shape[1] if len(shape[1:]) == 1 else list(shape[1:])
+                self.dim['tw'] = tw
+            elif sw is not None:
+                check(len(shape) >= 2, ValueError, "The dimension must be at least 2 if sw is set.")
+                self.dim['sw'] = sw
+                check(shape[0] == self.dim['sw'], ValueError,
+                      f"The sw = {sw} is different from sw = {shape[0]} of the values.")
+                dimensions = shape[1] if len(shape[1:]) == 1 else list(shape[1:])
+            else:
+                dimensions = 1 if len(shape[0:]) == 0 else shape[0] if len(shape[0:]) == 1 else list(shape[0:])
+
+            self.dim['dim'] = dimensions
+
+            # deepcopy dimention information inside Parameters
+            self.json['Parameters'][self.name] = copy.deepcopy(self.dim)
+            if type(values) in (int, float):
+                self.json['Parameters'][self.name]['init_values'] = [values]
+            else:
+                self.json['Parameters'][self.name]['init_values'] = values
+            self.json['Parameters'][self.name]['values'] = self.json['Parameters'][self.name]['init_values']
+
+        if init is not None:
+            check('values' not in self.json['Parameters'][self.name], ValueError, f"The parameter {self.name} is already initialized.")
+            #check(inspect.isfunction(init), ValueError,f"The init parameter must be a function.")
+            if inspect.isfunction(init):
+                code = textwrap.dedent(inspect.getsource(init)).replace('\"', '\'')
+                self.json['Parameters'][self.name]['init_fun'] = { 'code' : code, 'name' : init.__name__}
+            elif type(init) is str:
+                self.json['Parameters'][self.name]['init_fun'] = { 'name' : init}
+            if init_params is not None:
+                self.json['Parameters'][self.name]['init_fun']['params'] = init_params
+    
+class SampleTime():
+    """
+    Represents a constant value that is equal to the sample time.
+
+    Attributes
+    ----------
+    name : str
+        The name of the constant.
+    dim : dict
+        A dictionary containing the dimensions of the constant.
+    json : dict
+        A dictionary containing the configuration of the constant.
+
+    Example
+    -------
+        >>> dt = SampleTime()
+    """
+    name = 'SampleTime'
+    g = Constant(name, values=0)
+    def __new__(cls):
+        SampleTime.g.dim = {'dim': 1}
+        SampleTime.g.json['Constants'][SampleTime.name] = copy.deepcopy(SampleTime.g.dim)
+        SampleTime.g.json['Constants'][SampleTime.name]['values'] = SampleTime.name
+        return SampleTime.g
---
+++ packages/nnodely/nnodely-1.5.0-py3-none-any.whl/nnodely/operators/validator.py
@@ -0,0 +1,176 @@
+import torch, warnings
+import numpy as np
+
+from nnodely.support.utils import ReadOnlyDict, get_batch_size
+
+from nnodely.basic.loss import CustomLoss
+from nnodely.operators.network import Network
+from nnodely.support.utils import  check, TORCH_DTYPE, enforce_types
+
+class Validator(Network):
+    @enforce_types
+    def __init__(self):
+        check(type(self) is not Validator, TypeError, "Validator class cannot be instantiated directly")
+        super().__init__()
+
+        # Validation Parameters
+        self.__performance = {}
+        self.__prediction = {}
+
+    @property
+    def performance(self):
+        return ReadOnlyDict(self.__performance)
+
+    @property
+    def prediction(self):
+        return ReadOnlyDict(self.__prediction)
+    
+    @enforce_types
+    def resultAnalysis(self,
+                       dataset: str | list | dict, *,
+                       name: str | None = None,
+                       minimize_gain: dict = {},
+                       closed_loop: dict = {},
+                       connect: dict = {},
+                       prediction_samples: int | str = -1, #TODO uniform to training set to 0
+                       step: int = 0,
+                       batch_size: int | None = None
+                       ) -> None:
+        """
+        The function is used to analyze the performance of the model on the provided dataset.
+
+        Parameters
+        ----------
+        dataset : str | list | dict
+            Dataset to analyze the performance of the model on.
+        name : str or None
+            Label to be used in the plots
+        minimize_gain : dict
+            A dictionary specifying the gain for each minimization loss function.
+        closed_loop : dict or None, optional
+            A dictionary specifying closed loop connections. The keys are input names and the values are output names. Default is None.
+        connect : dict or None, optional
+            A dictionary specifying connections. The keys are input names and the values are output names. Default is None.
+        step : int or None, optional
+            The step size to analyze the model on the provided dataset. A big value will result in less data used for each epochs and a faster train. Default is None.
+        prediction_samples : int or None, optional
+            The size of the prediction horizon. Number of samples at each recurrent window Default is None.
+        batch_size :
+            The batch size use for analyse the performance of the model on the provided dataset.
+
+
+        """
+
+        with torch.enable_grad() if self._get_gradient_on_inference() else torch.inference_mode():
+            ## Init model for retults analysis
+            if name is None:
+                dataset_tag = self._get_tag(dataset)
+            else:
+                dataset_tag = name
+
+            self._model.eval()
+            self.__performance[dataset_tag] = {}
+            self.__prediction[dataset_tag] = {}
+            A = {}
+            B = {}
+            total_losses = {}
+
+            # Create the losses
+            losses = {}
+            for name, values in self._model_def['Minimizers'].items():
+                losses[name] = CustomLoss(values['loss'])
+
+            data = self._get_data(dataset)
+            n_samples = len(data[list(data.keys())[0]])
+
+            batch_size = get_batch_size(n_samples, batch_size, prediction_samples)
+            prediction_samples = self._setup_recurrent_variables(prediction_samples, closed_loop, connect)
+
+            if prediction_samples >= 0:
+                mandatory_inputs, non_mandatory_inputs = self._get_mandatory_inputs(connect,closed_loop)
+
+                for key, value in self._model_def['Minimizers'].items():
+                    total_losses[key], A[key], B[key] = [], [], []
+                    for horizon_idx in range(prediction_samples + 1):
+                        A[key].append([])
+                        B[key].append([])
+                if type(dataset) is not dict and dataset in self._multifile.keys(): ## Multi-file Dataset
+                    batch_indexes = self._get_batch_indexes(dataset, prediction_samples)
+                else:
+                    batch_indexes = list(range(n_samples - prediction_samples))
+
+                ## Update with virtual states
+                self._model.update(closed_loop=closed_loop, connect=connect)
+                self._recurrent_inference(data, batch_indexes, batch_size, minimize_gain, prediction_samples,
+                                          step, non_mandatory_inputs, mandatory_inputs, losses,
+                                          total_losses = total_losses, A = A, B = B)
+
+                for key, value in self._model_def['Minimizers'].items():
+                    for horizon_idx in range(prediction_samples + 1):
+                        if A is not None:
+                            A[key][horizon_idx] = np.concatenate(A[key][horizon_idx])
+                        if B is not None:
+                            B[key][horizon_idx] = np.concatenate(B[key][horizon_idx])
+                    if total_losses is not None:
+                        total_losses[key] = np.mean(total_losses[key])
+            else:
+                for key, value in self._model_def['Minimizers'].items():
+                    total_losses[key], A[key], B[key] = [], [], []
+
+                self._model.update(disconnect=True)
+                self._inference(data, n_samples, batch_size, minimize_gain, losses,
+                                total_losses = total_losses, A = A, B = B)
+
+                for key, value in self._model_def['Minimizers'].items():
+                    A[key] = np.concatenate(A[key])
+                    B[key] = np.concatenate(B[key])
+                    total_losses[key] = np.mean(total_losses[key])
+
+            for ind, (key, value) in enumerate(self._model_def['Minimizers'].items()):
+                A_np = np.array(A[key])
+                B_np = np.array(B[key])
+                self.__performance[dataset_tag][key] = {}
+                self.__performance[dataset_tag][key][value['loss']] = np.mean(total_losses[key]).item()
+                self.__performance[dataset_tag][key]['fvu'] = {}
+                # Compute FVU
+                residual = A_np - B_np
+                error_var = np.var(residual)
+                error_mean = np.mean(residual)
+                #error_var_manual = np.sum((residual-error_mean) ** 2) / (len(self.__prediction['B'][ind]) - 0)
+                #print(f"{key} var np:{new_error_var} and var manual:{error_var_manual}")
+                with warnings.catch_warnings(record=True) as w:
+                    self.__performance[dataset_tag][key]['fvu']['A'] = (error_var / np.var(A_np)).item()
+                    self.__performance[dataset_tag][key]['fvu']['B'] = (error_var / np.var(B_np)).item()
+                    if w and np.var(A_np) == 0.0 and  np.var(B_np) == 0.0:
+                        self.__performance[dataset_tag][key]['fvu']['A'] = np.nan
+                        self.__performance[dataset_tag][key]['fvu']['B'] = np.nan
+                self.__performance[dataset_tag][key]['fvu']['total'] = np.mean([self.__performance[dataset_tag][key]['fvu']['A'],self.__performance[dataset_tag][key]['fvu']['B']]).item()
+                # Compute AIC
+                #normal_dist = norm(0, error_var ** 0.5)
+                #probability_of_residual = normal_dist.pdf(residual)
+                #log_likelihood_first = sum(np.log(probability_of_residual))
+                p1 = -len(residual)/2.0*np.log(2*np.pi)
+                with warnings.catch_warnings(record=True) as w:
+                    p2 = -len(residual)/2.0*np.log(error_var)
+                    p3 = -1 / (2.0 * error_var) * np.sum(residual ** 2)
+                    if w and p2 == np.float32(np.inf) and p3 == np.float32(-np.inf):
+                        p2 = p3 = 0.0
+                log_likelihood = p1+p2+p3
+                #print(f"{key} log likelihood second mode:{log_likelihood} = {p1}+{p2}+{p3} first mode: {log_likelihood_first}")
+                total_params = sum(p.numel() for p in self._model.parameters() if p.requires_grad)
+                #print(f"{key} total_params:{total_params}")
+                aic = - 2 * log_likelihood + 2 * total_params
+                #print(f"{key} aic:{aic}")
+                self.__performance[dataset_tag][key]['aic'] = {'value':aic,'total_params':total_params,'log_likelihood':log_likelihood}
+                # Prediction and target
+                self.__prediction[dataset_tag][key] = {}
+                self.__prediction[dataset_tag][key]['A'] = A_np.tolist()
+                self.__prediction[dataset_tag][key]['B'] = B_np.tolist()
+
+            self.__performance[dataset_tag]['total'] = {}
+            self.__performance[dataset_tag]['total']['mean_error'] = np.mean([value for key,value in total_losses.items()])
+            self.__performance[dataset_tag]['total']['fvu'] = np.mean([self.__performance[dataset_tag][key]['fvu']['total'] for key in self._model_def['Minimizers'].keys()])
+            self.__performance[dataset_tag]['total']['aic'] = np.mean([self.__performance[dataset_tag][key]['aic']['value']for key in self._model_def['Minimizers'].keys()])
+
+        self.visualizer.showResult(dataset_tag)
+#227
