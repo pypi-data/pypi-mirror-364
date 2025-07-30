b510ba027407cf425306a2d25f3d0e6fd416f55c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:10:02 +0000

    Add package clij2-fft/0.29/clij2_fft-0.29-py3-none-any.whl

---
+++ packages/clij2-fft/clij2_fft-0.29-py3-none-any.whl/clij2fft/libs.py
@@ -0,0 +1,39 @@
+from ctypes import *
+import numpy as np
+import numpy.ctypeslib as npct
+import os
+import sys
+
+def getlib():
+    """
+    
+    returns the cdll that contains the clij2 functions and sets up the function argument types using ctypes
+
+    the code is inspired by this example https://github.com/koschink/PyYacuDecu
+
+    note the clij2fft library must be on the system path for the CDLL to be set up properly
+
+    Returns:
+        [cdll]: cdll containing clij2 functions
+    """
+
+    # here we load the clij2fft library.  The library is 
+    # expected to be somewhere on the library path of the 
+    # OS and application.   
+    if os.name=='posix' and sys.platform!='darwin':
+        clij2fft=CDLL('libclij2fft.so', mode=RTLD_GLOBAL)
+    elif os.name=='posix' and sys.platform =='darwin':
+        clij2fft=CDLL('libclij2fft.dylib', mode=RTLD_GLOBAL)
+    # if not posix assume windows
+    else:
+        clij2fft=CDLL('clij2fft.dll', mode=RTLD_GLOBAL)
+    
+    array_3d_float = npct.ndpointer(dtype=np.float32, ndim=3 , flags='CONTIGUOUS')
+
+    clij2fft.deconv3d_32f.argtypes = [c_int, c_int, c_int, c_int, array_3d_float, array_3d_float, array_3d_float, array_3d_float, c_int, c_int]
+    clij2fft.deconv3d_32f_tv.argtypes = [c_int, c_float, c_int, c_int, c_int, array_3d_float, array_3d_float, array_3d_float, array_3d_float, c_int, c_int]
+    clij2fft.convcorr3d_32f.argtypes = [c_int, c_int, c_int, array_3d_float, array_3d_float, array_3d_float, c_bool, c_int, c_int]
+
+    return clij2fft
+
+
---
+++ packages/clij2-fft/clij2_fft-0.29-py3-none-any.whl/clij2fft/richardson_lucy.py
@@ -0,0 +1,228 @@
+from clij2fft.libs import getlib
+import numpy as np
+from clij2fft.pad import pad, get_pad_size, get_next_smooth, unpad
+
+def richardson_lucy(img, psf, numiterations, regularizationfactor=0, first_guess=None, lib=None, platform=0, device=0):
+    """ perform Richardson-Lucy on img using psf.  The image is extended to the next smooth size 
+    because clfft only works on smooth sizes.  If additional extension is desired the image should 
+    be extended before calling this function.  
+
+    Args:
+        img ([type]): image to be deconvolved 
+        psf ([type]): point spread function 
+        numiterations ([type]): [description]
+        regularizationfactor ([type]): used for total varation noise regularization
+        lib ([type], optional): pass in if clfft lib is already initialized
+
+    Returns:
+        [type]: deconvolved image
+    """
+
+    # native code only works with 32 bit floats
+    img=img.astype(np.float32)
+    psf=psf.astype(np.float32)
+
+    original_size = img.shape
+
+    padded_size=[max(x,y) for x,y in zip(img.shape, psf.shape)]
+    
+    # native code only works with 7-smooth sizes
+    extended_size = get_next_smooth(padded_size)
+
+    # pad image and psf to next smooth size
+    img, _ = pad(img, extended_size,'reflect')
+    psf, _ = pad(psf, extended_size, 'constant')    
+
+    # shift psf so center is at 0,0
+    shifted_psf = np.fft.ifftshift(psf)
+    
+    # normalization factor for edge smoothing (all ones means no edge normalization)
+    normal=np.ones(img.shape).astype(np.float32)
+    
+    # memory for result
+    if (first_guess is None):
+        result = np.copy(img)
+    else:
+        print('copy first guess')
+        result = np.copy(normal)
+
+    # if the lib wasn't passed get it
+    if (lib==None):
+        print('get lib')
+        lib = getlib()
+        cleanup = True
+    else:
+        cleanup = False
+
+    # deconvolution using clij2fft
+    if regularizationfactor==0:
+        lib.deconv3d_32f(numiterations, int(img.shape[2]), int(img.shape[1]), int(img.shape[0]), img, shifted_psf, result, normal, platform, device)
+    else:
+        lib.deconv3d_32f_tv(numiterations, regularizationfactor, int(img.shape[2]), int(img.shape[1]), int(img.shape[0]), img, shifted_psf, result, normal, platform, device)
+
+    if cleanup==True:
+        lib.cleanup()
+
+    # unpad and return
+    return unpad(result, original_size)
+
+def richardson_lucy_nc(img, psf, numiterations, regularizationfactor=0, lib=None, platform=0, device=0):
+    """ perform non-circulant Richardson-Lucy on img using psf.  The image is extended to the size of
+    image+psf in each dimension then to the next smooth size because clfft only works on smooth sizes.
+    A non-circulant normalization factor is computed, as part of the Boundary condition handling scheme 
+    described here
+	http://bigwww.epfl.ch/deconvolution/challenge2013/index.html?p=doc_math_rl)
+
+    Args:
+        img ([type]): image to be deconvolved 
+        psf ([type]): point spread function 
+        numiterations ([type]): [description]
+        regularizationfactor ([type]): used for total varation noise regularization
+        lib ([type], optional): pass in if clfft lib is already initialized
+
+    Returns:
+        [type]: deconvolved image
+    """
+
+    # the native code only works with 32 bit floats
+    img=img.astype(np.float32)
+    psf=psf.astype(np.float32)
+
+    psf=psf/psf.sum()
+
+    # keep track of the original size
+    original_size = img.shape
+
+    # compute the extended size    
+    extended_size = [img.shape[0]+2*int(psf.shape[0]/2), img.shape[1]+2*int(psf.shape[1]/2), img.shape[2]+2*int(psf.shape[2]/2)] 
+    
+    # the native code also only works with 7-smooth sizes so extend further to the next smooth size
+    extended_size = get_next_smooth(extended_size)
+
+    # pad image and psf to the extended size computed above
+    img, padding = pad(img, extended_size,'constant')
+    psf, _ = pad(psf, extended_size, 'constant')    
+
+    # shift psf so center is at 0,0
+    shifted_psf = np.fft.ifftshift(psf)
+
+    # create result with initial guess as a constant with constant = to image mean
+    result = np.zeros(img.shape).astype('float32')
+    result[:,:,:]=img.mean()
+
+    # create normalization factor for non-circulant deconvolution
+    
+    start = [0,0,0]
+    end = [0,0,0]
+
+	# calculate the start and end of the original image space within the extended image space
+    # we call this the valid region 
+    for d in range(3):
+        start[d] = padding[d][0]#int((extended_size[d]-original_size[d])/2)
+        end[d]=int(start[d]+original_size[d])
+
+    valid=np.zeros(extended_size).astype(np.float32)
+    valid[start[0]:end[0],start[1]:end[1],start[2]:end[2]]=1
+
+    normal=np.zeros(extended_size).astype(np.float32)
+    # if the lib wasn't passed get it
+    if (lib==None):
+        print('get lib')
+        lib = getlib()
+        cleanup = True
+    else:
+        cleanup = False
+
+    print('calling convcorr',platform, device)
+    # the normalization factor is the valid region correlated with the PSF
+    lib.convcorr3d_32f(int(normal.shape[2]), int(normal.shape[1]), int(normal.shape[0]), valid, shifted_psf, normal,1,platform,device)
+
+    # get rid of any zeros in the normal to avoid divide by zero issues
+    # TODO: clarify why this changed
+    #normal[normal<0.00001]=1
+
+    # deconvolution using clij2fft
+    if regularizationfactor==0:
+        lib.deconv3d_32f(numiterations, int(img.shape[2]), int(img.shape[1]), int(img.shape[0]), img, shifted_psf, result, normal, platform, device)
+    else:
+        lib.deconv3d_32f_tv(numiterations, regularizationfactor, int(img.shape[2]), int(img.shape[1]), int(img.shape[0]), img, shifted_psf, result, normal, platform, device)
+
+    if cleanup==True:
+        lib.cleanup()
+
+    # unpad and return
+    return unpad(result, original_size)
+
+
+def richardson_lucy_interpolate(img, psf, numiterations, regularizationfactor=0, valid=None, firstguess=None, lib=None, platform=0, device=0):
+    """ perform Richardson-Lucy interpolated deconvolution on img using psf. The user passes in a map of valid pixels and deconvolution
+    is set up so non-valid values are 'interpolated'.  Non-valid values can be saturated areas, or areas between voxels (as to achieve sub-voxel resolution))
+    Note: the interpolated values may not be representative of true structure.  The quality of results is signal dependent. 
+
+    Note 2:  The algorithm is a general version of the non-circulant deconvolution algorithm implemented above.  If no valid is passed in, the algorithm
+    will behave exactly as the non-circulant algorithm above.  If valid is passed in, the algorithm will interpolate values in the non-valid region.  
+
+    Args:
+        img (numpy array): image to be deconvolved 
+        psf (numpy array): point spread function 
+        numiterations (int): [description]
+        regularizationfactor (float): used for total varation noise regularization
+        valid (numpy array, optional): [description]. Defaults to None. Map of valid pixels in the image
+        firstguess (numpy array, optional): [description]. Defaults to None. First guess for deconvolution
+        lib (numpy array, optional): pass in if clfft lib is already initialized
+
+    Returns:
+        [numpy array]: deconvolved image
+    """
+
+    # native code only works with 32 bit floats
+    img=img.astype(np.float32)
+    psf=psf.astype(np.float32)
+
+    original_size = img.shape
+
+    # if no valid map passed in all pixels are valid 
+    if valid is None:
+        valid=np.ones(img.shape).astype(np.float32)
+    
+    # compute the extended size    
+    extended_size = [img.shape[0]+2*int(psf.shape[0]/2), img.shape[1]+2*int(psf.shape[1]/2), img.shape[2]+2*int(psf.shape[2]/2)] 
+
+    # the native code also only works with 7-smooth sizes so extend further to the next smooth size
+    extended_size = get_next_smooth(extended_size)
+    
+    # pad image and psf and valid map to next smooth size
+    img, _ = pad(img, extended_size,'constant')
+    psf, _ = pad(psf, extended_size, 'constant')    
+    valid, _ = pad(valid, extended_size, 'constant')    
+
+    # shift psf so center is at 0,0
+    shifted_psf = np.fft.ifftshift(psf)
+    
+    # create result array which will be initialized to the first guess if passed in, otherwise to the mean of the image 
+    result = np.zeros(img.shape).astype('float32')
+
+    if firstguess is None:
+        result[:,:,:]=2*img.mean()
+    else:
+        result, _ = pad(firstguess, extended_size,'constant', constant_values=firstguess.mean())
+   
+    # if the lib wasn't passed get it
+    if (lib==None):
+        print('get lib')
+        lib = getlib()
+
+    normal=np.zeros(extended_size).astype(np.float32)
+    
+    # the normalization factor is the valid region correlated with the PSF
+    lib.convcorr3d_32f(int(normal.shape[2]), int(normal.shape[1]), int(normal.shape[0]), valid, shifted_psf, normal,1)
+    
+    # deconvolution using clij2fft
+    if regularizationfactor==0:
+        lib.deconv3d_32f(numiterations, int(img.shape[2]), int(img.shape[1]), int(img.shape[0]), img, shifted_psf, result, normal, platform, device)
+    else:
+        lib.deconv3d_32f_tv(numiterations, regularizationfactor, int(img.shape[2]), int(img.shape[1]), int(img.shape[0]), img, shifted_psf, result, normal, platform, device)
+    
+    # unpad and return
+    return unpad(result, original_size), normal, valid
+
