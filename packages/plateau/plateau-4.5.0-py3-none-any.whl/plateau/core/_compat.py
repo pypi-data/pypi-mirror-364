09a8886a1b2e64b01519eeb62819606739f8f8c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:40 +0000

    Add package plateau/4.5.0/plateau-4.5.0-py3-none-any.whl

---
+++ packages/plateau/plateau-4.5.0-py3-none-any.whl/plateau/core/_compat.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import simplejson
+from packaging.version import parse as parse_version
+
+PANDAS_3 = parse_version(pd.__version__).major >= 3
+
+
+def pandas_infer_string():
+    return (
+        pd.get_option("future.infer_string") or parse_version(pd.__version__).major >= 3
+    )
+
+
+def load_json(buf, **kwargs):
+    """Compatibility function to load JSON from str/bytes/unicode.
+
+    For Python 2.7 json.loads accepts str and unicode. Python 3.4 only
+    accepts str whereas 3.5+ accept bytes and str.
+    """
+    if isinstance(buf, bytes):
+        return simplejson.loads(buf.decode("utf-8"), **kwargs)
+    else:
+        return simplejson.loads(buf, **kwargs)
