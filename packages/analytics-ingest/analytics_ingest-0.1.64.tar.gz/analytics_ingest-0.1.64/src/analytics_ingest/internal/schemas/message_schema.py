f79ba462ac39ba40dac0dd6cecf2f5ef1c1f9983
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:07:11 +0000

    Add package analytics-ingest/0.1.64/analytics_ingest-0.1.64.tar.gz

---
+++ packages/analytics-ingest/analytics_ingest-0.1.64.tar.gz/analytics_ingest-0.1.64/src/analytics_ingest/internal/schemas/message_schema.py
@@ -0,0 +1,37 @@
+from typing import Optional
+
+from pydantic import BaseModel
+from datetime import datetime
+from analytics_ingest.internal.utils.time_utils import parse_time_val
+
+
+class MessageSchema(BaseModel):
+    arbId: Optional[str] = ""
+    name: str
+    networkName: str = ""
+    ecuName: Optional[str] = ""
+    ecuId: Optional[str] = ""
+    fileId: Optional[str] = ""
+    messageDate: Optional[str] = ""
+    requestCode: Optional[str] = ""
+
+    @classmethod
+    def from_variables(cls, variables):
+        name = variables.get("messageName") or variables["name"]
+        message_date = variables.get("messageDate", "")
+        if isinstance(message_date, float) or isinstance(message_date, int):
+            message_date = parse_time_val(message_date) + "Z"
+
+        return cls(
+            arbId=variables.get("arbId", ""),
+            name=name,
+            networkName=variables.get("networkName", ""),
+            ecuName=variables.get("ecuName", ""),
+            ecuId=variables.get("ecuId", ""),
+            fileId=variables.get("fileId", ""),
+            messageDate=message_date,
+            requestCode=variables.get("requestCode", ""),
+        )
+
+    def cache_key(self) -> str:
+        return f"{self.name}|{self.networkName}|{self.ecuName}|{self.arbId}"
