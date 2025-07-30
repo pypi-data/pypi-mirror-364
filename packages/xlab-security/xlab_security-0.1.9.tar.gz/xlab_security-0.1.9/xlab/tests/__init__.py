0fa5c94ca075c70d2b2f5edb0315dc97a8940daa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:07:53 +0000

    Add package xlab-security/0.1.9/xlab_security-0.1.9.tar.gz

---
+++ packages/xlab-security/xlab_security-0.1.9.tar.gz/xlab_security-0.1.9/xlab/tests/__init__.py
@@ -0,0 +1,5 @@
+"""
+This module makes the test sections available under the `xlab.tests` namespace.
+"""
+
+from . import section1_0, pgd, section2_1_2, section2_3, robustbench, gcg, ensemble, distillation, cw
