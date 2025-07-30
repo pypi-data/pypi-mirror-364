2d156a88f362683552afa6258a757fa5308a4251
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:41 +0000

    Add package mp-scrape-cli/0.1.7/mp_scrape_cli-0.1.7.tar.gz

---
+++ packages/mp-scrape-cli/mp_scrape_cli-0.1.7.tar.gz/mp_scrape_cli-0.1.7/mp_scrape_cli/__main__.py
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: 2025 Free Software Foundation Europe e.V. <mp-explore@fsfe.org>
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+if __name__ == "__main__":
+    print("╔════════════════════════════════════════════════════════╗")
+    print("║        MP Scrape has been renamed to MP Explore        ║")
+    print("║  Replace all references from mp-scrape* to mp-explore* ║")
+    print("║            Thank you for your understanding            ║")
+    print("║                                                        ║")
+    print("║ MP Explore Team                                        ║")
+    print("║ https://git.fsfe.org/mp-explore                        ║")
+    print("║ contact@fsfe.org                                       ║")
+    print("╚════════════════════════════════════════════════════════╝")
