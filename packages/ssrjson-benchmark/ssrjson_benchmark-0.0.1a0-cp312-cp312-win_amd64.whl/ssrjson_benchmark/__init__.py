e959f4d9c907ae35e29aa61bbb8d0d94437183e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:11 +0000

    Add package ssrjson-benchmark/0.0.1a0/ssrjson_benchmark-0.0.1a0-cp312-cp312-win_amd64.whl

---
+++ packages/ssrjson-benchmark/ssrjson_benchmark-0.0.1a0-cp312-cp312-win_amd64.whl/ssrjson_benchmark/__init__.py
@@ -0,0 +1,13 @@
+from .benchmark_main import (
+    run_benchmark,
+    generate_report_markdown,
+    generate_report,
+    run_benchmark_default,
+)
+
+__all__ = [
+    "run_benchmark",
+    "generate_report_markdown",
+    "generate_report",
+    "run_benchmark_default",
+]
