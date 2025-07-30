b14102446d0fc85fb3e7acbf79c3701526b6a7f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:45 +0000

    Add package ipulse-shared-core-ftredge/25.1.1/ipulse_shared_core_ftredge-25.1.1-py3-none-any.whl

---
+++ packages/ipulse-shared-core-ftredge/ipulse_shared_core_ftredge-25.1.1-py3-none-any.whl/ipulse_shared_core_ftredge/models/__init__.py
@@ -0,0 +1,4 @@
+from .base_data_model import BaseDataModel
+from .base_api_response import BaseAPIResponse , CustomJSONResponse, CreditChargeableAPIResponse, UserCreditBalance, UpdatedUserCreditInfo
+from .user import UserProfile, UserSubscription, UserStatus, UserAuth, UserAuthCreateNew, UserPermission
+from .catalog import SubscriptionPlan, ProrationMethod, PlanUpgradePath, UserType
