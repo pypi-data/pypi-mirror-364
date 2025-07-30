d72d7c2c4d5e9d306b428ac1b2658423256d7f0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:27 +0000

    Add package pysvc2/2.0.2/pysvc2-2.0.2.tar.gz

---
+++ packages/pysvc2/pysvc2-2.0.2.tar.gz/pysvc2-2.0.2/setup.py
@@ -0,0 +1,49 @@
+##############################################################################
+# Copyright 2025 IBM Corp.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##############################################################################
+
+import pysvc
+from setuptools import setup, find_packages
+
+install_requires = ['munch', 'paramiko']
+
+setup(
+    name='pysvc2',
+    version=pysvc.version,
+    description="IBM Spectrum Virtualize Python Client",
+    long_description="CLI Python Client for IBM Spectrum "
+                     "Virtualize Family Storage.",
+    author="CSI Block",
+    author_email="CSI.Block1@il.ibm.com",
+    maintainer="CSI Block",
+    keywords=["IBM", "Spectrum Virtualize Family Storage"],
+    requires=install_requires,
+    install_requires=install_requires,
+    tests_require=['nose', 'mock'],
+    license="Apache License, Version 2.0",
+    include_package_data=True,
+    packages=find_packages(),
+    provides=['pysvc'],
+    url="https://github.com/IBM/pysvc",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Environment :: Console',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ])
