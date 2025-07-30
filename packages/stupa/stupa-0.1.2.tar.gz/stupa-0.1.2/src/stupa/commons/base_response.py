3c6bf091108e3b102ae5948758539ce4478238f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:53 +0000

    Add package stupa/0.1.2/stupa-0.1.2.tar.gz

---
+++ packages/stupa/stupa-0.1.2.tar.gz/stupa-0.1.2/src/stupa/commons/base_response.py
@@ -0,0 +1,27 @@
+from django.http import JsonResponse
+from rest_framework import serializers
+from typing import Union
+
+
+from .paginator import PaginationData
+from stupa.exceptions import ResponseEnum
+
+
+class SkillsPeResonse:
+    def __new__(self,
+                response_type: ResponseEnum,
+                data: Union[dict, serializers.Serializer,
+                            serializers.ModelSerializer] = None,
+                pagination_data: PaginationData = None,
+                ) -> JsonResponse:
+
+        response_data = {
+            "message": response_type.message,
+            "response_code": response_type.response_code
+        }
+        if data:
+            response_data["data"] = data.data if hasattr(
+                data, 'data') else data
+        if pagination_data:
+            response_data["pagination"] = pagination_data
+        return JsonResponse(response_data, status=response_type.status_code)
