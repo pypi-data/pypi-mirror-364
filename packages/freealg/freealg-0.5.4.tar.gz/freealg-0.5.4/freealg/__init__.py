a16a52850d234e2a514d27528700eb8a43a1e8da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:29 +0000

    Add package freealg/0.5.4/freealg-0.5.4.tar.gz

---
+++ packages/freealg/freealg-0.5.4.tar.gz/freealg-0.5.4/freealg/__init__.py
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: Copyright 2025, Siavash Ameli <sameli@berkeley.edu>
+# SPDX-License-Identifier: BSD-3-Clause
+# SPDX-FileType: SOURCE
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the license found in the LICENSE.txt file in the root
+# directory of this source tree.
+
+from .freeform import FreeForm
+from ._linalg import eigvalsh, cond, norm, trace, slogdet
+from . import distributions
+
+__all__ = ['FreeForm', 'distributions', 'eigvalsh', 'cond', 'norm', 'trace',
+           'slogdet']
+
+from .__version__ import __version__                          # noqa: F401 E402
