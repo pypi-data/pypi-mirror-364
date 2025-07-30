27cc3b44f9d2c4d37bd736c36a492f04bea12180
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:06:27 +0000

    Add package python-ldap-faker/1.3.3/python_ldap_faker-1.3.3.tar.gz

---
+++ packages/python-ldap-faker/python_ldap_faker-1.3.3.tar.gz/python_ldap_faker-1.3.3/ldap_faker/__init__.py
@@ -0,0 +1,24 @@
+__version__ = "1.3.3"
+
+from .db import (
+    CallHistory,
+    LDAPCallRecord,
+    LDAPServerFactory,
+    ObjectStore,
+    OptionStore,
+)
+from .faker import (
+    FakeLDAP,
+    FakeLDAPObject,
+)
+from .hooks import (
+    Hook,
+    HookDefinition,
+    HookRegistry,
+    hooks,
+)
+from .types import LDAPData, LDAPOptionValue, LDAPRecord, LDAPSearchResult
+from .unittest import LDAPFakerMixin
+
+# Import our hooks
+import ldap_faker.servers.server_389
