f26e936f1248d10e937755d0af9af119a68c46ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:41 +0000

    Add package sierra-dev/0.1.3/sierra_dev-0.1.3.tar.gz

---
+++ packages/sierra-dev/sierra_dev-0.1.3.tar.gz/sierra_dev-0.1.3/sierra/_about.py
@@ -0,0 +1,15 @@
+import typing
+
+__author__: typing.Final = "Xsyncio"
+__version__: typing.Final = "0.1.3"
+__license__: typing.Final = "MIT"
+__github__: typing.Final = "https://github.com/xsyncio/sierra-dev"
+__description__: typing.Final = "Sierra  is a framework for building and managing invoker scripts that can be used across different nodes in Sierra during any investigation."
+__url__: typing.Final = "https://xsyncio.github.io/sierra-dev"
+__long_description__: typing.Final = (
+    __description__
+    + """
+Sierra is designed to be as lightweight as possible and is written in pure Python.
+It is intended to be used with the Sierra platform.
+"""
+)
---
+++ packages/sierra-dev/sierra_dev-0.1.3.tar.gz/sierra_dev-0.1.3/sierra/abc/base.py
@@ -0,0 +1,9 @@
+import typing
+
+
+class SierraABC(typing.TypedDict, total=False):
+    """
+    Base class for all Sierra ABCs.
+
+    This class is used to define the structure of Sierra ABCs.
+    """
