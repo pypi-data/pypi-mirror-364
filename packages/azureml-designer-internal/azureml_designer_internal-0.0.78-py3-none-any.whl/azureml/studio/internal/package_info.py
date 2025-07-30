c6bc2564572766d1bb661e14961930279b3eb418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:34 +0000

    Add package azureml-designer-internal/0.0.78/azureml_designer_internal-0.0.78-py3-none-any.whl

---
+++ packages/azureml-designer-internal/azureml_designer_internal-0.0.78-py3-none-any.whl/azureml/studio/internal/package_info.py
@@ -0,0 +1,4 @@
+VERSION = '0.0.78'
+PACKAGE_NAME = 'azureml-designer-internal'
+DESCRIPTION = 'Internal functionalities provided for Azure Machine Learning designer built-in modules. ' \
+              'Not intended for public use.'
