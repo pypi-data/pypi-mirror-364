57283d852e6e89dd859f9838775f7b3580dc23bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:48 +0000

    Add package rpa-sap-mock-bk/0.1.3/rpa_sap_mock_bk-0.1.3.tar.gz

---
+++ packages/rpa-sap-mock-bk/rpa_sap_mock_bk-0.1.3.tar.gz/rpa_sap_mock_bk-0.1.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rpa-sap-mock-bk',
+    version='0.1.3',
+    packages=find_packages(),
+    install_requires=[
+        'robotframework==6.1.1',
+        'requests',
+        'robotframework-pythonlibcore'
+    ],
+    author='Minh Chiến',
+    description='Robot Framework library for SAP Cloud SDK APIs',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/chiencse/rpa-sap-mock',
+    classifiers=[
+        'Framework :: Robot Framework',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ]
+)
