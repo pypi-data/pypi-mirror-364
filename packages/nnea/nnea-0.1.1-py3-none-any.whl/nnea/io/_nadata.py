85ef039430bc2b24e2d3175cf8a66d18b5e156e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:48 +0000

    Add package nnea/0.1.1/nnea-0.1.1-py3-none-any.whl

---
+++ packages/nnea/nnea-0.1.1-py3-none-any.whl/nnea/io/_nadata.py
@@ -0,0 +1,11 @@
+
+
+class nadata(object):
+
+    def __init__(self):
+
+        self.assays = {}
+        self.meta = 0
+        self.var = 0
+        self.model = 0
+        self.indicator = 0 # sparse matrix
