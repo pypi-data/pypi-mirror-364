12651b8c2b7468309a316d597093f76cb576d82e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:14:00 +0000

    Add package whiffle-client/0.4.20/whiffle_client-0.4.20-py3-none-any.whl

---
+++ packages/whiffle-client/whiffle_client-0.4.20-py3-none-any.whl/whiffle_client/common/mapping/base.py
@@ -0,0 +1,98 @@
+from abc import ABC, abstractmethod
+
+
+class BaseMapping(ABC):
+    """Base mapping
+
+    Parameters
+    ----------
+    ABC : AbstractBaseClass
+        Abstract base class
+
+    Raises
+    ------
+    NotImplementedError
+        add not implemented
+    NotImplementedError
+        edit not implemented
+    NotImplementedError
+        get_all not implemented
+    NotImplementedError
+        get not implemented
+    NotImplementedError
+        delete not implemented
+    NotImplementedError
+        download not implemented
+    """
+
+    URL = None
+    RESOURCE_TYPE = None
+
+    def __init__(self, session):
+        self.session = session
+
+    @abstractmethod
+    def add(self):
+        """Abstract method for add
+
+        Raises
+        ------
+        NotImplementedError
+            Not implemented
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def edit(self):
+        """Abstract method for edit
+
+        Raises
+        ------
+        NotImplementedError
+            Not implemented
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_all(self):
+        """Abstract method for get_all
+
+        Raises
+        ------
+        NotImplementedError
+            Not implemented
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def get(self):
+        """Abstract method for get
+
+        Raises
+        ------
+        NotImplementedError
+            Not implemented
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def delete(self):
+        """Abstract method for delete
+
+        Raises
+        ------
+        NotImplementedError
+            Not implemented
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def download(self):
+        """Abstract method for download
+
+        Raises
+        ------
+        NotImplementedError
+            Not implemented
+        """
+        raise NotImplementedError
