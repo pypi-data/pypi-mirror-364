576dfe58d81d410dc278734e6634746944629811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:07:49 +0000

    Add package mtbmasker/0.1.1/mtbmasker-0.1.1-py3-none-any.whl

---
+++ packages/mtbmasker/mtbmasker-0.1.1-py3-none-any.whl/mtbmasker/__init__.py
@@ -0,0 +1,9 @@
+
+"""
+mtbmasker: Generate isolate-specific genome masks for Mycobacterium tuberculosis.
+"""
+
+__version__ = "0.1.1"
+
+__author__ = "Etienne Ntumba Kabongo, Dan Whiley"
+__license__ = "MIT"
