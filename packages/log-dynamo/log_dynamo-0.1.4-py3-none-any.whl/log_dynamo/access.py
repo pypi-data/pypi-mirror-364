da522558860b2d6afef96c0126e5ab27e338e445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:35 +0000

    Add package log-dynamo/0.1.4/log_dynamo-0.1.4-py3-none-any.whl

---
+++ packages/log-dynamo/log_dynamo-0.1.4-py3-none-any.whl/log_dynamo/access.py
@@ -0,0 +1,44 @@
+from dataclasses import dataclass
+
+from log_dynamo.utils import link_dynamo_table, LogTimeHelper
+
+@dataclass
+class LogAccess:
+    project: str = "app"
+    access_key: str = ""
+    secret_key: str = ""
+    endpoint_url: str = ""
+    table_name: str = "log_access"
+
+    def __post_init__(self):
+        self.table = link_dynamo_table(
+            self.table_name,
+            self.access_key,
+            self.secret_key,
+            self.endpoint_url,
+        )
+
+    def increase(self, app: str = "app", action: str = "GET /", func: str = "func"):
+        month = LogTimeHelper.current_month()
+        pk = f"{self.project}#{month}"
+        self.table.update_item(
+            Key={"pk": pk, "action": action},
+            UpdateExpression="""
+            SET 
+                #c = if_not_exists(#c, :start) + :inc,
+                #a = :app,
+                #f = :func
+        """,
+            ExpressionAttributeNames={
+                "#c": "count",
+                "#a": "app",
+                "#f": "func",
+            },
+            ExpressionAttributeValues={
+                ":start": 0,
+                ":inc": 1,
+                ":app": app,
+                ":func": func,
+            },
+            ReturnValues="UPDATED_NEW",
+        )
