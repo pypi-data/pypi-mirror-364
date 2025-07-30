32fd30cdcd200e5ba08af488e316d1d342908710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:08:16 +0000

    Add package lzl-pytools/0.2.0/lzl_pytools-0.2.0.tar.gz

---
+++ packages/lzl-pytools/lzl_pytools-0.2.0.tar.gz/lzl_pytools-0.2.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lzl_pytools",
+    version="0.2.0",
+    author="lzlcodex",
+    author_email="yishikong@163.com",
+    description="a multi run http req tool",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://gitee.com/lzlcodex/lzl-pytools.git",
+    packages=['lzl_pytools', 'lzl_pytools/apig_sdk', 'lzl_pytools/multi3', 'lms'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        # 你的依赖列表
+        # "requests>=2.25.1",
+    ],
+)
